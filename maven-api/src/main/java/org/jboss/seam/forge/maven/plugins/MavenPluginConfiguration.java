paulbakker/forge

First steps of implementing the MavenPluginFacet
added test

Added building of configuration and improved builder API

removal of plugins

Adding new configuration elements to existing plugin config

Added license header and author

shell-api/src/main/java/org/jboss/seam/forge/project/mavenplugins/MavenPluginConfiguration.java
@@ -1,0 +1,36 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.project.mavenplugins;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public interface MavenPluginConfiguration extends MavenPluginElement
+{
+   boolean hasConfigurationElement(String element);
+   List<MavenPluginConfigurationElement> listConfigurationElements();
+   MavenPluginConfiguration addConfigurationElement(MavenPluginConfigurationElement element);
+
+}

shell-api/src/main/java/org/jboss/seam/forge/project/mavenplugins/MavenPluginConfigurationBuilder.java
@@ -1,0 +1,96 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.project.mavenplugins;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public class MavenPluginConfigurationBuilder implements MavenPluginConfiguration
+{
+   private MavenPluginConfigurationImpl mavenPluginConfiguration = new MavenPluginConfigurationImpl();
+   private MavenPluginBuilder origin;
+
+   @Override public boolean hasConfigurationElement(String configElement)
+   {
+      return mavenPluginConfiguration.hasConfigurationElement(configElement);
+   }
+
+   @Override public List<MavenPluginConfigurationElement> listConfigurationElements()
+   {
+      return mavenPluginConfiguration.listConfigurationElements();
+   }
+
+   @Override public MavenPluginConfiguration addConfigurationElement(MavenPluginConfigurationElement element)
+   {
+      return mavenPluginConfiguration.addConfigurationElement(element);
+   }
+
+   @Override public String toString()
+   {
+      return mavenPluginConfiguration.toString();
+   }
+
+   private MavenPluginConfigurationBuilder() {
+
+   }
+
+   public MavenPluginConfigurationElementBuilder createConfigurationElement(String name)
+   {
+      MavenPluginConfigurationElementBuilder builder = MavenPluginConfigurationElementBuilder.create(this);
+      builder.setName(name);
+      mavenPluginConfiguration.addConfigurationElement(builder);
+      return builder;
+   }
+
+   private MavenPluginConfigurationBuilder(MavenPluginBuilder pluginBuilder) {
+      origin = pluginBuilder;
+   }
+
+
+   private MavenPluginConfigurationBuilder(MavenPluginConfiguration existingConfig, MavenPluginBuilder pluginBuilder) {
+      origin = pluginBuilder;
+      for (MavenPluginConfigurationElement element : existingConfig.listConfigurationElements())
+      {
+         mavenPluginConfiguration.addConfigurationElement(element);
+      }
+   }
+
+   public static MavenPluginConfigurationBuilder create() {
+      return new MavenPluginConfigurationBuilder();
+   }
+
+   public static MavenPluginConfigurationBuilder create(MavenPluginBuilder pluginBuilder) {
+      return new MavenPluginConfigurationBuilder(pluginBuilder);
+   }
+
+   public static MavenPluginConfigurationBuilder create(MavenPluginConfiguration existingConfig, MavenPluginBuilder pluginBuilder) {
+      return new MavenPluginConfigurationBuilder(existingConfig, pluginBuilder);
+   }
+
+   public MavenPluginBuilder getOrigin()
+   {
+      return origin;
+   }
+}

shell-api/src/main/java/org/jboss/seam/forge/project/mavenplugins/MavenPluginConfigurationElementImpl.java
@@ -1,0 +1,91 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.project.mavenplugins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public class MavenPluginConfigurationElementImpl implements MavenPluginConfigurationElement
+{
+   private String name;
+   private String text;
+   private List<MavenPluginElement> children = new ArrayList<MavenPluginElement>();
+
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+   public void setText(String text)
+   {
+      this.text = text;
+   }
+
+   @Override public String getName()
+   {
+      return name;
+   }
+
+   @Override public boolean isPlugin()
+   {
+      return name.equals("plugin");
+   }
+
+   @Override public boolean hasChilderen()
+   {
+      return false;
+   }
+
+   @Override public String getText()
+   {
+      return text;
+   }
+
+   public void addChild(MavenPluginElement element)
+   {
+      children.add(element);
+   }
+
+
+   @Override public String toString()
+   {
+      StringBuilder b = new StringBuilder();
+      b.append("<").append(name).append(">");
+      for (MavenPluginElement child : children)
+      {
+         b.append(child.toString());
+      }
+
+      if (text != null)
+      {
+         b.append(text);
+      }
+
+      b.append("</").append(name).append(">");
+      return b.toString();
+   }
+}

project-model-tests/src/test/java/org/jboss/seam/forge/project/mavenplugins/MavenPluginConfigurationImplTest.java
@@ -1,0 +1,117 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.project.mavenplugins;
+
+import org.apache.maven.model.Plugin;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public class MavenPluginConfigurationImplTest
+{
+
+   private MavenPluginConfiguration pluginConfiguration;
+
+   @Before
+   public void setup() throws Exception
+   {
+      MavenPluginAdapter adapter = new MavenPluginAdapter(createMavenPlugin());
+      pluginConfiguration = adapter.getPluginConfiguration();
+   }
+
+   @Test
+   public void testHasConfigurationElement() throws Exception
+   {
+      boolean hasElement = pluginConfiguration.hasConfigurationElement("reportPlugins");
+      assertTrue(hasElement);
+   }
+
+   @Test
+   public void testHasNotConfigurationElement() throws Exception
+   {
+      boolean hasElement = pluginConfiguration.hasConfigurationElement("unknown");
+      assertFalse(hasElement);
+   }
+
+   @Test
+   public void testListConfigurationElements() throws Exception
+   {
+      List<MavenPluginConfigurationElement> plugins = pluginConfiguration.listConfigurationElements();
+      assertThat(plugins.size(), is(1));
+      MavenPluginConfigurationElement configurationElement = plugins.get(0);
+      assertThat(configurationElement.getName(), is("reportPlugins"));
+      assertFalse(configurationElement.isPlugin());
+   }
+
+   @Test
+   public void testAddConfigurationElement() throws Exception
+   {
+      List<MavenPluginConfigurationElement> plugins = pluginConfiguration.listConfigurationElements();
+      int pluginSize = plugins.size();
+      assertThat(pluginSize, is(1));
+
+      MavenPluginConfigurationElementBuilder element =
+              MavenPluginConfigurationElementBuilder.create()
+                      .setName("testElement");
+
+      pluginConfiguration.addConfigurationElement(element);
+
+      assertThat(pluginConfiguration.listConfigurationElements().size(), is(pluginSize + 1));
+   }
+
+   private Plugin createMavenPlugin() throws Exception
+   {
+      Plugin plugin = new Plugin();
+      plugin.setGroupId("org.apache.maven.plugins");
+      plugin.setGroupId("maven-site-plugin");
+      plugin.setVersion("3.0");
+      Xpp3Dom dom;
+      dom = Xpp3DomBuilder.build(
+              new ByteArrayInputStream(
+                      ("<configuration>" +
+                              "   <reportPlugins>" +
+                              "       <plugin>" +
+                              "           <groupId>" + plugin.getGroupId() + "</groupId>" +
+                              "           <artifactId>" + plugin.getArtifactId() + "</artifactId>" +
+                              "           <version>" + plugin.getVersion() + "</version>" +
+                              "       </plugin>" +
+                              "   </reportPlugins>" +
+                              "</configuration>").getBytes()),
+              "UTF-8");
+
+      plugin.setConfiguration(dom);
+      return plugin;
+   }
+}

project-model/src/main/java/org/jboss/seam/forge/project/facets/builtin/PluginNotFoundException.java
@@ -1,0 +1,8 @@
+package org.jboss.seam.forge.project.facets.builtin;
+
+public class PluginNotFoundException extends RuntimeException
+{
+   public PluginNotFoundException(String groupId, String artifactId) {
+      super("Plugin " + groupId + ":" + artifactId + " was not found");
+   }
+}

forge/core

maven-api/src/main/java/org/jboss/seam/forge/maven/plugins/MavenPluginConfiguration.java
@@ -1,0 +1,36 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.maven.plugins;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public interface MavenPluginConfiguration extends MavenPluginElement
+{
+   boolean hasConfigurationElement(String element);
+   List<MavenPluginConfigurationElement> listConfigurationElements();
+   MavenPluginConfiguration addConfigurationElement(MavenPluginConfigurationElement element);
+
+}

maven-api/src/main/java/org/jboss/seam/forge/maven/plugins/MavenPluginConfigurationBuilder.java
@@ -1,0 +1,96 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.maven.plugins;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public class MavenPluginConfigurationBuilder implements MavenPluginConfiguration
+{
+   private MavenPluginConfigurationImpl mavenPluginConfiguration = new MavenPluginConfigurationImpl();
+   private MavenPluginBuilder origin;
+
+   @Override public boolean hasConfigurationElement(String configElement)
+   {
+      return mavenPluginConfiguration.hasConfigurationElement(configElement);
+   }
+
+   @Override public List<MavenPluginConfigurationElement> listConfigurationElements()
+   {
+      return mavenPluginConfiguration.listConfigurationElements();
+   }
+
+   @Override public MavenPluginConfiguration addConfigurationElement(MavenPluginConfigurationElement element)
+   {
+      return mavenPluginConfiguration.addConfigurationElement(element);
+   }
+
+   @Override public String toString()
+   {
+      return mavenPluginConfiguration.toString();
+   }
+
+   private MavenPluginConfigurationBuilder() {
+
+   }
+
+   public MavenPluginConfigurationElementBuilder createConfigurationElement(String name)
+   {
+      MavenPluginConfigurationElementBuilder builder = MavenPluginConfigurationElementBuilder.create(this);
+      builder.setName(name);
+      mavenPluginConfiguration.addConfigurationElement(builder);
+      return builder;
+   }
+
+   private MavenPluginConfigurationBuilder(MavenPluginBuilder pluginBuilder) {
+      origin = pluginBuilder;
+   }
+
+
+   private MavenPluginConfigurationBuilder(MavenPluginConfiguration existingConfig, MavenPluginBuilder pluginBuilder) {
+      origin = pluginBuilder;
+      for (MavenPluginConfigurationElement element : existingConfig.listConfigurationElements())
+      {
+         mavenPluginConfiguration.addConfigurationElement(element);
+      }
+   }
+
+   public static MavenPluginConfigurationBuilder create() {
+      return new MavenPluginConfigurationBuilder();
+   }
+
+   public static MavenPluginConfigurationBuilder create(MavenPluginBuilder pluginBuilder) {
+      return new MavenPluginConfigurationBuilder(pluginBuilder);
+   }
+
+   public static MavenPluginConfigurationBuilder create(MavenPluginConfiguration existingConfig, MavenPluginBuilder pluginBuilder) {
+      return new MavenPluginConfigurationBuilder(existingConfig, pluginBuilder);
+   }
+
+   public MavenPluginBuilder getOrigin()
+   {
+      return origin;
+   }
+}

maven-api/src/main/java/org/jboss/seam/forge/maven/plugins/MavenPluginConfigurationElementImpl.java
@@ -1,0 +1,91 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.maven.plugins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public class MavenPluginConfigurationElementImpl implements MavenPluginConfigurationElement
+{
+   private String name;
+   private String text;
+   private List<MavenPluginElement> children = new ArrayList<MavenPluginElement>();
+
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+   public void setText(String text)
+   {
+      this.text = text;
+   }
+
+   @Override public String getName()
+   {
+      return name;
+   }
+
+   @Override public boolean isPlugin()
+   {
+      return name.equals("plugin");
+   }
+
+   @Override public boolean hasChilderen()
+   {
+      return false;
+   }
+
+   @Override public String getText()
+   {
+      return text;
+   }
+
+   public void addChild(MavenPluginElement element)
+   {
+      children.add(element);
+   }
+
+
+   @Override public String toString()
+   {
+      StringBuilder b = new StringBuilder();
+      b.append("<").append(name).append(">");
+      for (MavenPluginElement child : children)
+      {
+         b.append(child.toString());
+      }
+
+      if (text != null)
+      {
+         b.append(text);
+      }
+
+      b.append("</").append(name).append(">");
+      return b.toString();
+   }
+}

project-model-maven-tests/src/test/java/org/jboss/seam/forge/maven/mavenplugins/MavenPluginConfigurationImplTest.java
@@ -1,0 +1,121 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.seam.forge.maven.mavenplugins;
+
+import org.apache.maven.model.Plugin;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.jboss.seam.forge.maven.plugins.MavenPluginAdapter;
+import org.jboss.seam.forge.maven.plugins.MavenPluginConfiguration;
+import org.jboss.seam.forge.maven.plugins.MavenPluginConfigurationElement;
+import org.jboss.seam.forge.maven.plugins.MavenPluginConfigurationElementBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author <a href="mailto:paul.bakker.nl@gmail.com">Paul Bakker</a>
+ */
+public class MavenPluginConfigurationImplTest
+{
+
+   private MavenPluginConfiguration pluginConfiguration;
+
+   @Before
+   public void setup() throws Exception
+   {
+      MavenPluginAdapter adapter = new MavenPluginAdapter(createMavenPlugin());
+      pluginConfiguration = adapter.getConfig();
+   }
+
+   @Test
+   public void testHasConfigurationElement() throws Exception
+   {
+      boolean hasElement = pluginConfiguration.hasConfigurationElement("reportPlugins");
+      assertTrue(hasElement);
+   }
+
+   @Test
+   public void testHasNotConfigurationElement() throws Exception
+   {
+      boolean hasElement = pluginConfiguration.hasConfigurationElement("unknown");
+      assertFalse(hasElement);
+   }
+
+   @Test
+   public void testListConfigurationElements() throws Exception
+   {
+      List<MavenPluginConfigurationElement> plugins = pluginConfiguration.listConfigurationElements();
+      assertThat(plugins.size(), is(1));
+      MavenPluginConfigurationElement configurationElement = plugins.get(0);
+      assertThat(configurationElement.getName(), is("reportPlugins"));
+      assertFalse(configurationElement.isPlugin());
+   }
+
+   @Test
+   public void testAddConfigurationElement() throws Exception
+   {
+      List<MavenPluginConfigurationElement> plugins = pluginConfiguration.listConfigurationElements();
+      int pluginSize = plugins.size();
+      assertThat(pluginSize, is(1));
+
+      MavenPluginConfigurationElementBuilder element =
+              MavenPluginConfigurationElementBuilder.create()
+                      .setName("testElement");
+
+      pluginConfiguration.addConfigurationElement(element);
+
+      assertThat(pluginConfiguration.listConfigurationElements().size(), is(pluginSize + 1));
+   }
+
+   private Plugin createMavenPlugin() throws Exception
+   {
+      Plugin plugin = new Plugin();
+      plugin.setGroupId("org.apache.maven.plugins");
+      plugin.setGroupId("maven-site-plugin");
+      plugin.setVersion("3.0");
+      Xpp3Dom dom;
+      dom = Xpp3DomBuilder.build(
+              new ByteArrayInputStream(
+                      ("<configuration>" +
+                              "   <reportPlugins>" +
+                              "       <plugin>" +
+                              "           <groupId>" + plugin.getGroupId() + "</groupId>" +
+                              "           <artifactId>" + plugin.getArtifactId() + "</artifactId>" +
+                              "           <version>" + plugin.getVersion() + "</version>" +
+                              "       </plugin>" +
+                              "   </reportPlugins>" +
+                              "</configuration>").getBytes()),
+              "UTF-8");
+
+      plugin.setConfiguration(dom);
+      return plugin;
+   }
+}

project-model-maven/src/main/java/org/jboss/seam/forge/maven/facets/exceptions/PluginNotFoundException.java
@@ -1,0 +1,8 @@
+package org.jboss.seam.forge.maven.facets.exceptions;
+
+public class PluginNotFoundException extends RuntimeException
+{
+   public PluginNotFoundException(String groupId, String artifactId) {
+      super("Plugin " + groupId + ":" + artifactId + " was not found");
+   }
+}