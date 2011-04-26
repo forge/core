1017362427c8ae66172e29307ec0ad91cf25b1ea
Koen Aers <koen.aers@jboss.com>
2011-04-26 20:48:40 +0200

    Implemented SEAMFORGE-126

---
+++ shell/src/main/java/org/jboss/forge/shell/IdeTerminal.java
@@ -0,0 +1,24 @@
+package org.jboss.forge.shell;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import jline.TerminalSupport;
+
+public class IdeTerminal extends TerminalSupport 
+{
+   
+	public IdeTerminal() 
+	{
+	   super(true);
+	   setEchoEnabled(false);
+	   setAnsiSupported(true);
+	}
+	
+	public int readCharacter(final InputStream in) throws IOException 
+	{
+	   int result = in.read();
+	   return result == '\r' ? in.read() : result;
+	}
+
+}
