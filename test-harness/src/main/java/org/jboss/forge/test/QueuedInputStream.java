c460244ac4fa7533f66fc5d0dd4ad4bc7f340bce
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2011-09-09 17:51:31 -0400

    Re-added test prettification with comment so nobody like gastaldi deletes it again :)

--- test-harness/src/main/java/org/jboss/forge/test/QueuedInputStream.java
+++ test-harness/src/main/java/org/jboss/forge/test/QueuedInputStream.java
@@ -84,6 +84,7 @@ public class QueuedInputStream extends InputStream
             if (!inputQueue.isEmpty())
             {
                String line = inputQueue.remove();
+               System.out.println(); // makes the output look like someone typed the input :)
                byte[] bytes = new byte[] {};
 
                if (line != null)
