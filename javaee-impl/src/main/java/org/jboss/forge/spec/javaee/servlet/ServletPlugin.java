62dcebdda16706a7f5f68ad7b2d4cde0aad23443
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2011-09-19 14:08:14 -0400

    SEAMFORGE-232

---
+++ javaee-impl/src/main/java/org/jboss/forge/spec/javaee/servlet/ServletPlugin.java
@@ -0,0 +1,72 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.forge.spec.javaee.servlet;
+
+import javax.enterprise.event.Event;
+import javax.inject.Inject;
+
+import org.jboss.forge.project.Project;
+import org.jboss.forge.project.facets.events.InstallFacets;
+import org.jboss.forge.shell.ShellMessages;
+import org.jboss.forge.shell.plugins.Alias;
+import org.jboss.forge.shell.plugins.Command;
+import org.jboss.forge.shell.plugins.DefaultCommand;
+import org.jboss.forge.shell.plugins.PipeOut;
+import org.jboss.forge.shell.plugins.Plugin;
+import org.jboss.forge.spec.javaee.ServletFacet;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+@Alias("servlet")
+public class ServletPlugin implements Plugin
+{
+   @Inject
+   private Project project;
+
+   @Inject
+   private Event<InstallFacets> request;
+
+   @DefaultCommand
+   public void status(final PipeOut out)
+   {
+      if (project.hasFacet(ServletFacet.class))
+      {
+         ShellMessages.success(out, "Servlet is installed.");
+      }
+      else
+      {
+         ShellMessages.warn(out, "Servlet is NOT installed.");
+      }
+   }
+
+   @Command("setup")
+   public void setup(final PipeOut out)
+   {
+      if (!project.hasFacet(ServletFacet.class))
+      {
+         request.fire(new InstallFacets(ServletFacet.class));
+      }
+      status(out);
+   }
+
+}
---
+++ javaee-impl/src/test/java/org/jboss/forge/spec/servlet/ServletPluginTest.java
@@ -0,0 +1,68 @@
+package org.jboss.forge.spec.servlet;
+
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+import junit.framework.Assert;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.project.Project;
+import org.jboss.forge.project.dependencies.DependencyBuilder;
+import org.jboss.forge.project.facets.DependencyFacet;
+import org.jboss.forge.project.facets.WebResourceFacet;
+import org.jboss.forge.spec.javaee.ServletFacet;
+import org.jboss.forge.test.AbstractShellTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+@RunWith(Arquillian.class)
+public class ServletPluginTest extends AbstractShellTest
+{
+   @Test
+   public void testServletSetup() throws Exception
+   {
+      Project project = initializeJavaProject();
+      queueInputLines("y");
+      getShell().execute("servlet setup");
+
+      project.getFacet(ServletFacet.class).getConfig();
+      Assert.assertTrue(project.getFacet(DependencyFacet.class).hasDependency(
+               DependencyBuilder.create("org.jboss.spec:jboss-javaee-6.0")));
+      Assert.assertTrue(project.getFacet(WebResourceFacet.class).getWebResource("WEB-INF/web.xml").exists());
+   }
+
+   @Test
+   public void testSetupServlet() throws Exception
+   {
+      Project project = initializeJavaProject();
+      queueInputLines("y");
+      getShell().execute("setup servlet");
+
+      project.getFacet(ServletFacet.class).getConfig();
+      Assert.assertTrue(project.getFacet(DependencyFacet.class).hasDependency(
+               DependencyBuilder.create("org.jboss.spec:jboss-javaee-6.0")));
+      Assert.assertTrue(project.getFacet(WebResourceFacet.class).getWebResource("WEB-INF/web.xml").exists());
+   }
+}
