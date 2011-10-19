0674476cfb2c641a3d557bf84b03d4807dc8f801
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2011-10-19 17:13:04 -0400

    error pages and welcome page are now part of quickstart

--- javaee-impl/src/main/java/org/jboss/forge/spec/javaee/servlet/ServletPlugin.java
+++ javaee-impl/src/main/java/org/jboss/forge/spec/javaee/servlet/ServletPlugin.java
@@ -37,6 +37,8 @@ import org.jboss.forge.shell.plugins.PipeOut;
 import org.jboss.forge.shell.plugins.Plugin;
 import org.jboss.forge.shell.plugins.SetupCommand;
 import org.jboss.forge.spec.javaee.ServletFacet;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.spec.servlet.web.WebAppDescriptor;
 
 /**
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
@@ -79,25 +81,33 @@ public class ServletPlugin implements Plugin
       status(out);
    }
 
-    private void installQuickstart(DirectoryResource webRoot)
-    {
-        ((FileResource<?>) webRoot.getChild("404.html")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/404.html"));
-        ((FileResource<?>) webRoot.getChild("500.html")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/500.html"));
-        ((FileResource<?>) webRoot.getChild("index.html")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/index.html"));
-        ((FileResource<?>) webRoot.getChild("forge-logo.png")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/forge-logo.png"));
-        ((FileResource<?>) webRoot.getChild("forge-style.css")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/forge-style.css"));
-        ((FileResource<?>) webRoot.getChild("jboss-community.png")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/jboss-community.png"));
-        ((FileResource<?>) webRoot.getChild("bkg.gif")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/bkg.gif"));
-        ((FileResource<?>) webRoot.getChild("favicon.ico")).setContents(getClass().getResourceAsStream(
-                 "/org/jboss/forge/web/favicon.ico"));
-    }
+   private void installQuickstart(DirectoryResource webRoot)
+   {
+      ((FileResource<?>) webRoot.getChild("404.html")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/404.html"));
+      ((FileResource<?>) webRoot.getChild("500.html")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/500.html"));
+      ((FileResource<?>) webRoot.getChild("index.html")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/index.html"));
+      ((FileResource<?>) webRoot.getChild("forge-logo.png")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/forge-logo.png"));
+      ((FileResource<?>) webRoot.getChild("forge-style.css")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/forge-style.css"));
+      ((FileResource<?>) webRoot.getChild("jboss-community.png")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/jboss-community.png"));
+      ((FileResource<?>) webRoot.getChild("bkg.gif")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/bkg.gif"));
+      ((FileResource<?>) webRoot.getChild("favicon.ico")).setContents(getClass().getResourceAsStream(
+               "/org/jboss/forge/web/favicon.ico"));
 
+      FileResource<?> descriptor = project.getFacet(ServletFacet.class).getConfigFile();
+      WebAppDescriptor unit = Descriptors.create(WebAppDescriptor.class)
+               .welcomeFile("/index.html");
+
+      unit.errorPage(404, "/404.html");
+      unit.errorPage(500, "/500.html");
+
+      descriptor.setContents(unit.exportAsString());
+   }
 
 }
