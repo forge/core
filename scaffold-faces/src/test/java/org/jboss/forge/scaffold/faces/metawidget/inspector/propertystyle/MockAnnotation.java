fcf4743793efcfbe4fdc23dd49b4a43fa226800f
Richard Kennard <richard@kennardconsulting.com>
2012-01-14 21:08:55 +1100

    Flatten resources. Fix capitalization
    Change-Id: I2a158e64afe0b27c883cba54c8e957522dea0531

---
+++ scaffold-faces/src/test/java/org/jboss/forge/scaffold/faces/metawidget/inspector/propertystyle/MockAnnotation.java
@@ -0,0 +1,90 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.forge.scaffold.faces.metawidget.inspector.propertystyle;
+
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ *
+ */
+public @interface MockAnnotation
+{
+   byte aByte();
+
+   short aShort();
+
+   int anInt();
+
+   long aLong();
+
+   float aFloat();
+
+   double aDouble();
+
+   char aChar();
+
+   boolean aBoolean();
+
+   String aString();
+
+   Class<?> aClass();
+
+   AnotherMockAnnotation anAnnotation();
+
+   anEnum anEnum();
+
+   byte[] aByteArray();
+
+   short[] aShortArray();
+
+   int[] anIntArray();
+
+   long[] aLongArray();
+
+   float[] aFloatArray();
+
+   double[] aDoubleArray();
+
+   char[] aCharArray();
+
+   boolean[] aBooleanArray();
+
+   String[] aStringArray();
+
+   Class<?>[] aClassArray();
+
+   AnotherMockAnnotation[] anAnnotationArray();
+
+   anEnum[] anEnumArray();
+
+   //
+   // Inner class
+   //
+
+   public static enum anEnum {
+
+      ONE,
+      TWO,
+      THREE
+   }
+}
