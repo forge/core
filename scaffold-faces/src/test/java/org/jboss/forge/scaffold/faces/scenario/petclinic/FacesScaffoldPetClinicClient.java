f63c8c9e5141f35a04063a39d13dc6e240f4eb12
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-01-30 14:05:56 -0500

    Fixed tests for new toString methods

--- scaffold-faces/src/test/java/org/jboss/forge/scaffold/faces/scenario/petclinic/FacesScaffoldPetClinicClient.java
+++ scaffold-faces/src/test/java/org/jboss/forge/scaffold/faces/scenario/petclinic/FacesScaffoldPetClinicClient.java
@@ -21,7 +21,8 @@
  */
 package org.jboss.forge.scaffold.faces.scenario.petclinic;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.jboss.jsfunit.api.InitialPage;
 import org.jboss.jsfunit.api.JSFUnitResource;
@@ -109,15 +110,15 @@ public class FacesScaffoldPetClinicClient
          assertEquals("Pet #1", table.getCellAt(1, 0).getTextContent());
          assertEquals("2", table.getCellAt(1, 1).getTextContent());
          DomNode booleanNode = table.getCellAt(1, 2).getChildNodes().get(0).getChildNodes().get(0);
-         assertEquals("span", booleanNode.getNodeName() );
-         assertEquals("", booleanNode.getTextContent() );
-         assertEquals("boolean-true", booleanNode.getAttributes().getNamedItem("class").getNodeValue() );
-         assertEquals("Owner Firstname #1, Owner Lastname #1, Owner Address #1, , , , ", table.getCellAt(1, 3)
+         assertEquals("span", booleanNode.getNodeName());
+         assertEquals("", booleanNode.getTextContent());
+         assertEquals("boolean-true", booleanNode.getAttributes().getNamedItem("class").getNodeValue());
+         assertEquals("Owner Firstname #1 Owner Lastname #1 Owner Address #1", table.getCellAt(1, 3)
                   .getTextContent());
 
          page = page.getAnchorByText("Pet #1").click();
          assertTrue(page.asText().contains("View existing Pet"));
-         page = page.getAnchorByText("Owner Firstname #1, Owner Lastname #1, Owner Address #1, , , ,").click();
+         page = page.getAnchorByText("Owner Firstname #1 Owner Lastname #1 Owner Address #1").click();
          assertTrue(page.asText().contains("View existing Owner"));
 
          // Create a new Owner
