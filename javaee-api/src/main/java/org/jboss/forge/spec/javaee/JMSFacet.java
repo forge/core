1db3de0f94b4e9fa6c512ef189d0e1154fea1a41
Paul Bakker <paul.bakker.nl@gmail.com>
2012-02-11 21:11:59 +0100

    Adding jms plugin

---
+++ javaee-api/src/main/java/org/jboss/forge/spec/javaee/JMSFacet.java
@@ -0,0 +1,10 @@
+package org.jboss.forge.spec.javaee;
+
+import org.jboss.forge.project.Facet;
+
+/**
+ * @Author Paul Bakker - paul.bakker@luminis.eu
+ */
+public interface JMSFacet extends Facet
+{
+}
