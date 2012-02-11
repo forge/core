2012a50bda50f7783e41a9f27f0b3175cec72f9f
Paul Bakker <paul.bakker.nl@gmail.com>
2012-02-11 21:45:58 +0100

    Adding jms plugin

--- javaee-impl/src/main/java/org/jboss/forge/spec/javaee/jms/JmsFacetImpl.java
+++ javaee-impl/src/main/java/org/jboss/forge/spec/javaee/jms/JmsFacetImpl.java
@@ -3,17 +3,21 @@ package org.jboss.forge.spec.javaee.jms;
 import org.jboss.forge.project.dependencies.Dependency;
 import org.jboss.forge.project.dependencies.DependencyBuilder;
 import org.jboss.forge.project.dependencies.DependencyInstaller;
+import org.jboss.forge.shell.plugins.Alias;
 import org.jboss.forge.spec.javaee.BaseJavaEEFacet;
 import org.jboss.forge.spec.javaee.JMSFacet;
 
+import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * @Author Paul Bakker - paul.bakker@luminis.eu
  */
+@Alias("forge.spec.jms")
 public class JmsFacetImpl extends BaseJavaEEFacet implements JMSFacet
 {
+    @Inject
     public JmsFacetImpl(DependencyInstaller installer)
     {
         super(installer);
