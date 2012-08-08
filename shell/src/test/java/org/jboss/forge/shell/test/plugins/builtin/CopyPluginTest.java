6ecb8d098a8794a51e4140632b66842c9ba63ef6
George Gastaldi <gegastaldi@gmail.com>
2012-08-08 16:11:30 -0300

    FORGE-605: Safer unit tests

--- shell/src/test/java/org/jboss/forge/shell/test/plugins/builtin/CopyPluginTest.java
+++ shell/src/test/java/org/jboss/forge/shell/test/plugins/builtin/CopyPluginTest.java
@@ -4,280 +4,285 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 import org.jboss.forge.resources.DirectoryResource;
 import org.jboss.forge.resources.FileResource;
 import org.jboss.forge.resources.Resource;
 import org.jboss.forge.shell.Shell;
+import org.jboss.forge.shell.util.Streams;
 import org.jboss.forge.test.AbstractShellTest;
 import org.junit.Test;
 
 /**
  * Test for Copy Plugin
- * 
+ *
  * @author tremes@redhat.com
- * 
+ *
  */
 
-public class CopyPluginTest extends AbstractShellTest {
+public class CopyPluginTest extends AbstractShellTest
+{
 
-	@Test
-	public void testCopyFileToFolder() throws Exception {
-		
-		String testFolder= "testFolder";
-		String file = "copyFile";
+   @Test
+   public void testCopyFileToFolder() throws Exception
+   {
 
-		initializeJavaProject();
-		Shell shell = getShell();
-		shell.execute( "mkdir "+testFolder );
-		shell.execute( "touch "+file );
+      String testFolder = "testFolder";
+      String file = "copyFile";
 
-		Resource<?> fileToCopy = shell.getCurrentDirectory().getChild(file);
-		assertTrue( fileToCopy.exists() );
+      initializeJavaProject();
+      Shell shell = getShell();
+      shell.execute("mkdir " + testFolder);
+      shell.execute("touch " + file);
 
-		shell.execute( "cp "+file+" "+testFolder );
-		assertTrue( fileToCopy.exists() );
+      Resource<?> fileToCopy = shell.getCurrentDirectory().getChild(file);
+      assertTrue(fileToCopy.exists());
 
-		shell.execute( "cd "+testFolder );
-		Resource<?> copy = shell.getCurrentDirectory().getChild(file);
+      shell.execute("cp " + file + " " + testFolder);
+      assertTrue(fileToCopy.exists());
 
-		assertTrue( copy.exists() );
+      shell.execute("cd " + testFolder);
+      Resource<?> copy = shell.getCurrentDirectory().getChild(file);
 
-	}
+      assertTrue(copy.exists());
 
-	@Test
-	public void testCopyFileToNewFile() throws Exception {
+   }
 
-		String file = "copyFile";
-		String nonExisting = "newNoneExisting";
-		
-		initializeJavaProject();
-		Shell shell = getShell();
-		shell.execute( "touch "+file );
+   @Test
+   public void testCopyFileToNewFile() throws Exception
+   {
 
-		Resource<?> fileToCopy = shell.getCurrentDirectory().getChild(file);
-		assertTrue( fileToCopy.exists() );
+      String file = "copyFile";
+      String nonExisting = "newNoneExisting";
 
-		shell.execute( "cp "+file+" "+nonExisting );
-		Resource<?> copy = shell.getCurrentDirectory().getChild(nonExisting);
-		assertTrue( copy.exists() );
+      initializeJavaProject();
+      Shell shell = getShell();
+      shell.execute("touch " + file);
 
-	}
+      Resource<?> fileToCopy = shell.getCurrentDirectory().getChild(file);
+      assertTrue(fileToCopy.exists());
 
-	@Test
-	public void testCopyFileWithRewrite() throws Exception {
+      shell.execute("cp " + file + " " + nonExisting);
+      Resource<?> copy = shell.getCurrentDirectory().getChild(nonExisting);
+      assertTrue(copy.exists());
 
-		String testFolder= "testFolder";
-		String file = "copyFile";
-		
-		initializeJavaProject();
-		Shell shell = getShell();
-		shell.execute( "mkdir "+testFolder );
-		shell.execute( "touch "+file );
-		shell.execute( "cd "+testFolder );
-		shell.execute( "touch "+file );
-		shell.execute( "cd .." );
+   }
 
-		FileResource<?> fileToCopy = (FileResource<?>) shell.getCurrentDirectory().getChild(file);
-		assertTrue( fileToCopy.exists() );
+   @Test
+   public void testCopyFileWithRewrite() throws Exception
+   {
 
-		shell.execute( "cd "+testFolder );
-		FileResource<?> copy = (FileResource<?>) shell.getCurrentDirectory().getChild(file);
-		shell.execute( "cp "+file+" "+testFolder );
-		assertTrue( fileToCopy.exists() );
-		assertTrue( copy.exists() );
-		assertTrue( compareFiles( fileToCopy.getUnderlyingResourceObject(), copy.getUnderlyingResourceObject() ) );
-		
-	}
+      String testFolder = "testFolder";
+      String file = "copyFile";
 
-	@Test
-	public void testCopyEmptyFolder() throws Exception {
+      initializeJavaProject();
+      Shell shell = getShell();
+      shell.execute("mkdir " + testFolder);
+      shell.execute("touch " + file);
+      shell.execute("cd " + testFolder);
+      shell.execute("touch " + file);
+      shell.execute("cd ..");
 
-		String testFolder= "testFolder";
-		String newFolder= "newFolder";
-		
-		initializeJavaProject();
-		Shell shell = getShell();
-		shell.execute( "mkdir "+testFolder );
+      FileResource<?> fileToCopy = (FileResource<?>) shell.getCurrentDirectory().getChild(file);
+      assertTrue(fileToCopy.exists());
 
-		Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory(testFolder);
-		assertTrue( dirToCopy.exists() );
+      shell.execute("cd " + testFolder);
+      FileResource<?> copy = (FileResource<?>) shell.getCurrentDirectory().getChild(file);
+      shell.execute("cp " + file + " " + testFolder);
+      assertTrue(fileToCopy.exists());
+      assertTrue(copy.exists());
+      assertTrue(compareFiles(fileToCopy.getUnderlyingResourceObject(), copy.getUnderlyingResourceObject()));
 
-		shell.execute( "cp "+testFolder+" " + newFolder );
-		Resource<?> copy = shell.getCurrentDirectory().getChild(newFolder);
-		assertTrue( copy.exists() );
-		assertTrue( ( (DirectoryResource) copy ).isDirectory() );
+   }
 
-	}
+   @Test
+   public void testCopyEmptyFolder() throws Exception
+   {
 
-	@Test
-	public void testCopyEmptyFolderToExisting() throws Exception {
+      String testFolder = "testFolder";
+      String newFolder = "newFolder";
 
-		String testFolder= "testFolder";
-		String newFolder= "newFolder";
-		
-		initializeJavaProject();
-		Shell shell = getShell();
-		shell.execute( "mkdir "+testFolder );
-		shell.execute( "mkdir "+newFolder );
+      initializeJavaProject();
+      Shell shell = getShell();
+      shell.execute("mkdir " + testFolder);
 
-		Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory(testFolder);
-		assertTrue( dirToCopy.exists() );
+      Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory(testFolder);
+      assertTrue(dirToCopy.exists());
 
-		shell.execute( "cp "+testFolder+" "+newFolder );
-		Resource<?> targetParent = shell.getCurrentDirectory().getChild(newFolder);
-		Resource<?> copy = ( (DirectoryResource) targetParent ).getChildDirectory(testFolder);
-		assertTrue( copy.exists() );
-		assertTrue( ( (DirectoryResource) copy ).isDirectory() );
+      shell.execute("cp " + testFolder + " " + newFolder);
+      Resource<?> copy = shell.getCurrentDirectory().getChild(newFolder);
+      assertTrue(copy.exists());
+      assertTrue(((DirectoryResource) copy).isDirectory());
 
-	}
+   }
 
-	@Test
-	public void testCopyNonEmptyFolder() throws Exception{
-		
-		String testFolder= "testFolder";
-		String newFolder= "newFolder";
-		String subFolderA= "subFolder1";
-		String subFolderB= "subFolder2";
-		String fileA= "file1";
-		String fileB= "file2";
-		
-		 initializeJavaProject();
-	     Shell shell = getShell();
-	     shell.execute( "mkdir "+testFolder );
-	     shell.execute( "cd "+testFolder );
-	     shell.execute( "mkdir "+subFolderA );
-	     shell.execute( "mkdir "+subFolderB );
-	     shell.execute( "touch "+fileA );
-	     shell.execute( "cd "+subFolderA );
-	     shell.execute( "touch "+fileB );
-	     shell.execute( "cd .." );
-	     shell.execute( "cd .." );
-	     
-	     Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory(testFolder);
-	     assertTrue(dirToCopy.exists());
-	     FileResource<?> file1 = (FileResource<?>) dirToCopy.getChild(fileA);
-	     assertTrue(file1.exists());
-	     Resource<?> subFolder1 = ((DirectoryResource) dirToCopy).getChildDirectory(subFolderA);
-	     Resource<?> file2 =((DirectoryResource) subFolder1).getChild( fileB );
-	     assertTrue(file2.exists());
-	     
-	     assertTrue(((DirectoryResource) subFolder1).isDirectory());
-	     assertTrue(((DirectoryResource) subFolder1).exists());
-	     
-	     Resource<?> subFolder2= ((DirectoryResource) dirToCopy).getChildDirectory(subFolderB);
-	     assertTrue(((DirectoryResource) subFolder2).isDirectory());
-	     assertTrue(((DirectoryResource) subFolder2).exists());
-	     
-	     
-	     shell.execute("cp "+testFolder+" "+newFolder);
-	     
-	     Resource<?> copy = shell.getCurrentDirectory().getChildDirectory(newFolder);
-	     assertTrue(copy.exists());
-	     assertTrue(((DirectoryResource)copy).isDirectory());
-	     
-	     FileResource<?> copyfile1 = (FileResource<?>) copy.getChild(fileA);
-	     assertTrue(copyfile1.exists());
+   @Test
+   public void testCopyEmptyFolderToExisting() throws Exception
+   {
 
-	     DirectoryResource copySubFolder1 = ((DirectoryResource) copy).getChildDirectory(subFolderA);
-	     assertTrue(copySubFolder1.exists());
-	     Resource<?> copyfile2 =((DirectoryResource) copySubFolder1).getChild( fileB);
-	     assertTrue(copyfile2.exists());
-	     
-	     DirectoryResource copySubFolder2 = ((DirectoryResource) copy).getChildDirectory(subFolderA);
-	     assertTrue(copySubFolder2.exists());
-	     
-		
-	}
+      String testFolder = "testFolder";
+      String newFolder = "newFolder";
 
-	@Test
-	public void testCopyFolderToExistingFolder() throws Exception {
+      initializeJavaProject();
+      Shell shell = getShell();
+      shell.execute("mkdir " + testFolder);
+      shell.execute("mkdir " + newFolder);
 
-		String testFolder= "testFolder";
-		String newFolder= "newFolder";
-		String subFolderA= "subFolder1";
-		String subFolderB= "subFolder2";
-		String fileA= "file1";
-		String fileB= "file2";
-		
-		initializeJavaProject();
-		Shell shell = getShell();
-		shell.execute( "mkdir "+testFolder );
-		shell.execute( "cd "+testFolder );
-		shell.execute( "mkdir "+subFolderA );
-		shell.execute( "mkdir "+subFolderB );
-		shell.execute( "touch "+fileA );
-		shell.execute( "cd "+subFolderA );
-		shell.execute( "touch "+fileB );
-		shell.execute( "cd .." );
-		shell.execute( "cd .." );
-		shell.execute( "mkdir "+newFolder );
+      Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory(testFolder);
+      assertTrue(dirToCopy.exists());
 
-		Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory( testFolder );
-		assertTrue( dirToCopy.exists() );
-		FileResource<?> file1 = (FileResource<?>) dirToCopy.getChild( fileA );
-		assertTrue( file1.exists() );
-		Resource<?> subFolder1 = ( (DirectoryResource) dirToCopy ).getChildDirectory( subFolderA );
-		FileResource<?> file2 = (FileResource<?>) subFolder1.getChild( fileB );
-		assertTrue( file2.exists() );
+      shell.execute("cp " + testFolder + " " + newFolder);
+      Resource<?> targetParent = shell.getCurrentDirectory().getChild(newFolder);
+      Resource<?> copy = ((DirectoryResource) targetParent).getChildDirectory(testFolder);
+      assertTrue(copy.exists());
+      assertTrue(((DirectoryResource) copy).isDirectory());
 
-		assertTrue( ( (DirectoryResource) subFolder1 ).isDirectory() );
-		assertTrue( ( (DirectoryResource) subFolder1 ).exists() );
+   }
 
-		Resource<?> subFolder2 = ( (DirectoryResource) dirToCopy ).getChildDirectory( subFolderB );
-		assertTrue( ( (DirectoryResource) subFolder2 ).isDirectory() );
-		assertTrue( ( (DirectoryResource) subFolder2 ).exists() );
+   @Test
+   public void testCopyNonEmptyFolder() throws Exception
+   {
 
-		shell.execute( "cp "+testFolder+" "+newFolder );
+      String testFolder = "testFolder";
+      String newFolder = "newFolder";
+      String subFolderA = "subFolder1";
+      String subFolderB = "subFolder2";
+      String fileA = "file1";
+      String fileB = "file2";
 
-		Resource<?> targetParent = shell.getCurrentDirectory().getChildDirectory( newFolder );
-		assertTrue( targetParent.exists() );
-		assertTrue( ( (DirectoryResource) targetParent ).isDirectory() );
+      initializeJavaProject();
+      Shell shell = getShell();
+      shell.execute("mkdir " + testFolder);
+      shell.execute("cd " + testFolder);
+      shell.execute("mkdir " + subFolderA);
+      shell.execute("mkdir " + subFolderB);
+      shell.execute("touch " + fileA);
+      shell.execute("cd " + subFolderA);
+      shell.execute("touch " + fileB);
+      shell.execute("cd ..");
+      shell.execute("cd ..");
 
-		Resource<?> copy = ( (DirectoryResource) targetParent ).getChildDirectory( testFolder );
-		assertTrue( copy.exists() );
-		assertTrue( ( (DirectoryResource) copy ).isDirectory() );
+      Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory(testFolder);
+      assertTrue(dirToCopy.exists());
+      FileResource<?> file1 = (FileResource<?>) dirToCopy.getChild(fileA);
+      assertTrue(file1.exists());
+      Resource<?> subFolder1 = ((DirectoryResource) dirToCopy).getChildDirectory(subFolderA);
+      Resource<?> file2 = ((DirectoryResource) subFolder1).getChild(fileB);
+      assertTrue(file2.exists());
 
-		FileResource<?> copyfile1 = (FileResource<?>) copy.getChild( fileA );
-		assertTrue( copyfile1.exists() );
-		assertTrue( compareFiles( copyfile1.getUnderlyingResourceObject(), file1.getUnderlyingResourceObject() ) );
+      assertTrue(((DirectoryResource) subFolder1).isDirectory());
+      assertTrue(((DirectoryResource) subFolder1).exists());
 
-		DirectoryResource copySubFolder1 = ( (DirectoryResource) copy ).getChildDirectory( subFolderA );
-		assertTrue( copySubFolder1.exists() );
-		FileResource<?> copyfile2 = (FileResource<?>) copySubFolder1.getChild( fileB );
-		assertTrue( copyfile2.exists() );
-		assertTrue( compareFiles( copyfile2.getUnderlyingResourceObject(), file2.getUnderlyingResourceObject() ) );
+      Resource<?> subFolder2 = ((DirectoryResource) dirToCopy).getChildDirectory(subFolderB);
+      assertTrue(((DirectoryResource) subFolder2).isDirectory());
+      assertTrue(((DirectoryResource) subFolder2).exists());
 
-		DirectoryResource copySubFolder2 = ( (DirectoryResource) copy ).getChildDirectory( subFolderB );
-		assertTrue( copySubFolder2.exists() );
-	}
+      shell.execute("cp " + testFolder + " " + newFolder);
 
-	private boolean compareFiles(File f1, File f2) {
-		try {
-			if ( f1.length() == f2.length() ) {
-				FileInputStream fis1 = new FileInputStream( f1 );
-				FileInputStream fis2 = new FileInputStream( f2 );
+      Resource<?> copy = shell.getCurrentDirectory().getChildDirectory(newFolder);
+      assertTrue(copy.exists());
+      assertTrue(((DirectoryResource) copy).isDirectory());
 
-				while ( true ) {
-					int a = fis1.read();
-					int b = fis2.read();
-					if ( a != b ) {
-						return false;
-					}
-					if ( a == -1 ) {
-						return true;
-					}
-				}
-			}
-			else {
-				return false;
-			}
+      FileResource<?> copyfile1 = (FileResource<?>) copy.getChild(fileA);
+      assertTrue(copyfile1.exists());
 
-		}
-		catch ( Exception e ) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+      DirectoryResource copySubFolder1 = ((DirectoryResource) copy).getChildDirectory(subFolderA);
+      assertTrue(copySubFolder1.exists());
+      Resource<?> copyfile2 = ((DirectoryResource) copySubFolder1).getChild(fileB);
+      assertTrue(copyfile2.exists());
 
+      DirectoryResource copySubFolder2 = ((DirectoryResource) copy).getChildDirectory(subFolderA);
+      assertTrue(copySubFolder2.exists());
+
+   }
+
+   @Test
+   public void testCopyFolderToExistingFolder() throws Exception
+   {
+
+      String testFolder = "testFolder";
+      String newFolder = "newFolder";
+      String subFolderA = "subFolder1";
+      String subFolderB = "subFolder2";
+      String fileA = "file1";
+      String fileB = "file2";
+
+      initializeJavaProject();
+      Shell shell = getShell();
+      shell.execute("mkdir " + testFolder);
+      shell.execute("cd " + testFolder);
+      shell.execute("mkdir " + subFolderA);
+      shell.execute("mkdir " + subFolderB);
+      shell.execute("touch " + fileA);
+      shell.execute("cd " + subFolderA);
+      shell.execute("touch " + fileB);
+      shell.execute("cd ..");
+      shell.execute("cd ..");
+      shell.execute("mkdir " + newFolder);
+
+      Resource<?> dirToCopy = shell.getCurrentDirectory().getChildDirectory(testFolder);
+      assertTrue(dirToCopy.exists());
+      FileResource<?> file1 = (FileResource<?>) dirToCopy.getChild(fileA);
+      assertTrue(file1.exists());
+      Resource<?> subFolder1 = ((DirectoryResource) dirToCopy).getChildDirectory(subFolderA);
+      FileResource<?> file2 = (FileResource<?>) subFolder1.getChild(fileB);
+      assertTrue(file2.exists());
+
+      assertTrue(((DirectoryResource) subFolder1).isDirectory());
+      assertTrue(((DirectoryResource) subFolder1).exists());
+
+      Resource<?> subFolder2 = ((DirectoryResource) dirToCopy).getChildDirectory(subFolderB);
+      assertTrue(((DirectoryResource) subFolder2).isDirectory());
+      assertTrue(((DirectoryResource) subFolder2).exists());
+
+      shell.execute("cp " + testFolder + " " + newFolder);
+
+      Resource<?> targetParent = shell.getCurrentDirectory().getChildDirectory(newFolder);
+      assertTrue(targetParent.exists());
+      assertTrue(((DirectoryResource) targetParent).isDirectory());
+
+      Resource<?> copy = ((DirectoryResource) targetParent).getChildDirectory(testFolder);
+      assertTrue(copy.exists());
+      assertTrue(((DirectoryResource) copy).isDirectory());
+
+      FileResource<?> copyfile1 = (FileResource<?>) copy.getChild(fileA);
+      assertTrue(copyfile1.exists());
+      assertTrue(compareFiles(copyfile1.getUnderlyingResourceObject(), file1.getUnderlyingResourceObject()));
+
+      DirectoryResource copySubFolder1 = ((DirectoryResource) copy).getChildDirectory(subFolderA);
+      assertTrue(copySubFolder1.exists());
+      FileResource<?> copyfile2 = (FileResource<?>) copySubFolder1.getChild(fileB);
+      assertTrue(copyfile2.exists());
+      assertTrue(compareFiles(copyfile2.getUnderlyingResourceObject(), file2.getUnderlyingResourceObject()));
+
+      DirectoryResource copySubFolder2 = ((DirectoryResource) copy).getChildDirectory(subFolderB);
+      assertTrue(copySubFolder2.exists());
+   }
+
+   private boolean compareFiles(File f1, File f2)
+   {
+      boolean ret = false;
+      FileInputStream fis1 = null;
+      FileInputStream fis2 = null;
+      try
+      {
+         fis1 = new FileInputStream(f1);
+         fis2 = new FileInputStream(f2);
+
+         String fis1Str = Streams.toString(fis1);
+         String fis2Str = Streams.toString(fis2);
+         ret = fis1Str.equals(fis2Str);
+      }
+      catch (IOException e)
+      {
+         ret = false;
+      }
+      finally
+      {
+         Streams.closeQuietly(fis1);
+         Streams.closeQuietly(fis2);
+      }
+      return ret;
+   }
 }
