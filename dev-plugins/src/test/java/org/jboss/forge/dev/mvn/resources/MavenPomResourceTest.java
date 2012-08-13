2ff8d46db218bb00db8e94c49cdad28669b4ac0f
Jevgeni Zelenkov <jevgeni.zelenkov@gmail.com>
2012-08-13 16:43:22 +0200

    License change to EPL: dev-plugins

--- dev-plugins/src/test/java/org/jboss/forge/dev/mvn/resources/MavenPomResourceTest.java
+++ dev-plugins/src/test/java/org/jboss/forge/dev/mvn/resources/MavenPomResourceTest.java
@@ -1,13 +1,12 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2010, Red Hat Middleware LLC, and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Eclipse Public License Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
