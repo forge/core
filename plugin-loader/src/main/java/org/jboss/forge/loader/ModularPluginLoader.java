9fd4c4ee543dea35b28275897446e7e31b398a1b
George Gastaldi <gegastaldi@gmail.com>
2012-08-13 12:53:27 -0300

    FORGE-640

--- plugin-loader/src/main/java/org/jboss/forge/loader/ModularPluginLoader.java
+++ plugin-loader/src/main/java/org/jboss/forge/loader/ModularPluginLoader.java
@@ -1,24 +1,10 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat, Inc., and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.jboss.forge.loader;
 
 import java.io.File;
@@ -38,9 +24,9 @@ import org.jboss.modules.ResourceLoaders;
 /**
  * To use this loader, just instantiate it and start loading modules. it will handle delegation to the boot module
  * loader.
- * 
+ *
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
- * 
+ *
  *         http://docs.jboss.org/jbossmodules/1.0.2.GA/api/index.html?org/jboss/modules/ModuleSpec.html
  */
 public class ModularPluginLoader extends ModuleLoader
@@ -49,9 +35,8 @@ public class ModularPluginLoader extends ModuleLoader
    protected Module preloadModule(final ModuleIdentifier identifier) throws ModuleLoadException
    {
       /*
-       * This method is used only to determine which module loader a module belongs to. 
-       * Calling preloadModule() from that loader specifies that the module should be 
-       * loaded from that loader.
+       * This method is used only to determine which module loader a module belongs to. Calling preloadModule() from
+       * that loader specifies that the module should be loaded from that loader.
        */
 
       if (!identifier.equals("belongs to me"))
@@ -61,8 +46,8 @@ public class ModularPluginLoader extends ModuleLoader
       }
 
       /*
-       * need some kind of criteria that determines whether or not a module is a plugin, or whether
-       * it should actually be loaded by the system module loader - see: ClassifyingModuleLoader
+       * need some kind of criteria that determines whether or not a module is a plugin, or whether it should actually
+       * be loaded by the system module loader - see: ClassifyingModuleLoader
        */
 
       return super.preloadModule(identifier); // this is mine,
@@ -71,7 +56,8 @@ public class ModularPluginLoader extends ModuleLoader
    @Override
    protected ModuleSpec findModule(final ModuleIdentifier id) throws ModuleLoadException
    {
-      try {
+      try
+      {
          id.getName();
          id.getSlot();
          Builder builder = ModuleSpec.build(id);
@@ -81,7 +67,8 @@ public class ModularPluginLoader extends ModuleLoader
          builder.addDependency(DependencySpec.createModuleDependencySpec(ModuleIdentifier.create("org.jboss.forge")));
          return builder.create();
       }
-      catch (IOException e) {
+      catch (IOException e)
+      {
          throw new ModuleLoadException("Failed to load module", e);
       }
    }
