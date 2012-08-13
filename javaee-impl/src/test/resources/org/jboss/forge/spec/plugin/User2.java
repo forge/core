16797f2ee182dc41c5a5e2464d5239ce58da18bd
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-08-13 11:58:01 -0400

    FORGE-635

--- javaee-impl/src/test/resources/org/jboss/forge/spec/plugin/User2.java
+++ javaee-impl/src/test/resources/org/jboss/forge/spec/plugin/User2.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 import javax.persistence.Entity;
 import java.io.Serializable;
 import javax.persistence.Id;
