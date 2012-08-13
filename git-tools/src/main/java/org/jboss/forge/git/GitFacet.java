2f689f264bba55056b0704e5c6a657905c0353cd
George Gastaldi <gegastaldi@gmail.com>
2012-08-13 13:16:27 -0300

    FORGE-631

--- git-tools/src/main/java/org/jboss/forge/git/GitFacet.java
+++ git-tools/src/main/java/org/jboss/forge/git/GitFacet.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.jboss.forge.git;
 
 import org.eclipse.jgit.api.Git;
@@ -9,7 +16,7 @@ import org.jboss.forge.shell.plugins.Alias;
 
 /**
  * @author <a href="mailto:jevgeni.zelenkov@gmail.com">Jevgeni Zelenkov</a>
- * 
+ *
  */
 @Alias("forge.vcs.git")
 public class GitFacet extends BaseFacet
--- git-tools/src/main/java/org/jboss/forge/git/GitUtils.java
+++ git-tools/src/main/java/org/jboss/forge/git/GitUtils.java
@@ -1,24 +1,10 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat, Inc., and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.jboss.forge.git;
 
 import java.io.IOException;
@@ -50,10 +36,10 @@ import org.jboss.forge.resources.FileResource;
 
 /**
  * Convenience tools for interacting with the Git version control system.
- * 
+ *
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  * @author <a href="mailto:jevgeni.zelenkov@gmail.com">Jevgeni Zelenkov</a>
- * 
+ *
  */
 public abstract class GitUtils
 {
@@ -117,7 +103,7 @@ public abstract class GitUtils
 
    /**
     * Initialize a new git repository.
-    * 
+    *
     * @param dir The directory in which to create a new .git/ folder and repository.
     */
    public static Git init(final DirectoryResource dir) throws IOException
