05b4d834f5c58fdf19719c8b2e5e31198aeabf1f
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-11-08 15:33:23 -0500

    Container API is no longer a beans archive

--- plugin-container/src/main/java/org/jboss/forge/container/impl/BeanRegistrant.java
+++ plugin-container/src/main/java/org/jboss/forge/container/impl/BeanRegistrant.java
@@ -5,9 +5,6 @@ import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 
-import org.jboss.forge.container.event.ContainerRestart;
-import org.jboss.forge.container.event.ContainerShutdown;
-import org.jboss.forge.container.event.ContainerStartup;
 import org.jboss.forge.container.services.RemoteProxyBeanProducerMethod;
 
 public class BeanRegistrant implements Extension
@@ -20,10 +17,5 @@ public class BeanRegistrant implements Extension
       event.addAnnotatedType(manager.createAnnotatedType(ServiceRegistryImpl.class));
       event.addAnnotatedType(manager.createAnnotatedType(ServiceRegistryInitializer.class));
       event.addAnnotatedType(manager.createAnnotatedType(RemoteProxyBeanProducerMethod.class));
-
-      // Impl event types
-      event.addAnnotatedType(manager.createAnnotatedType(ContainerRestart.class));
-      event.addAnnotatedType(manager.createAnnotatedType(ContainerShutdown.class));
-      event.addAnnotatedType(manager.createAnnotatedType(ContainerStartup.class));
    }
 }
