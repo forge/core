02a622f25ee8f1bc8a88d446a38b1652d668a31f
George Gastaldi <gegastaldi@gmail.com>
2012-12-20 18:46:56 -0200

    Added more tests to conversion API #2

--- ui/impl/src/test/java/org/jboss/forge/ui/impl/converter/ConverterTest.java
+++ ui/impl/src/test/java/org/jboss/forge/ui/impl/converter/ConverterTest.java
@@ -7,6 +7,8 @@
 
 package org.jboss.forge.ui.impl.converter;
 
+import java.util.HashMap;
+import java.util.Properties;
 import java.util.ServiceLoader;
 import java.util.Vector;
 
@@ -19,12 +21,12 @@ import org.junit.Test;
 
 public class ConverterTest
 {
-   private ConverterRegistry converterRegistry;
+   private ConverterRegistry registry;
 
    @Before
    public void setUp()
    {
-      converterRegistry = ServiceLoader.load(ConverterRegistry.class).iterator().next();
+      registry = ServiceLoader.load(ConverterRegistry.class).iterator().next();
    }
 
    @Test
@@ -32,7 +34,7 @@ public class ConverterTest
    {
       String input = "123";
       Integer expected = 123;
-      Converter<String, Integer> converter = converterRegistry.getConverter(String.class, Integer.class);
+      Converter<String, Integer> converter = registry.getConverter(String.class, Integer.class);
       Assert.assertEquals(expected, converter.convert(input));
    }
 
@@ -41,7 +43,7 @@ public class ConverterTest
    {
       String input = "123";
       String expected = "valueOf_123";
-      Converter<String, SimpleBean> converter = converterRegistry.getConverter(String.class, SimpleBean.class);
+      Converter<String, SimpleBean> converter = registry.getConverter(String.class, SimpleBean.class);
       SimpleBean obj = converter.convert(input);
       Assert.assertNotNull(obj);
       Assert.assertEquals(expected, obj.getValue());
@@ -51,7 +53,7 @@ public class ConverterTest
    public void testObjectToObjectConversion() throws Exception
    {
       SimpleBean input = new SimpleBean("value");
-      Converter<SimpleBean, AnotherBean> converter = converterRegistry
+      Converter<SimpleBean, AnotherBean> converter = registry
                .getConverter(SimpleBean.class, AnotherBean.class);
       AnotherBean obj = converter.convert(input);
       Assert.assertNotNull(obj);
@@ -61,7 +63,41 @@ public class ConverterTest
    @Test(expected = ConverterNotFoundException.class)
    public void testConverterNotFound() throws Exception
    {
-      converterRegistry.getConverter(AnotherBean.class, Vector.class);
+      registry.getConverter(AnotherBean.class, Vector.class);
    }
 
+   @Test
+   public void testEnumConverter() throws Exception
+   {
+      Converter<String, SimpleEnum> converter = registry.getConverter(String.class, SimpleEnum.class);
+      SimpleEnum result = converter.convert("STARTED");
+      Assert.assertEquals(SimpleEnum.STARTED, result);
+   }
+
+   @SuppressWarnings("rawtypes")
+   @Test
+   public void testCustomConverter() throws Exception
+   {
+      Converter<Properties, HashMap> converter = new Converter<Properties, HashMap>()
+      {
+         @Override
+         @SuppressWarnings("unchecked")
+         public HashMap convert(Properties source) throws Exception
+         {
+            HashMap map = new HashMap();
+            map.putAll(source);
+            return map;
+         }
+      };
+      registry.addConverter(Properties.class, HashMap.class, converter);
+
+      Converter<Properties, HashMap> converterFromRegistry = registry.getConverter(Properties.class, HashMap.class);
+      Assert.assertSame(converter, converterFromRegistry);
+
+      Properties props = new Properties();
+      props.setProperty("propOne", "valOne");
+      HashMap map = converterFromRegistry.convert(props);
+      Assert.assertEquals(1, map.size());
+      Assert.assertEquals("valOne", map.get("propOne"));
+   }
 }
