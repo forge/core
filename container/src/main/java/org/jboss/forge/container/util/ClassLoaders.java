4f6233366b6cc5f5dadfbdb7618ce762897568e2
George Gastaldi <gegastaldi@gmail.com>
2012-12-21 18:04:40 -0200

    Using JUL in ClassLoaders

--- container/src/main/java/org/jboss/forge/container/util/ClassLoaders.java
+++ container/src/main/java/org/jboss/forge/container/util/ClassLoaders.java
@@ -1,27 +1,30 @@
 package org.jboss.forge.container.util;
 
 import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.jboss.forge.container.exception.ContainerException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Utility class for executing fragments of code within a specific {@link Thread#getContextClassLoader()}
- * 
+ *
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  */
 public class ClassLoaders
 {
-   private static Logger log = LoggerFactory.getLogger(ClassLoaders.class);
+   private static Logger log = Logger.getLogger(ClassLoaders.class.getName());
 
    /**
     * Execute the given {@link Callable} in the {@link ClassLoader} provided. Return the result, if any.
     */
    public static <T> T executeIn(ClassLoader loader, Callable<T> task)
    {
-      log.debug("[Thread " + Thread.currentThread().getName() + "] ClassLoader ["
-               + loader + "] task began.");
+      if (log.isLoggable(Level.FINE))
+      {
+         log.fine("[Thread " + Thread.currentThread().getName() + "] ClassLoader ["
+                  + loader + "] task began.");
+      }
       ClassLoader original = Thread.currentThread().getContextClassLoader();
       try
       {
@@ -36,8 +39,11 @@ public class ClassLoaders
       finally
       {
          Thread.currentThread().setContextClassLoader(original);
-         log.debug("[Thread " + Thread.currentThread().getName() + "] ClassLoader ["
-                  + loader + "] task ended.");
+         if (log.isLoggable(Level.FINE))
+         {
+            log.fine("[Thread " + Thread.currentThread().getName() + "] ClassLoader ["
+                     + loader + "] task ended.");
+         }
       }
    }
 }
