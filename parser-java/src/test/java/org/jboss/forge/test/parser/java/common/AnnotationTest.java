4861ed65cbeaf6f481d5fc48e1fd53553a7a422e
Matt Benson <gudnabrsam@gmail.com>
2013-01-03 10:51:32 -0600

    FORGE-436: JavaSource needs a way to parse nested annotations

--- parser-java/src/test/java/org/jboss/forge/test/parser/java/common/AnnotationTest.java
+++ parser-java/src/test/java/org/jboss/forge/test/parser/java/common/AnnotationTest.java
@@ -156,6 +156,10 @@ public abstract class AnnotationTest<O extends JavaSource<O>, T>
 
       Annotation<O> annotation = annotations.get(annotations.size() - 1);
       assertEquals("@com.test.Foo({ElementType.FIELD,ElementType.METHOD})", annotation.getLiteralValue());
+      
+      Annotation<O> nested = annotation.getAnnotationValue();
+      assertEquals("com.test.Foo", nested.getName());
+      assertEquals("{ElementType.FIELD,ElementType.METHOD}", nested.getLiteralValue());
    }
 
    @Test
@@ -167,6 +171,10 @@ public abstract class AnnotationTest<O extends JavaSource<O>, T>
 
       Annotation<O> annotation = annotations.get(annotations.size() - 1);
       assertEquals("@com.test.Foo(bar=\"baz\")", annotation.getLiteralValue("foo"));
+
+      Annotation<O> nested = annotation.getAnnotationValue("foo");
+      assertEquals("com.test.Foo", nested.getName());
+      assertEquals("baz", nested.getStringValue("bar"));
    }
 
    @Test
@@ -179,6 +187,10 @@ public abstract class AnnotationTest<O extends JavaSource<O>, T>
 
       Annotation<O> annotation = annotations.get(annotations.size() - 1);
       assertEquals("@com.test.Foo(@com.test.Bar)", annotation.getLiteralValue());
+
+      Annotation<O> deeplyNested = annotation.getAnnotationValue().getAnnotationValue();
+      assertEquals("com.test.Bar", deeplyNested.getName());
+      assertTrue(deeplyNested.isMarker());
    }
 
    @Test
@@ -191,6 +203,10 @@ public abstract class AnnotationTest<O extends JavaSource<O>, T>
       
       Annotation<O> annotation = annotations.get(annotations.size() - 1);
       assertEquals("@com.test.Foo(bar=@com.test.Bar)", annotation.getLiteralValue("foo"));
+
+      Annotation<O> deeplyNested = annotation.getAnnotationValue("foo").getAnnotationValue("bar");
+      assertEquals("com.test.Bar", deeplyNested.getName());
+      assertTrue(deeplyNested.isMarker());
    }
    
    @Test
