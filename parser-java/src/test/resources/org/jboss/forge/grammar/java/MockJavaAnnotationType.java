5910a1de7d69f480081f3f7057a9001cc9c0aec8
mbenson <mbenson@pgac.com>
2013-01-04 18:19:45 -0600

    FORGE-733: API for annotation elements in JavaAnnotation type definitions

---
+++ parser-java/src/test/resources/org/jboss/forge/grammar/java/MockJavaAnnotationType.java
@@ -0,0 +1,18 @@
+package org.jboss.forge.grammar.java;
+
+import org.jboss.forge.test.parser.java.common.MockEnumType;
+
+public @interface MockJavaAnnotationType
+{
+   public @interface MockNestedJavaAnnotationType
+   {
+      int value();
+
+      Class<? extends CharSequence> charSequenceType() default String.class;
+
+      @Deprecated
+      MockEnumType metasyntacticVariable() default FOO;
+   }
+
+   MockNestedJavaAnnotationType value() default @MockNestedJavaAnnotationType(-1);
+}
