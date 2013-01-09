c2f79c21143aacd7b560f76a381c500cfafa5ddc
George Gastaldi <gegastaldi@gmail.com>
2013-01-09 13:51:59 -0200

    FORGE-739: BeansPlugin: add new command 'new-qualifier'

--- javaee-impl/src/test/java/org/jboss/forge/spec/cdi/BeansPluginTest.java
+++ javaee-impl/src/test/java/org/jboss/forge/spec/cdi/BeansPluginTest.java
@@ -6,12 +6,22 @@
  */
 package org.jboss.forge.spec.cdi;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Qualifier;
 
 import junit.framework.Assert;
 
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.parser.JavaParser;
+import org.jboss.forge.parser.java.JavaAnnotation;
 import org.jboss.forge.parser.java.JavaClass;
 import org.jboss.forge.parser.java.JavaSource;
 import org.jboss.forge.project.Project;
@@ -110,5 +120,104 @@ public class BeansPluginTest extends AbstractShellTest
       // reload:
       resource = java.getJavaResource("foo.beans.ExampleBean");
       Assert.assertTrue(JavaClass.class.cast(resource.getJavaSource()).hasAnnotation(ApplicationScoped.class));
+   }
+
+   @Test
+   public void testNewQualifier() throws Exception
+   {
+      final boolean inherited = false;
+      testNewQualifier(inherited);
+   }
+
+   @Test
+   public void testNewQualifierInherited() throws Exception
+   {
+      final boolean inherited = true;
+      testNewQualifier(inherited);
+   }
+
+   private void testNewQualifier(boolean inherited) throws Exception
+   {
+      Project project = initializeJavaProject();
+      queueInputLines("y", "");
+      getShell().execute("beans setup");
+      String command = "beans new-qualifier --type foo.beans.ExampleQualifier";
+      if (inherited)
+      {
+         command += " --inherited true";
+      }
+      getShell().execute(command);
+
+      JavaSourceFacet java = project.getFacet(JavaSourceFacet.class);
+      JavaResource resource = java.getJavaResource("foo.beans.ExampleQualifier");
+
+      Assert.assertTrue(resource.exists());
+      JavaSource<?> source = resource.getJavaSource();
+
+      Assert.assertNotNull(source);
+      Assert.assertEquals("foo.beans", source.getPackage());
+      Assert.assertTrue(source.isAnnotation());
+      JavaAnnotation qualifier = (JavaAnnotation) source;
+      Assert.assertTrue(qualifier.hasAnnotation(Qualifier.class));
+      Assert.assertTrue(qualifier.getAnnotation(Qualifier.class).isMarker());
+
+      if (inherited)
+      {
+         Assert.assertTrue(qualifier.hasAnnotation(Inherited.class));
+         Assert.assertTrue(qualifier.getAnnotation(Inherited.class).isMarker());
+      }
+
+      Assert.assertTrue(qualifier.hasAnnotation(Retention.class));
+      Assert.assertSame(RetentionPolicy.RUNTIME,
+               qualifier.getAnnotation(Retention.class).getEnumValue(RetentionPolicy.class));
+
+      Assert.assertTrue(qualifier.hasAnnotation(Target.class));
+      JavaAnnotation stub = JavaParser.parse(JavaAnnotation.class, "public @interface Stub {}");
+      stub.addAnnotation(Target.class).setEnumValue(ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER,
+               ElementType.TYPE);
+      Assert.assertEquals(stub.getAnnotation(Target.class).getLiteralValue(), qualifier.getAnnotation(Target.class)
+               .getLiteralValue());
+
+      Assert.assertTrue(qualifier.hasAnnotation(Documented.class));
+      Assert.assertTrue(qualifier.getAnnotation(Documented.class).isMarker());
+
+      project.getFacet(CDIFacet.class).getConfig();
+   }
+
+   @Test(expected = RuntimeException.class)
+   public void testCannotOverwriteQualifer() throws Exception
+   {
+      Project project = initializeJavaProject();
+      queueInputLines("y", "");
+      getShell().execute("beans setup");
+      String command = "beans new-qualifier --type foo.beans.ExampleQualifier";
+      getShell().execute(command);
+
+      JavaSourceFacet java = project.getFacet(JavaSourceFacet.class);
+      JavaResource resource = java.getJavaResource("foo.beans.ExampleQualifier");
+
+      Assert.assertTrue(resource.exists());
+      getShell().execute(command);
+   }
+
+   @Test
+   public void testOverwriteQualifer() throws Exception
+   {
+      Project project = initializeJavaProject();
+      queueInputLines("y", "");
+      getShell().execute("beans setup");
+      String command = "beans new-qualifier --type foo.beans.ExampleQualifier";
+      getShell().execute(command);
+
+      JavaSourceFacet java = project.getFacet(JavaSourceFacet.class);
+      JavaResource resource = java.getJavaResource("foo.beans.ExampleQualifier");
+
+      Assert.assertTrue(resource.exists());
+      Assert.assertFalse(JavaAnnotation.class.cast(resource.getJavaSource()).hasAnnotation(Inherited.class));
+
+      getShell().execute(command + " --inherited --overwrite");
+      // reload:
+      resource = java.getJavaResource("foo.beans.ExampleQualifier");
+      Assert.assertTrue(JavaAnnotation.class.cast(resource.getJavaSource()).hasAnnotation(Inherited.class));
    }
 }
