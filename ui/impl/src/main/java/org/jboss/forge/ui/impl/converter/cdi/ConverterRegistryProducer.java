1923607787075a3f41fc7b6f416217eda98819d5
George Gastaldi <gegastaldi@gmail.com>
2013-01-10 21:11:45 -0200

    Removed scope annotations

--- ui/impl/src/main/java/org/jboss/forge/ui/impl/converter/cdi/ConverterRegistryProducer.java
+++ ui/impl/src/main/java/org/jboss/forge/ui/impl/converter/cdi/ConverterRegistryProducer.java
@@ -7,10 +7,8 @@
 
 package org.jboss.forge.ui.impl.converter.cdi;
 
-import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.inject.Singleton;
 
 import org.jboss.forge.ui.converter.ConverterRegistry;
 import org.jboss.forge.ui.impl.ConverterRegistryImpl;
@@ -18,8 +16,6 @@ import org.jboss.forge.ui.impl.ConverterRegistryImpl;
 public class ConverterRegistryProducer
 {
    @Produces
-   @Default
-   @Singleton
    public ConverterRegistry produce(InjectionPoint injectionPoint)
    {
       return ConverterRegistryImpl.INSTANCE;
