a4351c0f2b1631576621890150a7950d68414c3b
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-01-14 19:16:32 -0500

    Services should be looked up by native type (not qualified as @Service) from RemoteServiceImpl lazy proxy

--- container/src/main/java/org/jboss/forge/container/services/RemoteServiceProxyBeanCallback.java
+++ container/src/main/java/org/jboss/forge/container/services/RemoteServiceProxyBeanCallback.java
@@ -1,5 +1,13 @@
 package org.jboss.forge.container.services;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Member;
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.InjectionPoint;
 
 import net.sf.cglib.proxy.LazyLoader;
@@ -7,9 +15,11 @@ import net.sf.cglib.proxy.LazyLoader;
 import org.jboss.forge.container.Addon;
 import org.jboss.forge.container.AddonRegistry;
 import org.jboss.forge.container.impl.RemoteInstanceImpl;
+import org.jboss.forge.container.impl.Service;
 
 public class RemoteServiceProxyBeanCallback implements LazyLoader
 {
+
    private final Class<?> serviceType;
    private final AddonRegistry registry;
    private final InjectionPoint injectionPoint;
@@ -32,10 +42,11 @@ public class RemoteServiceProxyBeanCallback implements LazyLoader
          {
             RemoteInstance<?> instance = serviceRegistry.getRemoteInstance(serviceType);
             if (instance instanceof RemoteInstanceImpl)
-            {
-               result = ((RemoteInstanceImpl<?>) instance).get(injectionPoint);
-            }
-            result = instance.get();
+               // FIXME remove the need for this implementation coupling
+               result = ((RemoteInstanceImpl<?>) instance).get(new NativeServiceInjectionPoint(injectionPoint,
+                        serviceType));
+            else
+               result = instance.get();
             break;
          }
       }
@@ -46,5 +57,76 @@ public class RemoteServiceProxyBeanCallback implements LazyLoader
       }
 
       return result;
+   }
+
+   /**
+    * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+    * 
+    */
+   public class NativeServiceInjectionPoint implements InjectionPoint
+   {
+
+      private InjectionPoint wrapped;
+      private Set<Annotation> qualifiers;
+      private Class<?> serviceType;
+
+      public NativeServiceInjectionPoint(InjectionPoint wrapped, Class<?> serviceType)
+      {
+         this.wrapped = wrapped;
+         this.qualifiers = new HashSet<Annotation>(wrapped.getQualifiers());
+
+         for (Annotation a : qualifiers)
+         {
+            if (a instanceof Service)
+            {
+               qualifiers.remove(a);
+               break;
+            }
+         }
+
+         this.serviceType = serviceType;
+      }
+
+      @Override
+      public Type getType()
+      {
+         return serviceType;
+      }
+
+      @Override
+      public Set<Annotation> getQualifiers()
+      {
+         return qualifiers;
+      }
+
+      @Override
+      public Bean<?> getBean()
+      {
+         return wrapped.getBean();
+      }
+
+      @Override
+      public Member getMember()
+      {
+         return wrapped.getMember();
+      }
+
+      @Override
+      public Annotated getAnnotated()
+      {
+         return wrapped.getAnnotated();
+      }
+
+      @Override
+      public boolean isDelegate()
+      {
+         return wrapped.isDelegate();
+      }
+
+      @Override
+      public boolean isTransient()
+      {
+         return wrapped.isTransient();
+      }
    }
 }
