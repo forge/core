0b6d916af2bcd20634bd3788e4defbd24d1c7075
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-01-17 17:09:44 -0500

    Added projects test and fixed pom hierarchy of UI addon

---
+++ projects/tests/src/test/java/org/jboss/forge/projects/NewProjectCommandTest.java
@@ -0,0 +1,53 @@
+package org.jboss.forge.projects;
+
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.Addon;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.container.AddonDependency;
+import org.jboss.forge.container.AddonId;
+import org.jboss.forge.projects.impl.NewProjectCommand;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class NewProjectCommandTest
+{
+   @Deployment
+   @Dependencies({ @Addon(name = "org.jboss.forge:facets", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:projects", version = "2.0.0-SNAPSHOT") })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap
+               .create(ForgeArchive.class)
+               .addAsManifestResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"))
+               .addAsAddonDependencies(
+                        AddonDependency.create(AddonId.from("org.jboss.forge:projects", "2.0.0-SNAPSHOT")));
+
+      return archive;
+   }
+
+   @Inject
+   private NewProjectCommand command;
+
+   @Test
+   public void testInjectionNotNull()
+   {
+      Assert.assertNotNull(command);
+   }
+
+}
