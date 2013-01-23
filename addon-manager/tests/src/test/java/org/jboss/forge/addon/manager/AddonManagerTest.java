3f3e6576521d31f28ca5354e7b8e1d43126b505f
George Gastaldi <gegastaldi@gmail.com>
2013-01-23 01:47:24 -0200

    Fixed tests due to change of deps

--- addon-manager/tests/src/test/java/org/jboss/forge/addon/manager/AddonManagerTest.java
+++ addon-manager/tests/src/test/java/org/jboss/forge/addon/manager/AddonManagerTest.java
@@ -95,8 +95,8 @@ public class AddonManagerTest
       AddonId resources = AddonId.fromCoordinates("org.jboss.forge:resources,2.0.0-SNAPSHOT");
       InstallRequest request = addonManager.install(resources);
 
-      Assert.assertEquals(2, request.getRequiredAddons().size());
-      Assert.assertEquals(0, request.getOptionalAddons().size());
+      Assert.assertEquals(1, request.getRequiredAddons().size());
+      Assert.assertEquals(1, request.getOptionalAddons().size());
 
       request.perform();
 
@@ -128,7 +128,6 @@ public class AddonManagerTest
          Assert.assertTrue("Not a valid addon dependency: " + dependency.getId().getName(),
                   addonDependenciesIds.remove(dependency.getId().getName()));
          Assert.assertEquals("2.0.0-SNAPSHOT", dependency.getId().getVersion());
-         Assert.assertFalse(dependency.isOptional());
       }
       Assert.assertTrue("Addons not detected as dependency: " + addonDependenciesIds, addonDependenciesIds.isEmpty());
       // FIXME Should this be true?
