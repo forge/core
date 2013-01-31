47774e35d60fb70719de8d35fd8d54d3b2173cf0
George Gastaldi <gegastaldi@gmail.com>
2013-01-31 18:20:21 -0200

    Added some methods to UISelection

--- ui/api/src/main/java/org/jboss/forge/ui/UISelection.java
+++ ui/api/src/main/java/org/jboss/forge/ui/UISelection.java
@@ -3,12 +3,25 @@ package org.jboss.forge.ui;
 /**
  * Represents the objects with on which the {@link UIContext} is currently focused. This may be the current working
  * directory, highlighted files, text, or other focusable items.
- * 
+ *
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
- * 
+ *
  * @param <SELECTIONTYPE> The selection type, must be common between all selections (if multiple).
  */
 public interface UISelection<SELECTIONTYPE> extends Iterable<SELECTIONTYPE>
 {
-   // TODO define interface
+   /**
+    * Returns the first element in this selection
+    *
+    * @return
+    */
+   public SELECTIONTYPE get();
+
+   /**
+    * Returns the number of elements of this selection
+    *
+    * @return
+    */
+   public int size();
+
 }
