c99b3beb6fc5de1622081c54a77cf0f2b5ab9e14
George Gastaldi <gegastaldi@gmail.com>
2013-01-31 18:29:23 -0200

    Improved javadoc for UISelection

--- ui/api/src/main/java/org/jboss/forge/ui/UISelection.java
+++ ui/api/src/main/java/org/jboss/forge/ui/UISelection.java
@@ -4,23 +4,27 @@ package org.jboss.forge.ui;
  * Represents the objects with on which the {@link UIContext} is currently focused. This may be the current working
  * directory, highlighted files, text, or other focusable items.
  *
- * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * This object and the iterator provided by this object are immutable.
  *
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  * @param <SELECTIONTYPE> The selection type, must be common between all selections (if multiple).
  */
 public interface UISelection<SELECTIONTYPE> extends Iterable<SELECTIONTYPE>
 {
    /**
-    * Returns the first element in this selection
+    * Provides the first selected element as a{@code SELECTIONTYPE}.
     *
-    * @return
+    * This is a convenience method to avoid iteration.
+    *
+    * @return the first element in this selection
     */
    public SELECTIONTYPE get();
 
    /**
-    * Returns the number of elements of this selection
+    * Counts the elements in this selection.
     *
-    * @return
+    * @return the number of selected elements
     */
    public int size();
 
