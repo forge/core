8f21d09fcf2116eceda8c28575a39ec5172ae872
George Gastaldi <gegastaldi@gmail.com>
2013-02-16 17:06:53 -0200

    Added license headers and improved URLResource impl

--- resources/api/src/main/java/org/jboss/forge/resource/URLResource.java
+++ resources/api/src/main/java/org/jboss/forge/resource/URLResource.java
@@ -1,14 +1,26 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.jboss.forge.resource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.jboss.forge.container.util.Assert;
 import org.jboss.forge.facets.Facet;
 
 /**
+ * Represents a {@link URL} resource
+ *
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 public class URLResource extends VirtualResource<URL>
 {
@@ -17,12 +29,14 @@ public class URLResource extends VirtualResource<URL>
    public URLResource(ResourceFactory factory, URL resource)
    {
       super(factory, null);
+      Assert.notNull(resource, "URL resource cannot be null");
       this.resource = resource;
    }
 
    protected URLResource(ResourceFactory factory, Resource<?> parent, URL resource)
    {
       super(factory, parent);
+      Assert.notNull(resource, "URL resource cannot be null");
       this.resource = resource;
    }
 
@@ -54,6 +68,40 @@ public class URLResource extends VirtualResource<URL>
    public boolean supports(Class<? extends Facet<?>> type)
    {
       return false;
+   }
+
+   @Override
+   public InputStream getResourceInputStream()
+   {
+      try
+      {
+         return resource.openStream();
+      }
+      catch (IOException e)
+      {
+         throw new RuntimeException("Could not open stream", e);
+      }
+   }
+
+   @Override
+   public boolean exists()
+   {
+      HttpURLConnection connection;
+      try
+      {
+         connection = (HttpURLConnection) resource.openConnection();
+         connection.setRequestMethod("HEAD");
+         int responseCode = connection.getResponseCode();
+         if (responseCode != 200)
+         {
+            return false;
+         }
+      }
+      catch (IOException e)
+      {
+         return false;
+      }
+      return true;
    }
 
    @Override
