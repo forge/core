5471014a74795d6fae043cda0e095dd77def0512
George Gastaldi <gegastaldi@gmail.com>
2013-02-22 18:13:49 -0300

    Added ui-hints for JavaResource

---
+++ parser-java/impl/src/main/java/org/jboss/forge/parser/java/hints/InputTypeHintsRegistrant.java
@@ -0,0 +1,33 @@
+package org.jboss.forge.parser.java.hints;
+
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.forge.container.event.Startup;
+import org.jboss.forge.environment.Environment;
+import org.jboss.forge.parser.java.resources.JavaResource;
+import org.jboss.forge.ui.hints.HintsLookup;
+import org.jboss.forge.ui.hints.InputTypes;
+
+/**
+ * Only active when ui-hints addon is installed.
+ *
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ *
+ */
+public class InputTypeHintsRegistrant
+{
+   private Environment environment;
+
+   @Inject
+   public InputTypeHintsRegistrant(Environment environment)
+   {
+      this.environment = environment;
+   }
+
+   public void initialize(@Observes Startup event)
+   {
+      HintsLookup hints = new HintsLookup(environment);
+      hints.setInputType(JavaResource.class, InputTypes.JAVA_CLASS_PICKER);
+   }
+}
