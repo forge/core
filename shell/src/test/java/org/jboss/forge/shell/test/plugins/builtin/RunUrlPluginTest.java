35b9b8161d18c5c654c800a4c013f3f5700b8dce
George Gastaldi <gegastaldi@gmail.com>
2013-02-25 18:47:14 -0300

    FORGE-576: Using Jetty to mock tests

--- shell/src/test/java/org/jboss/forge/shell/test/plugins/builtin/RunUrlPluginTest.java
+++ shell/src/test/java/org/jboss/forge/shell/test/plugins/builtin/RunUrlPluginTest.java
@@ -6,25 +6,58 @@
  */
 package org.jboss.forge.shell.test.plugins.builtin;
 
-import java.net.UnknownHostException;
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.forge.shell.Shell;
 import org.jboss.forge.test.AbstractShellTest;
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
 public class RunUrlPluginTest extends AbstractShellTest
 {
+   private static Server server;
+
+   @BeforeClass
+   public static void setupHttpServer() throws Exception
+   {
+      server = new Server(18080);
+      server.setHandler(new AbstractHandler()
+      {
+
+         @Override
+         public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+                  throws IOException, ServletException
+         {
+            if (target.equals("/RunUrlPluginTest.fsh"))
+            {
+               response.getOutputStream().write("echo 'run-url plugin test';".getBytes());
+            }
+            else
+            {
+               response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            }
+         }
+      });
+      server.start();
+   }
 
    @Test
    public void testRunScriptFromHttpUrl() throws Exception
    {
       Shell shell = getShell();
 
-      shell.execute("run-url https://raw.github.com/forge/core/master/shell/src/test/resources/org/jboss/forge/shell/test/plugins/builtin/RunUrlPluginTest.fsh");
+      shell.execute("run-url http://localhost:18080/RunUrlPluginTest.fsh");
    }
 
    @Test(expected = RuntimeException.class)
@@ -35,21 +68,18 @@ public class RunUrlPluginTest extends AbstractShellTest
       shell.execute("run-url new_file_invented.script");
    }
 
-   @Ignore
-   @Test(expected = UnknownHostException.class)
-   public void testRunScriptNotHostHttpUrl() throws Exception
-   {
-      Shell shell = getShell();
-
-      shell.execute("run-url http://not-found-host-ffsdf2423.org/not-found-file");
-   }
-
    @Test
    public void testRunScriptNotFoundHttpUrl() throws Exception
    {
       Shell shell = getShell();
 
-      shell.execute("run-url http://dl.dropbox.com/u/19065548/not-found-file");
+      shell.execute("run-url http://localhost:18080/not-found-file");
+   }
+
+   @AfterClass
+   public static void tearDownHttpServer() throws Exception
+   {
+      server.stop();
    }
 
 }
