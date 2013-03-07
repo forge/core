655d9714b70345b52465378a42e8193165cc935d
George Gastaldi <gegastaldi@gmail.com>
2013-03-07 18:16:00 -0300

    Created placeholder classes

---
+++ dependencies/addon/src/main/java/org/jboss/forge/dependencies/Root.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.dependencies;
+
+public class Root
+{
+
+   public Root()
+   {
+      // TODO Auto-generated constructor stub
+   }
+
+}
