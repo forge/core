fd11e603c6efdb4b134975c983d56b18556fbe77
George Gastaldi <gegastaldi@gmail.com>
2013-03-07 18:24:37 -0300

    Changed Root to package-info to allow package-level annotations and provide some usefulness

--- maven/addon/src/main/java/org/jboss/forge/maven/package-info.java
+++ maven/addon/src/main/java/org/jboss/forge/maven/package-info.java
@@ -4,14 +4,5 @@
  * Licensed under the Eclipse Public License version 1.0, available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.jboss.forge.convert;
+package org.jboss.forge.maven;
 
-public class Root
-{
-
-   public Root()
-   {
-      // TODO Auto-generated constructor stub
-   }
-
-}
--- parser-java/addon/src/main/java/org/jboss/forge/parser/package-info.java
+++ parser-java/addon/src/main/java/org/jboss/forge/parser/package-info.java
@@ -4,14 +4,4 @@
  * Licensed under the Eclipse Public License version 1.0, available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.jboss.forge.projects;
-
-public class Root
-{
-
-   public Root()
-   {
-      // TODO Auto-generated constructor stub
-   }
-
-}
+package org.jboss.forge.parser;
