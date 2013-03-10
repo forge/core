7faa5c4350f7da3b303bdd296fd4ec02889794fb
Ivan Ivanov <ivan.ivanov@sap.com>
2013-03-11 01:07:08 +0200

    Add support for SAP's Lean Java Server container

---
+++ javaee-impl/src/main/java/org/jboss/forge/spec/javaee/jpa/container/LeanJavaServerContainer.java
@@ -0,0 +1,30 @@
+package org.jboss.forge.spec.javaee.jpa.container;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.shell.ShellPrintWriter;
+import org.jboss.forge.spec.javaee.jpa.api.DatabaseType;
+
+public class LeanJavaServerContainer extends JavaEEDefaultContainer {
+
+    private static final String DEFAULT_DATA_SOURCE = "jdbc/DefaultDB";
+    
+    @Inject
+    private ShellPrintWriter writer;
+
+    @Override
+    protected String getDefaultDataSource() {
+        return DEFAULT_DATA_SOURCE;
+    }
+
+    @Override
+    protected DatabaseType getDefaultDatabaseType() {
+        return DatabaseType.HSQLDB;
+    }
+
+    @Override
+    protected ShellPrintWriter getWriter() {
+        return writer;
+    }
+
+}
