754ef62debab9cc570dc20efcc9067cc83b4c4a6
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-03-26 15:25:04 -0400

    Updated OperatingSystemUtils JavaDocs and getSafeFilename()

--- container-api/src/main/java/org/jboss/forge/container/util/OperatingSystemUtils.java
+++ container-api/src/main/java/org/jboss/forge/container/util/OperatingSystemUtils.java
@@ -9,6 +9,8 @@ package org.jboss.forge.container.util;
 import java.io.File;
 
 /**
+ * Utility for dealing with the inconsistencies between common operating systems.
+ * 
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  */
 public final class OperatingSystemUtils
@@ -17,6 +19,9 @@ public final class OperatingSystemUtils
 
    private static String operatingSystem = null;
 
+   /**
+    * Return the name of the host operating system.
+    */
    public static String getOsName()
    {
       if (operatingSystem == null)
@@ -26,70 +31,89 @@ public final class OperatingSystemUtils
       return operatingSystem;
    }
 
+   /**
+    * Return <code>true</code> if the host environment is Windows.
+    */
    public static boolean isWindows()
    {
       return PRETEND_WINDOWS || getOsName().startsWith("Windows") || getOsName().startsWith("windows");
    }
 
+   /**
+    * Return <code>true</code> if the host environment is OSX.
+    */
    public static boolean isOSX()
    {
       return getOsName().startsWith("Mac") || getOsName().startsWith("mac");
    }
 
+   /**
+    * Return <code>true</code> if the host environment is Linux.
+    */
    public static boolean isLinux()
    {
       return getOsName().startsWith("Linux") || getOsName().startsWith("linux");
    }
 
+   /**
+    * Get the FORGE_HOME directory as a {@link File}.
+    */
    public static File getForgeHomeDir()
    {
       return new File(System.getProperty("forge.home")).getAbsoluteFile();
    }
 
+   /**
+    * Get the logged in user's home directory as a {@link File}.
+    */
    public static File getUserHomeDir()
    {
       return new File(System.getProperty("user.home")).getAbsoluteFile();
    }
 
+   /**
+    * Get the path of the logged in user's home directory.
+    */
    public static String getUserHomePath()
    {
       return getUserHomeDir().getAbsolutePath();
    }
 
+   /**
+    * Get the logged in user's Forge directory.
+    */
    public static File getUserForgeDir()
    {
       return new File(getUserHomeDir(), ".forge").getAbsoluteFile();
    }
 
+   /**
+    * Set this utility to pretend that the current host environment is actually Windows.
+    */
    public static void setPretendWindows(boolean value)
    {
       PRETEND_WINDOWS = value;
    }
 
+   /**
+    * Return the {@link String} used as line separator for carriage returns.
+    */
    public static String getLineSeparator()
    {
       return System.getProperty("line.separator");
    }
 
    /**
-    * The following characters are invalid as file or folder names on Windows using NTFS:
-    *
-    * / ? < > \ : * | ” and any character you can type with the Ctrl key
-    *
-    * @param filename
-    * @return
+    * Generate a string that is usable as a single file name or directory path segment on any operating system. Replaces
+    * unsafe characters with the underscore '_' character
     */
    public static String getSafeFilename(String filename)
    {
-      String safeFilename;
-      if (isWindows())
+      String result = filename;
+      if (result != null)
       {
-         safeFilename = filename.replaceAll("[/?<>\\\\:*|\"]", "_");
+         result = result.replaceAll("[/?<>\\\\:*|\"]", "_");
       }
-      else
-      {
-         safeFilename = filename;
-      }
-      return safeFilename;
+      return result;
    }
 }
