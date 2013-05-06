f784f6ed1991a96f961ca1a5563522f40e4243e4
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-05-06 19:18:27 -0400

    Test NewProjectWizard with Aesh

---
+++ projects/tests/src/test/java/org/jboss/forge/projects/aesh/NewProjectAeshTest.java
@@ -0,0 +1,93 @@
+package org.jboss.forge.projects.aesh;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.aesh.ForgeShell;
+import org.jboss.forge.aesh.ShellTests;
+import org.jboss.forge.aesh.TestShellConfiguration;
+import org.jboss.forge.arquillian.Addon;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.container.addons.AddonId;
+import org.jboss.forge.container.repositories.AddonDependencyEntry;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+@RunWith(Arquillian.class)
+public class NewProjectAeshTest
+{
+   @Deployment
+   @Dependencies({ @Addon(name = "org.jboss.forge:ui", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:aesh-test-harness", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:aesh", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:maven", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:resources", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge:projects", version = "2.0.0-SNAPSHOT")
+   })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap
+               .create(ForgeArchive.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:ui", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:projects", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:aesh", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:aesh-test-harness", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge:resources", "2.0.0-SNAPSHOT"))
+               );
+
+      return archive;
+   }
+
+   @Inject
+   private ForgeShell shell;
+
+   @Inject
+   private TestShellConfiguration streams;
+
+   @Test
+   public void testContainerInjection() throws Exception
+   {
+      Assert.assertNotNull(shell);
+
+      File target = File.createTempFile("forge", "new-project-aesh");
+      target.delete();
+
+      streams.getStdIn().write(("new-project " +
+               "--named lincoln " +
+               "--topLevelPackage org.lincoln " +
+               "--targetLocation " + target.getAbsolutePath() + " " +
+               "--type \"Maven\" " +
+               "--overwrite " +
+               "--version 1.0.0-SNAPSHOT").getBytes());
+
+      streams.getStdIn().write("\n".getBytes());
+
+      Thread.sleep(100);
+      ShellTests.waitForCallback(streams.getStdOut(), 10, TimeUnit.SECONDS);
+      Thread.sleep(100);
+      ShellTests.waitForCallback(streams.getStdOut(), 10, TimeUnit.SECONDS);
+
+      System.out.println("OUT:" + streams.getStdOut().toString());
+      System.out.println("ERR:" + streams.getStdErr().toString());
+
+      Assert.assertTrue(target.exists());
+      Assert.assertTrue(target.isDirectory());
+      File projectDir = new File(target, "lincoln");
+      Assert.assertTrue(projectDir.exists());
+      Assert.assertTrue(new File(projectDir, "pom.xml").exists());
+
+      shell.stopShell();
+   }
+}
