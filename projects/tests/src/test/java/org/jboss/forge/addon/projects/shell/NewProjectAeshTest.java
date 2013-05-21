d482976d6ba6e66db5ebed01137d8c6386a6ec87
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-05-21 14:03:01 -0400

    Re enable test

--- projects/tests/src/test/java/org/jboss/forge/addon/projects/shell/NewProjectAeshTest.java
+++ projects/tests/src/test/java/org/jboss/forge/addon/projects/shell/NewProjectAeshTest.java
@@ -1,13 +1,13 @@
 package org.jboss.forge.addon.projects.shell;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.forge.addon.shell.ForgeShell;
-import org.jboss.forge.addon.shell.test.TestShellConfiguration;
+import org.jboss.forge.addon.shell.test.ShellTest;
 import org.jboss.forge.arquillian.Addon;
 import org.jboss.forge.arquillian.Dependencies;
 import org.jboss.forge.arquillian.archive.ForgeArchive;
@@ -22,14 +22,12 @@ import org.junit.runner.RunWith;
 /**
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  */
-@Ignore("Problematic test. Needs digging.")
 @RunWith(Arquillian.class)
 public class NewProjectAeshTest
 {
    @Deployment
    @Dependencies({ @Addon(name = "org.jboss.forge.addon:ui", version = "2.0.0-SNAPSHOT"),
             @Addon(name = "org.jboss.forge.addon:shell-test-harness", version = "2.0.0-SNAPSHOT"),
-            @Addon(name = "org.jboss.forge.addon:shell", version = "2.0.0-SNAPSHOT"),
             @Addon(name = "org.jboss.forge.addon:maven", version = "2.0.0-SNAPSHOT"),
             @Addon(name = "org.jboss.forge.addon:resources", version = "2.0.0-SNAPSHOT"),
             @Addon(name = "org.jboss.forge.addon:projects", version = "2.0.0-SNAPSHOT")
@@ -42,8 +40,8 @@ public class NewProjectAeshTest
                .addAsAddonDependencies(
                         AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:ui", "2.0.0-SNAPSHOT")),
                         AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:projects", "2.0.0-SNAPSHOT")),
-                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:shell", "2.0.0-SNAPSHOT")),
-                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:shell-test-harness", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:shell-test-harness",
+                                 "2.0.0-SNAPSHOT")),
                         AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:resources", "2.0.0-SNAPSHOT"))
                );
 
@@ -51,40 +49,26 @@ public class NewProjectAeshTest
    }
 
    @Inject
-   private ForgeShell shell;
-
-   @Inject
-   private TestShellConfiguration streams;
+   private ShellTest test;
 
    @Test
    public void testContainerInjection() throws Exception
    {
-      Assert.assertNotNull(shell);
-
       File target = File.createTempFile("forge", "new-project-aesh");
       target.delete();
 
-      streams.getStdIn().write(("new-project " +
+      test.execute(("new-project " +
                "--named lincoln " +
                "--topLevelPackage org.lincoln " +
                "--targetLocation " + target.getAbsolutePath() + " " +
                "--type \"Maven\" " +
                "--overwrite " +
-               "--version 1.0.0-SNAPSHOT").getBytes());
-
-      streams.getStdIn().write("\n".getBytes());
-
-      Thread.sleep(500); // TODO Need some form of synchronous waiting here.
-
-      System.out.println("OUT:" + streams.getStdOut().toString());
-      System.out.println("ERR:" + streams.getStdErr().toString());
+               "--version 1.0.0-SNAPSHOT"), 30, TimeUnit.SECONDS);
 
       Assert.assertTrue(target.exists());
       Assert.assertTrue(target.isDirectory());
       File projectDir = new File(target, "lincoln");
       Assert.assertTrue(projectDir.exists());
       Assert.assertTrue(new File(projectDir, "pom.xml").exists());
-
-      shell.stopShell();
    }
 }
