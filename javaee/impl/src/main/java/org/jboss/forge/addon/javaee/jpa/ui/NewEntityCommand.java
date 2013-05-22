9a9565c07c7ec92f7a598e1636ba6e3c31568c13
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-05-21 20:03:15 -0400

    Moved

--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/ui/NewEntityCommand.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/ui/NewEntityCommand.java
@@ -4,7 +4,7 @@
  * Licensed under the Eclipse Public License version 1.0, available at
  * http://www.eclipse.org/legal/epl-v10.html
  */
-package org.jboss.forge.addon.javaee.jpa;
+package org.jboss.forge.addon.javaee.jpa.ui;
 
 import java.util.Arrays;
 
