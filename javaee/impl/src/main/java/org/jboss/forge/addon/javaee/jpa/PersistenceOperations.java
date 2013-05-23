97d64bb7230692f81fcbac061f35f299cdd38ac9
George Gastaldi <gegastaldi@gmail.com>
2013-05-23 16:55:14 -0300

    NewEntityCommand is now functional

--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/PersistenceOperations.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/PersistenceOperations.java
@@ -7,32 +7,53 @@
 
 package org.jboss.forge.addon.javaee.jpa;
 
+import java.io.FileNotFoundException;
+import java.io.Serializable;
+
 import javax.inject.Inject;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
 
 import org.jboss.forge.addon.facets.FacetFactory;
 import org.jboss.forge.addon.javaee.facets.PersistenceFacet;
 import org.jboss.forge.addon.javaee.facets.PersistenceMetaModelFacet;
+import org.jboss.forge.addon.parser.java.JavaSourceFactory;
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
 import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFactory;
 import org.jboss.forge.addon.resource.DirectoryResource;
-import org.jboss.forge.parser.java.JavaSource;
+import org.jboss.forge.furnace.util.Strings;
+import org.jboss.forge.parser.java.Field;
+import org.jboss.forge.parser.java.JavaClass;
+import org.jboss.forge.parser.java.util.Refactory;
 import org.jboss.shrinkwrap.descriptor.api.persistence20.PersistenceDescriptor;
 import org.jboss.shrinkwrap.descriptor.api.persistence20.PersistenceUnit;
 import org.jboss.shrinkwrap.descriptor.api.persistence20.PersistenceUnitTransactionType;
 
+/**
+ * This class contains JPA specific operations
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ * 
+ */
 public class PersistenceOperations
 {
-   public static final String DEFAULT_UNIT_NAME = "forge-default";
+   private static final String DEFAULT_UNIT_NAME = "forge-default";
    private static final String DEFAULT_UNIT_DESC = "Forge Persistence Unit";
 
+   @Inject
+   private ProjectFactory projectFactory;
+
+   @Inject
    private FacetFactory facetFactory;
 
    @Inject
-   public PersistenceOperations(FacetFactory facetFactory)
-   {
-      super();
-      this.facetFactory = facetFactory;
-   }
+   private JavaSourceFactory javaSourceFactory;
 
    /**
     * Setups JPA in the project
@@ -45,11 +66,7 @@ public class PersistenceOperations
    {
       if (project != null)
       {
-         if (!project.hasFacet(PersistenceFacet.class))
-         {
-            facetFactory.install(PersistenceFacet.class, project);
-         }
-         PersistenceFacet facet = project.getFacet(PersistenceFacet.class);
+         PersistenceFacet facet = facetFactory.install(PersistenceFacet.class, project);
          PersistenceContainer container = dataSource.getContainer();
          PersistenceProvider provider = dataSource.getProvider();
          PersistenceDescriptor config = facet.getConfig();
@@ -69,10 +86,85 @@ public class PersistenceOperations
       }
    }
 
-   public JavaSource<?> newEntity(String entityName, String targetPackage, GenerationType strategy,
-            DirectoryResource targetDirectory)
+   /**
+    * Creates a new {@link JavaResource} in the specified project. If no project is available, use
+    * {@link PersistenceOperations#newEntity(DirectoryResource, String, String, GenerationType)}
+    * 
+    * @param project the current project to create the entity. Must not be null
+    * @param entityName the name of the entity
+    * @param entityPackage the package of the entity to be created
+    * @param idStrategy the ID strategy chosen for this entity
+    * @return the created java resource
+    * @throws FileNotFoundException if something wrong happens while saving the {@link JavaClass}
+    */
+   public JavaResource newEntity(Project project, String entityName, String entityPackage, GenerationType idStrategy)
+            throws FileNotFoundException
    {
-      return null;
+      final JavaSourceFacet java = facetFactory.install(JavaSourceFacet.class, project);
+      JavaClass javaClass = createJavaClass(entityName, entityPackage, idStrategy);
+      return java.saveJavaSource(javaClass);
    }
 
+   /**
+    * Creates a new {@link JavaResource} in the specified target. If a project is available, use
+    * {@link PersistenceOperations#newEntity(Project, String, String, GenerationType)}
+    * 
+    * @param target the target directory resource to create this class
+    * @param entityName the name of the entity
+    * @param entityPackage the package of the entity to be created
+    * @param idStrategy the ID strategy chosen for this entity
+    * @return the created java resource
+    * @throws FileNotFoundException if something wrong happens while saving the {@link JavaClass}
+    */
+   public JavaResource newEntity(DirectoryResource target, String entityName, String entityPackage,
+            GenerationType idStrategy)
+   {
+      JavaClass javaClass = createJavaClass(entityName, entityPackage, idStrategy);
+      String pkg = Strings.isNullOrEmpty(javaClass.getPackage()) ? "" : javaClass.getPackage() + ".";
+      String className = pkg + javaClass.getName();
+      JavaResource javaResource = getJavaResource(target, className);
+      javaResource.setContents(javaClass);
+      return javaResource;
+   }
+
+   @SuppressWarnings("unchecked")
+   private JavaClass createJavaClass(String entityName, String entityPackage, GenerationType idStrategy)
+   {
+      JavaClass javaClass = javaSourceFactory.create(JavaClass.class)
+               .setName(entityName)
+               .setPublic()
+               .addAnnotation(Entity.class).getOrigin()
+               .addInterface(Serializable.class);
+      if (entityPackage != null && !entityPackage.isEmpty())
+      {
+         javaClass.setPackage(entityPackage);
+      }
+      Field<JavaClass> id = javaClass.addField("private Long id = null;");
+      id.addAnnotation(Id.class);
+      id.addAnnotation(GeneratedValue.class)
+               .setEnumValue("strategy", idStrategy);
+      id.addAnnotation(Column.class)
+               .setStringValue("name", "id")
+               .setLiteralValue("updatable", "false")
+               .setLiteralValue("nullable", "false");
+
+      Field<JavaClass> version = javaClass.addField("private int version = 0;");
+      version.addAnnotation(Version.class);
+      version.addAnnotation(Column.class).setStringValue("name", "version");
+
+      Refactory.createGetterAndSetter(javaClass, id);
+      Refactory.createGetterAndSetter(javaClass, version);
+      Refactory.createToStringFromFields(javaClass, id);
+      Refactory.createHashCodeAndEquals(javaClass);
+      return javaClass;
+   }
+
+   private JavaResource getJavaResource(final DirectoryResource sourceDir, final String relativePath)
+   {
+      String path = relativePath.trim().endsWith(".java")
+               ? relativePath.substring(0, relativePath.lastIndexOf(".java")) : relativePath;
+      path = path.replace(".", "/") + ".java";
+      JavaResource target = sourceDir.getChildOfType(JavaResource.class, path);
+      return target;
+   }
 }
--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/ui/NewEntityCommand.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/ui/NewEntityCommand.java
@@ -9,9 +9,17 @@ package org.jboss.forge.addon.javaee.jpa.ui;
 import javax.inject.Inject;
 import javax.persistence.GenerationType;
 
+import org.jboss.forge.addon.javaee.jpa.PersistenceOperations;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFactory;
+import org.jboss.forge.addon.resource.DirectoryResource;
+import org.jboss.forge.addon.resource.FileResource;
+import org.jboss.forge.addon.resource.Resource;
 import org.jboss.forge.addon.ui.UICommand;
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UISelection;
 import org.jboss.forge.addon.ui.context.UIValidationContext;
 import org.jboss.forge.addon.ui.facets.HintsFacet;
 import org.jboss.forge.addon.ui.hints.InputTypes;
@@ -20,6 +28,7 @@ import org.jboss.forge.addon.ui.input.UISelectOne;
 import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.metadata.WithAttributes;
 import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
 import org.jboss.forge.addon.ui.util.Metadata;
 
 /**
@@ -32,12 +41,22 @@ public class NewEntityCommand implements UICommand
    private UIInput<String> named;
 
    @Inject
-   @WithAttributes(label = "Target package", required = true)
+   @WithAttributes(label = "Target package")
    private UIInput<String> targetPackage;
 
    @Inject
    @WithAttributes(label = "ID Column Generation Strategy", required = true)
    private UISelectOne<GenerationType> idStrategy;
+
+   @Inject
+   @WithAttributes(label = "Target Directory", required = true)
+   private UIInput<DirectoryResource> targetLocation;
+
+   @Inject
+   private ProjectFactory projectFactory;
+
+   @Inject
+   private PersistenceOperations persistenceOperations;
 
    @Override
    public UICommandMetadata getMetadata()
@@ -51,6 +70,23 @@ public class NewEntityCommand implements UICommand
       targetPackage.getFacet(HintsFacet.class).setInputType(InputTypes.JAVA_PACKAGE_PICKER);
       idStrategy.setDefaultValue(GenerationType.AUTO);
       builder.add(named).add(targetPackage).add(idStrategy);
+      if (getSelectedProject(builder.getUIContext()) == null)
+      {
+         UISelection<Resource<?>> currentSelection = builder.getUIContext().getInitialSelection();
+         if (currentSelection != null)
+         {
+            Resource<?> resource = currentSelection.get();
+            if (resource instanceof DirectoryResource)
+            {
+               targetLocation.setDefaultValue((DirectoryResource) resource);
+            }
+            else if (resource instanceof FileResource)
+            {
+               targetLocation.setDefaultValue(((FileResource<?>) resource).getParent());
+            }
+         }
+         builder.add(targetLocation);
+      }
    }
 
    @Override
@@ -61,14 +97,43 @@ public class NewEntityCommand implements UICommand
    @Override
    public Result execute(UIContext context) throws Exception
    {
-      
-      return null;
+      String entityName = named.getValue();
+      String entityPackage = targetPackage.getValue();
+      GenerationType idStrategyChosen = idStrategy.getValue();
+      DirectoryResource targetDir = targetLocation.getValue();
+      Project project = getSelectedProject(context);
+      JavaResource javaResource;
+      if (project == null)
+      {
+         javaResource = persistenceOperations.newEntity(targetDir, entityName, entityPackage, idStrategyChosen);
+      }
+      else
+      {
+         javaResource = persistenceOperations.newEntity(project, entityName, entityPackage, idStrategyChosen);
+      }
+      context.setSelection(javaResource);
+      return Results.success("Entity " + javaResource + " created");
    }
 
    @Override
    public boolean isEnabled(UIContext context)
    {
       return true;
+   }
+
+   /**
+    * Returns the selected project. null if no project is found
+    */
+   protected Project getSelectedProject(UIContext context)
+   {
+      UISelection<Resource<?>> initialSelection = context.getInitialSelection();
+      Resource<?> resource = initialSelection.get();
+      Project project = null;
+      if (resource instanceof DirectoryResource)
+      {
+         project = projectFactory.findProject((DirectoryResource) resource);
+      }
+      return project;
    }
 
 }
