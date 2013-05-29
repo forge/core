47c4a08df196e325d77e83d9ce2c68eab24883c4
George Gastaldi <gegastaldi@gmail.com>
2013-05-29 19:48:19 -0300

    Added JAX-WS Facet

---
+++ javaee/api/src/main/java/org/jboss/forge/addon/javaee/facets/JAXWSFacet.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.javaee.facets;
+
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFacet;
+
+/**
+ * If installed, this {@link Project} supports features from the JAX-WS specification.
+ * 
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+public interface JAXWSFacet extends ProjectFacet
+{
+}
---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jaxws/JAXWSFacetImpl.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.javaee.jaxws;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.dependencies.Dependency;
+import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
+import org.jboss.forge.addon.javaee.AbstractJavaEEFacet;
+import org.jboss.forge.addon.javaee.facets.JAXWSFacet;
+import org.jboss.forge.addon.projects.dependencies.DependencyInstaller;
+
+/**
+ * Implementation of {@link JAXWSFacet}
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ * 
+ */
+public class JAXWSFacetImpl extends AbstractJavaEEFacet implements JAXWSFacet
+{
+   private static final Dependency JBOSS_JAXB_API = DependencyBuilder
+            .create("org.jboss.spec.javax.xml.bind:jboss-jaxb-api_2.2_spec");
+
+   private static final Dependency JBOSS_JAXRPC_API = DependencyBuilder
+            .create("org.jboss.spec.javax.xml.rpc:jboss-jaxrpc-api_1.1_spec");
+
+   private static final Dependency JBOSS_SAAJ_API = DependencyBuilder
+            .create("org.jboss.spec.javax.xml.soap:jboss-saaj-api_1.3_spec");
+
+   private static final Dependency JBOSS_JAXWS_API = DependencyBuilder
+            .create("org.jboss.spec.javax.xml.ws:jboss-jaxws-api_2.2_spec");
+
+   @Inject
+   public JAXWSFacetImpl(DependencyInstaller installer)
+   {
+      super(installer);
+   }
+
+   @Override
+   protected Map<Dependency, List<Dependency>> getRequiredDependencyOptions()
+   {
+      Map<Dependency, List<Dependency>> result = new HashMap<Dependency, List<Dependency>>();
+      result.put(JBOSS_JAXB_API, Arrays.asList(JBOSS_JAXB_API));
+      result.put(JBOSS_JAXRPC_API, Arrays.asList(JBOSS_JAXRPC_API));
+      result.put(JBOSS_SAAJ_API, Arrays.asList(JBOSS_SAAJ_API));
+      result.put(JBOSS_JAXWS_API, Arrays.asList(JBOSS_JAXWS_API));
+      return result;
+   }
+}
