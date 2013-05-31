45a2752d4f8710ee87fb8badfda7f1849858c1ad
George Gastaldi <gegastaldi@gmail.com>
2013-05-31 10:53:49 -0300

    Moved Observer tests to observers package

--- shell/src/test/java/org/jboss/forge/shell/observers/DeprecatedObserverTest.java
+++ shell/src/test/java/org/jboss/forge/shell/observers/DeprecatedObserverTest.java
@@ -5,7 +5,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.jboss.forge.shell.command;
+package org.jboss.forge.shell.observers;
 
 import static org.junit.internal.matchers.StringContains.containsString;
 
