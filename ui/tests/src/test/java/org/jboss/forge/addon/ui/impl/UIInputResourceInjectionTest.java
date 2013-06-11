1407f6779abe173d79c1f18a39cabc2059ce9e2e
George Gastaldi <gegastaldi@gmail.com>
2013-06-10 23:07:52 -0300

    FORGE-876: Added ignored test

---
+++ ui/tests/src/test/java/org/jboss/forge/addon/ui/impl/UIInputResourceInjectionTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.ui.impl;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.addon.resource.FileResource;
+import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.arquillian.Addon;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.addons.AddonId;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@Ignore("FORGE-876")
+public class UIInputResourceInjectionTest
+{
+   @Deployment
+   @Dependencies({
+            @Addon(name = "org.jboss.forge.addon:ui", version = "2.0.0-SNAPSHOT"),
+            @Addon(name = "org.jboss.forge.addon:resources", version = "2.0.0-SNAPSHOT") })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap
+               .create(ForgeArchive.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:ui", "2.0.0-SNAPSHOT")),
+                        AddonDependencyEntry.create(AddonId.from("org.jboss.forge.addon:resources", "2.0.0-SNAPSHOT")));
+
+      return archive;
+   }
+
+   @Inject
+   UIInput<FileResource<?>> firstName;
+
+   @Test
+   public void testInjectionNotNull()
+   {
+      Assert.assertNotNull(firstName);
+   }
+
+   @Test
+   public void testInputValues()
+   {
+      Assert.assertEquals("firstName", firstName.getName());
+      Assert.assertEquals(FileResource.class, firstName.getValueType());
+   }
+
+}
