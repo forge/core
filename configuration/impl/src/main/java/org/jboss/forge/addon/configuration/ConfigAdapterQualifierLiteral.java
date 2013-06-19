3e952445de98e246781b32dc6b5fc805c50190d2
George Gastaldi <gegastaldi@gmail.com>
2013-06-19 13:39:11 -0300

    Added partial impl for configuration addon

---
+++ configuration/impl/src/main/java/org/jboss/forge/addon/configuration/ConfigAdapterQualifierLiteral.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.configuration;
+
+import javax.enterprise.util.AnnotationLiteral;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
+public class ConfigAdapterQualifierLiteral extends AnnotationLiteral<ConfigAdapterQualifier>
+{
+   private static final long serialVersionUID = 6887822398324205947L;
+}
---
+++ configuration/impl/src/main/java/org/jboss/forge/addon/configuration/ConfigurationAdapter.java
@@ -0,0 +1,348 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.configuration;
+
+import java.lang.annotation.Annotation;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
+@Dependent
+@ConfigAdapterQualifier
+public class ConfigurationAdapter implements Configuration
+{
+   private ScopedConfigurationAdapter parent;
+   private org.apache.commons.configuration.Configuration delegate;
+   private BeanManager bm;
+
+   public ConfigurationAdapter()
+   {
+   }
+
+   public ConfigurationAdapter(final ScopedConfigurationAdapter parent,
+            final org.apache.commons.configuration.Configuration delegate)
+   {
+      this.parent = parent;
+      this.delegate = delegate;
+   }
+
+   public ConfigurationAdapter(org.apache.commons.configuration.Configuration delegate)
+   {
+      this.parent = null;
+      this.delegate = delegate;
+   }
+
+   public org.apache.commons.configuration.Configuration getDelegate()
+   {
+      return delegate;
+   }
+
+   public void setParent(ScopedConfigurationAdapter parent)
+   {
+      this.parent = parent;
+   }
+
+   public void setDelegate(org.apache.commons.configuration.Configuration delegate)
+   {
+      this.delegate = delegate;
+   }
+
+   public void setBeanManager(BeanManager bm)
+   {
+      this.bm = bm;
+   }
+
+   @Override
+   public Configuration getScopedConfiguration(final ConfigurationScope scope)
+   {
+      if (parent == null)
+      {
+         throw new IllegalArgumentException("No Configuration registered to scope type: " + scope);
+      }
+      return parent.getScopedConfiguration(scope);
+   }
+
+   @Override
+   public Configuration subset(final String prefix)
+   {
+      ConfigurationAdapter adapter = getContextualInstance(bm, ConfigurationAdapter.class,
+               new ConfigAdapterQualifierLiteral());
+      adapter.setParent(parent);
+      adapter.setDelegate(delegate.subset(prefix));
+      adapter.setBeanManager(bm);
+      return adapter;
+   }
+
+   @SuppressWarnings("unchecked")
+   private static <T> T getContextualInstance(final BeanManager manager, final Class<T> type, Annotation... qualifiers)
+   {
+      T result = null;
+      Bean<T> bean = (Bean<T>) manager.resolve(manager.getBeans(type, qualifiers));
+      if (bean != null)
+      {
+         CreationalContext<T> context = manager.createCreationalContext(bean);
+         if (context != null)
+         {
+            result = (T) manager.getReference(bean, type, context);
+         }
+      }
+      return result;
+   }
+
+   @Override
+   public boolean isEmpty()
+   {
+      return delegate.isEmpty();
+   }
+
+   @Override
+   public boolean containsKey(final String key)
+   {
+      return delegate.containsKey(key);
+   }
+
+   @Override
+   public void addProperty(final String key, final Object value)
+   {
+      delegate.addProperty(key, value);
+   }
+
+   @Override
+   public void setProperty(final String key, final Object value)
+   {
+      delegate.setProperty(key, value);
+   }
+
+   @Override
+   public void clearProperty(final String key)
+   {
+      delegate.clearProperty(key);
+   }
+
+   @Override
+   public void clear()
+   {
+      delegate.clear();
+   }
+
+   @Override
+   public Object getProperty(final String key)
+   {
+      return delegate.getProperty(key);
+   }
+
+   @Override
+   public Iterator<?> getKeys(final String prefix)
+   {
+      return delegate.getKeys(prefix);
+   }
+
+   @Override
+   public Iterator<?> getKeys()
+   {
+      return delegate.getKeys();
+   }
+
+   @Override
+   public Properties getProperties(final String key)
+   {
+      return delegate.getProperties(key);
+   }
+
+   @Override
+   public boolean getBoolean(final String key)
+   {
+      return delegate.getBoolean(key);
+   }
+
+   @Override
+   public boolean getBoolean(final String key, final boolean defaultValue)
+   {
+      return delegate.getBoolean(key, defaultValue);
+   }
+
+   @Override
+   public Boolean getBoolean(final String key, final Boolean defaultValue)
+   {
+      return delegate.getBoolean(key, defaultValue);
+   }
+
+   @Override
+   public byte getByte(final String key)
+   {
+      return delegate.getByte(key);
+   }
+
+   @Override
+   public byte getByte(final String key, final byte defaultValue)
+   {
+      return delegate.getByte(key, defaultValue);
+   }
+
+   @Override
+   public Byte getByte(final String key, final Byte defaultValue)
+   {
+      return delegate.getByte(key, defaultValue);
+   }
+
+   @Override
+   public double getDouble(final String key)
+   {
+      return delegate.getDouble(key);
+   }
+
+   @Override
+   public double getDouble(final String key, final double defaultValue)
+   {
+      return delegate.getDouble(key, defaultValue);
+   }
+
+   @Override
+   public Double getDouble(final String key, final Double defaultValue)
+   {
+      return delegate.getDouble(key, defaultValue);
+   }
+
+   @Override
+   public float getFloat(final String key)
+   {
+      return delegate.getFloat(key);
+   }
+
+   @Override
+   public float getFloat(final String key, final float defaultValue)
+   {
+      return delegate.getFloat(key, defaultValue);
+   }
+
+   @Override
+   public Float getFloat(final String key, final Float defaultValue)
+   {
+      return delegate.getFloat(key, defaultValue);
+   }
+
+   @Override
+   public int getInt(final String key)
+   {
+      return delegate.getInt(key);
+   }
+
+   @Override
+   public int getInt(final String key, final int defaultValue)
+   {
+      return delegate.getInt(key, defaultValue);
+   }
+
+   @Override
+   public Integer getInteger(final String key, final Integer defaultValue)
+   {
+      return delegate.getInteger(key, defaultValue);
+   }
+
+   @Override
+   public long getLong(final String key)
+   {
+      return delegate.getLong(key);
+   }
+
+   @Override
+   public long getLong(final String key, final long defaultValue)
+   {
+      return delegate.getLong(key, defaultValue);
+   }
+
+   @Override
+   public Long getLong(final String key, final Long defaultValue)
+   {
+      return delegate.getLong(key, defaultValue);
+   }
+
+   @Override
+   public short getShort(final String key)
+   {
+      return delegate.getShort(key);
+   }
+
+   @Override
+   public short getShort(final String key, final short defaultValue)
+   {
+      return delegate.getShort(key, defaultValue);
+   }
+
+   @Override
+   public Short getShort(final String key, final Short defaultValue)
+   {
+      return delegate.getShort(key, defaultValue);
+   }
+
+   @Override
+   public BigDecimal getBigDecimal(final String key)
+   {
+      return delegate.getBigDecimal(key);
+   }
+
+   @Override
+   public BigDecimal getBigDecimal(final String key, final BigDecimal defaultValue)
+   {
+      return delegate.getBigDecimal(key, defaultValue);
+   }
+
+   @Override
+   public BigInteger getBigInteger(final String key)
+   {
+      return delegate.getBigInteger(key);
+   }
+
+   @Override
+   public BigInteger getBigInteger(final String key, final BigInteger defaultValue)
+   {
+      return delegate.getBigInteger(key, defaultValue);
+   }
+
+   @Override
+   public String getString(final String key)
+   {
+      return delegate.getString(key);
+   }
+
+   @Override
+   public String getString(final String key, final String defaultValue)
+   {
+      return delegate.getString(key, defaultValue);
+   }
+
+   @Override
+   public String[] getStringArray(final String key)
+   {
+      return delegate.getStringArray(key);
+   }
+
+   @Override
+   public List<?> getList(final String key)
+   {
+      return delegate.getList(key);
+   }
+
+   @SuppressWarnings("unchecked")
+   @Override
+   public List<?> getList(final String key, final List<?> defaultValue)
+   {
+      return delegate.getList(key, (List<Object>) defaultValue);
+   }
+
+}
