5a24c239e0efa1f658da6d37f105e1c7b58e83c4
George Gastaldi <gegastaldi@gmail.com>
2013-07-01 17:53:54 -0300

    FORGE-988: AddonManager refactoring

---
+++ addon-manager/api/src/main/java/org/jboss/forge/addon/manager/request/DisableRequest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.manager.request;
+
+import org.jboss.forge.furnace.addons.AddonId;
+
+/**
+ * This object is responsible for disabling an {@link AddonId}.
+ * 
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+public interface DisableRequest extends AddonActionRequest
+{
+
+}
--- addon-manager/example2/src/main/java/org/example/other/OtherExampleAddon.java
+++
@@ -1,8 +0,0 @@
-package org.example.other;
-
-import org.jboss.forge.furnace.services.Exported;
-
-@Exported
-public class OtherExampleAddon
-{
-}
