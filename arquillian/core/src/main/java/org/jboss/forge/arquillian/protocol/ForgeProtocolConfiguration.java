b12862f528a590bdcb649e4b7ce7849ed56ad6dc
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-07-19 18:14:11 -0400

    Moved Arquillian Adapters and Test Harness to: https://github.com/forge/furnace-test-harness

--- arquillian/core/src/main/java/org/jboss/forge/arquillian/protocol/ForgeProtocolConfiguration.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.forge.arquillian.protocol;
-
-import org.jboss.arquillian.container.test.spi.client.protocol.ProtocolConfiguration;
-
-public class ForgeProtocolConfiguration implements ProtocolConfiguration
-{
-}
