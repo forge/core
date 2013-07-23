4f95844ff81ab1bf3fe1d89682357fc199fa58d4
George Gastaldi <gegastaldi@gmail.com>
2013-07-23 12:01:29 -0300

    Ported test from container-cdi to addon-manager-tests

---
+++ addon-manager/tests/src/test/java/org/jboss/forge/addon/manager/AddonInstancesAreSingletonsTest.java
@@ -0,0 +1,107 @@
+package org.jboss.forge.addon.manager;
+
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.addons.Addon;
+import org.jboss.forge.furnace.addons.AddonId;
+import org.jboss.forge.furnace.addons.AddonRegistry;
+import org.jboss.forge.furnace.manager.AddonManager;
+import org.jboss.forge.furnace.manager.maven.MavenContainer;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.forge.furnace.util.Addons;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * FIXME This test needs to be refactored to be a bit less brittle. It breaks when addon POMs change.
+ * 
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+@RunWith(Arquillian.class)
+public class AddonInstancesAreSingletonsTest
+{
+   @BeforeClass
+   public static void setRemoteRepository() throws IOException
+   {
+      System.setProperty(MavenContainer.ALT_USER_SETTINGS_XML_LOCATION, getAbsolutePath("profiles/settings.xml"));
+      System.setProperty(MavenContainer.ALT_LOCAL_REPOSITORY_LOCATION, "target/the-other-repository");
+   }
+
+   private static String getAbsolutePath(String path) throws FileNotFoundException
+   {
+      URL resource = Thread.currentThread().getContextClassLoader().getResource(path);
+      if (resource == null)
+         throw new FileNotFoundException(path);
+      return resource.getFile();
+   }
+
+   @AfterClass
+   public static void clearRemoteRepository()
+   {
+      System.clearProperty(MavenContainer.ALT_USER_SETTINGS_XML_LOCATION);
+      System.clearProperty(MavenContainer.ALT_LOCAL_REPOSITORY_LOCATION);
+   }
+
+   @Deployment
+   @Dependencies({
+            @AddonDependency(name = "org.jboss.forge.addon:addon-manager", version = "2.0.0-SNAPSHOT"),
+            @AddonDependency(name = "org.jboss.forge.addon:maven", version = "2.0.0-SNAPSHOT")
+   })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap
+               .create(ForgeArchive.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create("org.jboss.forge.furnace:container-cdi", "2.0.0-SNAPSHOT"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:maven", "2.0.0-SNAPSHOT"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:addon-manager", "2.0.0-SNAPSHOT")
+               );
+
+      return archive;
+   }
+
+   @Inject
+   private AddonRegistry registry;
+
+   @Inject
+   private AddonManager addonManager;
+
+   @Test
+   public void testInstallingAddonWithSingleOptionalAddonDependency() throws InterruptedException, TimeoutException
+   {
+      int addonCount = registry.getAddons().size();
+      final AddonId exampleId = AddonId.fromCoordinates("test:no_dep,1.0.0.Final");
+
+      /*
+       * Ensure that the Addon instance we receive is requested before configuration is rescanned.
+       */
+      addonManager.deploy(exampleId).perform();
+      Addon example = registry.getAddon(exampleId);
+      Addons.waitUntilStarted(example, 10, TimeUnit.SECONDS);
+      Assert.assertEquals(addonCount + 1, registry.getAddons().size());
+   }
+
+}
