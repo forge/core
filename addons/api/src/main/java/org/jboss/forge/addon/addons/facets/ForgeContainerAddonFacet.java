0fb4316deb5de9bb37d99025d25cf986fe6c6019
George Gastaldi <gegastaldi@gmail.com>
2013-07-26 11:13:00 -0300

    Added @RequiresFacet(CDIFacet.class) to ForgeContainerAddonFacet

--- addons/api/src/main/java/org/jboss/forge/addon/addons/facets/ForgeContainerAddonFacet.java
+++ addons/api/src/main/java/org/jboss/forge/addon/addons/facets/ForgeContainerAddonFacet.java
@@ -11,6 +11,8 @@ import javax.inject.Inject;
 import org.jboss.forge.addon.dependencies.Dependency;
 import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
 import org.jboss.forge.addon.facets.AbstractFacet;
+import org.jboss.forge.addon.facets.constraints.RequiresFacet;
+import org.jboss.forge.addon.javaee.facets.CDIFacet;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.ProjectFacet;
 import org.jboss.forge.addon.projects.dependencies.DependencyInstaller;
@@ -21,6 +23,7 @@ import org.jboss.forge.addon.projects.dependencies.DependencyInstaller;
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
+@RequiresFacet(CDIFacet.class)
 public class ForgeContainerAddonFacet extends AbstractFacet<Project> implements ProjectFacet
 {
    public static Dependency FORGE_CONTAINER_DEPENDENCY = DependencyBuilder.create()
