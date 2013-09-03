fedb74e1a1e004fe3372eb06e25a139799ce48c2
George Gastaldi <gegastaldi@gmail.com>
2013-09-03 08:35:36 -0300

    Added MockWizards to ui-example

---
+++ ui/example/src/main/java/org/jboss/forge/addon/ui/example/wizards/MockWizardBegin.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.ui.example.wizards;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.ui.AbstractUICommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIValidationContext;
+import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.addon.ui.input.UIInputMany;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.result.NavigationResult;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.addon.ui.wizard.UIWizard;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
+public class MockWizardBegin extends AbstractUICommand implements UIWizard
+{
+   @Inject
+   private UIInput<Boolean> key;
+
+   @Inject
+   @WithAttributes(defaultValue = "false", label = "proceed")
+   private UIInput<Boolean> proceed;
+
+   @Inject
+   private UIInputMany<String> values;
+
+   @Override
+   public Metadata getMetadata()
+   {
+      return Metadata.forCommand(getClass()).name("mockwizard").description("Mock it up");
+   }
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      builder.add(key).add(values).add(proceed);
+   }
+
+   @Override
+   public Result execute(UIContext context) throws Exception
+   {
+      return Results.success("Begin step executed.");
+   }
+
+   @Override
+   public void validate(UIValidationContext validator)
+   {
+      if (!proceed.getValue())
+         validator.addValidationError(proceed, "Must --proceed before continuing.");
+   }
+
+   @Override
+   public NavigationResult next(UIContext context) throws Exception
+   {
+      return Results.navigateTo(MockWizardStep.class);
+   }
+
+}
---
+++ ui/example/src/main/java/org/jboss/forge/addon/ui/example/wizards/MockWizardStep.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.ui.example.wizards;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.ui.AbstractUICommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIValidationContext;
+import org.jboss.forge.addon.ui.input.UISelectMany;
+import org.jboss.forge.addon.ui.input.UISelectOne;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.result.NavigationResult;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.wizard.UIWizardStep;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ */
+public class MockWizardStep extends AbstractUICommand implements UIWizardStep
+{
+   @Inject
+   @WithAttributes(defaultValue = "false", label = "done")
+   private UISelectOne<Boolean> done;
+
+   @Inject
+   private UISelectMany<String> selections;
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      builder.add(selections).add(done);
+   }
+
+   @Override
+   public Result execute(UIContext context) throws Exception
+   {
+      return Results.success("Begin step executed.");
+   }
+
+   @Override
+   public void validate(UIValidationContext validator)
+   {
+      if (!done.getValue())
+         validator.addValidationError(done, "Must be --done before continuing.");
+   }
+
+   @Override
+   public NavigationResult next(UIContext context) throws Exception
+   {
+      return null;
+   }
+
+}
