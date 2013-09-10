9d87a3654583c4e63c7feb485deab69f62dacc92
George Gastaldi <gegastaldi@gmail.com>
2013-09-10 17:06:19 -0300

    FORGE-966: Created test to assert that issue is fixed

---
+++ resources/tests/src/test/java/org/jboss/forge/addon/resource/FileResourceTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.resource;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class FileResourceTest
+{
+   @Deployment
+   @Dependencies({
+            @AddonDependency(name = "org.jboss.forge.addon:facets"),
+            @AddonDependency(name = "org.jboss.forge.addon:resources") })
+   public static ForgeArchive getDeployment()
+   {
+      ForgeArchive archive = ShrinkWrap.create(ForgeArchive.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:facets"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:resources")
+               );
+
+      return archive;
+   }
+
+   @Inject
+   private ResourceFactory resourceFactory;
+
+   @Test
+   @SuppressWarnings("unchecked")
+   public void testDirectoryResourceReifyShouldRetunNullForFiles() throws IOException
+   {
+      File file = File.createTempFile("fileresourcetest", ".tmp");
+      file.deleteOnExit();
+      FileResource<?> fileResource = resourceFactory.create(FileResource.class, file);
+      Assert.assertNotNull(fileResource);
+      Assert.assertNull(fileResource.reify(DirectoryResource.class));
+   }
+}
