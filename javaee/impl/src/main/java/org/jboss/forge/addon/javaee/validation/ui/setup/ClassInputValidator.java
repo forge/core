a52e7ec97e3c59ca02999e0c1877d696354811a0
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2013-09-12 11:17:12 +0530

    Added Validators for Java identifier naming schemes

--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/validation/ui/setup/ClassInputValidator.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/validation/ui/setup/ClassInputValidator.java
@@ -1,13 +1,21 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.jboss.forge.addon.javaee.validation.ui.setup;
 
+import org.jboss.forge.addon.parser.java.utils.ResultType;
+import org.jboss.forge.addon.parser.java.utils.ValidationResult;
+import org.jboss.forge.addon.parser.java.utils.JLSValidator;
 import org.jboss.forge.addon.ui.UIValidator;
 import org.jboss.forge.addon.ui.context.UIValidationContext;
 import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.furnace.util.Strings;
 
 public class ClassInputValidator implements UIValidator
 {
-   private String[] patterns = new String[] { "(?i)(~\\.)?([a-z0-9_]+\\.?)+[a-z0-9_]",
-            "^(?!.*\\b(" + Patterns.JAVA_KEYWORDS + ")\\b.*).*$" };
    private UIInput<String> component;
 
    public ClassInputValidator(UIInput<String> component)
@@ -19,9 +27,13 @@ public class ClassInputValidator implements UIValidator
    public void validate(UIValidationContext context)
    {
       String className = component().getValue();
-      if (className != null && !matchesValidClassName(className))
+      if (!Strings.isNullOrEmpty(className))
       {
-         context.addValidationError(component(), className + " is not a valid class name for " + component.getLabel());
+         ValidationResult result = JLSValidator.validateClassName(className);
+         if (!result.getType().equals(ResultType.INFO))
+         {
+            context.addValidationError(component(), result.getMessage());
+         }
       }
    }
 
@@ -30,20 +42,4 @@ public class ClassInputValidator implements UIValidator
       return component;
    }
 
-   public boolean matchesValidClassName(final String value)
-   {
-      if (value == null)
-      {
-         return false;
-      }
-
-      for (int i = 0; i < patterns.length; i++)
-      {
-         if (!value.matches(patterns[i]))
-         {
-            return false;
-         }
-      }
-      return true;
-   }
 }
