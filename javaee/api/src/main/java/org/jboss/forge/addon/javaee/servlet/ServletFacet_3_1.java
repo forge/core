3e037f011708e5e64596314ef1b428d8c0dad92f
George Gastaldi <gegastaldi@gmail.com>
2013-09-24 17:44:03 -0300

    Added support for Servlet 3.1

---
+++ javaee/api/src/main/java/org/jboss/forge/addon/javaee/servlet/ServletFacet_3_1.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.servlet;
+
+import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.facets.constraints.FacetConstraintType;
+import org.jboss.forge.addon.facets.constraints.FacetConstraints;
+import org.jboss.forge.addon.projects.facets.WebResourcesFacet;
+import org.jboss.shrinkwrap.descriptor.api.webapp31.WebAppDescriptor;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+@FacetConstraints({
+         @FacetConstraint(value = WebResourcesFacet.class, type = FacetConstraintType.REQUIRED)
+})
+public interface ServletFacet_3_1 extends ServletFacet<WebAppDescriptor>
+{
+
+}
---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/servlet/ServletFacetImpl_3_1.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.servlet;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.dependencies.Dependency;
+import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
+import org.jboss.forge.addon.javaee.Descriptors;
+import org.jboss.forge.addon.projects.dependencies.DependencyInstaller;
+import org.jboss.forge.addon.projects.facets.MetadataFacet;
+import org.jboss.forge.addon.resource.FileResource;
+import org.jboss.forge.furnace.util.Streams;
+import org.jboss.forge.furnace.versions.SingleVersion;
+import org.jboss.forge.furnace.versions.Version;
+import org.jboss.shrinkwrap.descriptor.api.DescriptorImporter;
+import org.jboss.shrinkwrap.descriptor.api.webapp31.WebAppDescriptor;
+
+public class ServletFacetImpl_3_1 extends AbstractServletFacet<WebAppDescriptor> implements ServletFacet_3_1
+{
+   private static final Dependency JAVAX_SERVLET_API = DependencyBuilder
+            .create("javax.servlet:javax.servlet-api:3.1.0");
+
+   @Inject
+   public ServletFacetImpl_3_1(DependencyInstaller installer)
+   {
+      super(installer);
+   }
+
+   @Override
+   public Version getSpecVersion()
+   {
+      return new SingleVersion("3.1");
+   }
+
+   @Override
+   protected Map<Dependency, List<Dependency>> getRequiredDependencyOptions()
+   {
+      Map<Dependency, List<Dependency>> map = new LinkedHashMap<Dependency, List<Dependency>>();
+      map.put(JAVAX_SERVLET_API, Arrays.asList(JAVAX_SERVLET_API));
+      return map;
+   }
+
+   /*
+    * Facet Methods
+    */
+   @Override
+   public WebAppDescriptor getConfig()
+   {
+      FileResource<?> configFile = getConfigFile();
+      WebAppDescriptor descriptor;
+      if (configFile.exists())
+      {
+
+         DescriptorImporter<WebAppDescriptor> importer = Descriptors.importAs(WebAppDescriptor.class);
+         InputStream inputStream = configFile.getResourceInputStream();
+         try
+         {
+            descriptor = importer.fromStream(inputStream);
+         }
+         finally
+         {
+            Streams.closeQuietly(inputStream);
+         }
+      }
+      else
+      {
+         descriptor = Descriptors.create(WebAppDescriptor.class);
+         String projectName = getFaceted().getFacet(MetadataFacet.class).getProjectName();
+         WebAppDescriptor unit = descriptor
+                  .version("3.1")
+                  .displayName(projectName)
+                  .createSessionConfig()
+                  .sessionTimeout(30).up();
+         // FORGE-657
+         unit.createMimeMapping().extension("ico").mimeType("image/x-icon");
+      }
+      return descriptor;
+   }
+
+   @Override
+   public void saveConfig(final WebAppDescriptor descriptor)
+   {
+      FileResource<?> configFile = getConfigFile();
+      String output = descriptor.exportAsString();
+      configFile.setContents(output);
+   }
+}
