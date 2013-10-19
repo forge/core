8e0585124d5fb2c7cd617285ec1b76ac5d418e47
George Gastaldi <gegastaldi@gmail.com>
2013-10-19 03:59:16 -0300

    Removed ambiguous commands

--- shell/impl/src/main/java/org/jboss/forge/addon/shell/commands/ExitCommand.java
+++ shell/impl/src/main/java/org/jboss/forge/addon/shell/commands/ExitCommand.java
@@ -1,9 +1,42 @@
 package org.jboss.forge.addon.shell.commands;
 
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.shell.ui.AbstractShellCommand;
+import org.jboss.forge.addon.shell.ui.ShellContext;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.furnace.Furnace;
 
 /**
  * @author <a href="mailto:stale.pedersen@jboss.org">Ståle W. Pedersen</a>
  */
-public class ExitCommand extends AbstractExitCommand
+public class ExitCommand extends AbstractShellCommand
 {
+
+   @Inject
+   private Furnace forge;
+
+   @Override
+   public UICommandMetadata getMetadata(UIContext context)
+   {
+      return Metadata.from(super.getMetadata(context), getClass()).name("exit").description("Exit the shell");
+   }
+
+   @Override
+   public void initializeUI(UIBuilder context) throws Exception
+   {
+   }
+
+   @Override
+   public Result execute(ShellContext context) throws Exception
+   {
+      context.getProvider().getConsole().stop();
+      forge.stop();
+      return Results.success();
+   }
 }
