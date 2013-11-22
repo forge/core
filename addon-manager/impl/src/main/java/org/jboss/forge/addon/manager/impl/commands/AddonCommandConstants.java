8353da6eeaebe13ec318b483b6e9c5bfe8ce0ebd
George Gastaldi <gegastaldi@gmail.com>
2013-11-22 19:03:51 -0200

    FORGE-1328: Renamed command from addon-install-build to addon-build-and-install

--- addon-manager/impl/src/main/java/org/jboss/forge/addon/manager/impl/commands/AddonCommandConstants.java
+++ addon-manager/impl/src/main/java/org/jboss/forge/addon/manager/impl/commands/AddonCommandConstants.java
@@ -6,7 +6,7 @@ public interface AddonCommandConstants
    String ADDON_INSTALL_COMMAND_NAME = "Install an Addon";
    String ADDON_INSTALL_COMMAND_NAME_NO_GUI = "addon-install";
    String ADDON_BUILD_INSTALL_COMMAND_NAME = "Build and Install an Addon";
-   String ADDON_BUILD_INSTALL_COMMAND_NAME_NO_GUI= "addon-install-build";
+   String ADDON_BUILD_INSTALL_COMMAND_NAME_NO_GUI= "addon-build-and-install";
    String ADDON_INSTALL_COMMAND_DESCRIPTION = "Command to install a Furnace 2 addon.";
    String ADDON_BUILD_INSTALL_COMMAND_DESCRIPTION = "Command to build and install a Furnace 2 addon.";
    String ADDON_REMOVE_COMMAND_NAME = "Remove an Addon";
