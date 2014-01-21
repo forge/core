bffedd74a7ea214e04cd32268e0c3d2e6fe90d9a
George Gastaldi <gegastaldi@gmail.com>
2014-01-21 15:27:06 -0200

    Another attempt to fix teamcity build

--- text/src/test/java/org/jboss/forge/addon/text/InflectorServiceTest.java
+++ text/src/test/java/org/jboss/forge/addon/text/InflectorServiceTest.java
@@ -25,7 +25,7 @@ public class InflectorServiceTest
    @Deployment
    @Dependencies({
             // FIXME remove hard-coded version
-            @AddonDependency(name = "org.jboss.forge.addon:text", version = "2.0.0-SNAPSHOT"),
+            @AddonDependency(name = "org.jboss.forge.addon:text", version = "2.0.0.CR1"),
             @AddonDependency(name = "org.jboss.forge.furnace.container:cdi") })
    public static ForgeArchive getDeployment()
    {
