b68418afb76036cf346e36098b3455309c422711
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2014-01-23 16:40:46 -0500

    Dependencies addon now produces a converter for use in the UIInput<Dependency> fields, and optionally depends on UI addon

---
+++ dependencies/api/src/main/java/org/jboss/forge/addon/dependencies/convert/DependencyConverter.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.dependencies.convert;
+
+import org.jboss.forge.addon.convert.Converter;
+import org.jboss.forge.addon.dependencies.Dependency;
+import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
+
+/**
+ * Implementation of {@link UICompleter} for {@link Dependency} instances.
+ * 
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+public class DependencyConverter implements Converter<String, Dependency>
+{
+   @Override
+   public Dependency convert(String source)
+   {
+      return DependencyBuilder.create(source);
+   }
+}
---
+++ dependencies/impl/src/main/java/org/jboss/forge/addon/dependencies/convert/DependencyConverterGenerator.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.dependencies.convert;
+
+import org.jboss.forge.addon.convert.Converter;
+import org.jboss.forge.addon.convert.ConverterGenerator;
+import org.jboss.forge.addon.dependencies.Dependency;
+import org.jboss.forge.furnace.container.simple.Service;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+public class DependencyConverterGenerator implements ConverterGenerator, Service
+{
+   @Override
+   public boolean handles(Class<?> source, Class<?> target)
+   {
+      return Dependency.class.isAssignableFrom(target) && String.class.isAssignableFrom(source);
+   }
+
+   @Override
+   public Converter<?, ?> generateConverter(Class<?> source, Class<?> target)
+   {
+      return new DependencyConverter();
+   }
+
+   @Override
+   public Class<? extends Converter<?, ?>> getConverterType()
+   {
+      return DependencyConverter.class;
+   }
+}
