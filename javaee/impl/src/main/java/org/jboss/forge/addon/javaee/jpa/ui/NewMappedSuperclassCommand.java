625abfadd5e032f48a798e5739c2fc77fbb673d9
George Gastaldi <gegastaldi@gmail.com>
2014-01-24 16:31:40 -0200

    FORGE-1477: added jpa-new-mapped-superclass

---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/ui/NewMappedSuperclassCommand.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.jpa.ui;
+
+import javax.persistence.MappedSuperclass;
+
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.parser.java.ui.AbstractJavaSourceCommand;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.util.Categories;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.parser.java.JavaClass;
+import org.jboss.forge.parser.java.JavaSource;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class NewMappedSuperclassCommand extends AbstractJavaSourceCommand {
+
+    @Override
+    protected String getType() {
+        return "Mapped Superclass";
+    }
+
+    @Override
+    protected Class<? extends JavaSource<?>> getSourceType() {
+        return JavaClass.class;
+    }
+
+    @Override
+    public UICommandMetadata getMetadata(UIContext context) {
+        return Metadata.forCommand(getClass()).name("JPA: New " + getType())
+            .description("Creates a new " + getType())
+            .category(Categories.create("JPA"));
+    }
+
+    @Override
+    public Result execute(UIExecutionContext context) throws Exception {
+        Result result = super.execute(context);
+        UIContext uiContext = context.getUIContext();
+        Project project = getSelectedProject(uiContext);
+        JavaSourceFacet javaSourceFacet = project.getFacet(JavaSourceFacet.class);
+        JavaResource javaResource = context.getUIContext().getSelection();
+        JavaSource<?> javaSource = javaResource.getJavaSource();
+        javaSource.addAnnotation(MappedSuperclass.class);
+        javaResource = javaSourceFacet.saveJavaSource(javaSource);
+        uiContext.setSelection(javaResource);
+        return result;
+    }
+}
---
+++ javaee/tests/src/test/java/org/jboss/forge/addon/javaee/jpa/ui/NewMappedSuperclassCommandTest.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.jpa.ui;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+import javax.inject.Inject;
+import javax.persistence.MappedSuperclass;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.addon.facets.FacetFactory;
+import org.jboss.forge.addon.javaee.ProjectHelper;
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.ui.controller.CommandController;
+import org.jboss.forge.addon.ui.result.Failed;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.forge.parser.java.JavaClass;
+import org.jboss.forge.ui.test.UITestHarness;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class NewMappedSuperclassCommandTest
+{
+    @Deployment
+    @Dependencies({
+        @AddonDependency(name = "org.jboss.forge.addon:ui"),
+        @AddonDependency(name = "org.jboss.forge.addon:ui-test-harness"),
+        @AddonDependency(name = "org.jboss.forge.addon:javaee"),
+        @AddonDependency(name = "org.jboss.forge.addon:maven")
+    })
+    public static ForgeArchive getDeployment()
+    {
+        return ShrinkWrap
+            .create(ForgeArchive.class)
+            .addClass(ProjectHelper.class)
+            .addBeansXML()
+            .addAsAddonDependencies(
+                AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi"),
+                AddonDependencyEntry.create("org.jboss.forge.addon:projects"),
+                AddonDependencyEntry.create("org.jboss.forge.addon:javaee"),
+                AddonDependencyEntry.create("org.jboss.forge.addon:maven"),
+                AddonDependencyEntry.create("org.jboss.forge.addon:ui"),
+                AddonDependencyEntry.create("org.jboss.forge.addon:ui-test-harness")
+            );
+    }
+
+    @Inject
+    private UITestHarness testHarness;
+
+    @Inject
+    private ProjectHelper projectHelper;
+
+    @Inject
+    private FacetFactory facetFactory;
+
+    @Test
+    public void testCreateMappedSuperclass() throws Exception
+    {
+        Project project = projectHelper.createJavaLibraryProject();
+        facetFactory.install(project, JavaSourceFacet.class);
+        CommandController controller = testHarness.createCommandController(NewMappedSuperclassCommand.class,
+            project.getProjectRoot());
+        controller.initialize();
+        controller.setValueFor("named", "CreditCardType");
+        controller.setValueFor("targetPackage", "org.jboss.forge.test");
+        Assert.assertTrue(controller.isValid());
+        Assert.assertTrue(controller.canExecute());
+        Result result = controller.execute();
+        Assert.assertThat(result, is(not(instanceOf(Failed.class))));
+
+        JavaSourceFacet facet = project.getFacet(JavaSourceFacet.class);
+        JavaResource javaResource = facet.getJavaResource("org.jboss.forge.test.CreditCardType");
+        Assert.assertNotNull(javaResource);
+        Assert.assertThat(javaResource.getJavaSource(), is(instanceOf(JavaClass.class)));
+        Assert.assertTrue(javaResource.getJavaSource().hasAnnotation(MappedSuperclass.class));
+    }
+}
