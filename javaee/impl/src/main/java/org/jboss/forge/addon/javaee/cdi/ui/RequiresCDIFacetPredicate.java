0284a2c7b717a6764e73e81e76a0b7a7759d30d6
George Gastaldi <gegastaldi@gmail.com>
2014-02-03 17:21:09 -0200

    Fixed RequiresCDIFacetPredicate implementation

--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/cdi/ui/RequiresCDIFacetPredicate.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/cdi/ui/RequiresCDIFacetPredicate.java
@@ -7,24 +7,34 @@
 
 package org.jboss.forge.addon.javaee.cdi.ui;
 
+import javax.inject.Inject;
+
 import org.jboss.forge.addon.javaee.cdi.CDIFacet;
-import org.jboss.forge.addon.projects.ui.RequiresProjectPredicate;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFactory;
+import org.jboss.forge.addon.projects.Projects;
 import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.furnace.util.Predicate;
 
 /**
  * Requires a project, to be executed in a non-GUI environment and have the {@link CDIFacet} installed to be enabled
  * 
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
-public class RequiresCDIFacetPredicate extends RequiresProjectPredicate
+public class RequiresCDIFacetPredicate implements Predicate<UIContext>
 {
+
+   @Inject
+   private ProjectFactory projectFactory;
+
    @Override
    public boolean accept(UIContext context)
    {
-      boolean enabled = super.accept(context);
-      if (enabled)
+      boolean enabled = false;
+      Project project = Projects.getSelectedProject(projectFactory, context);
+      if (project != null)
       {
-         enabled = !context.getProvider().isGUI() && getProject(context).hasFacet(CDIFacet.class);
+         enabled = project.hasFacet(CDIFacet.class);
       }
       return enabled;
    }
