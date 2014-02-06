6924957ea994b5bdf6fde9891b08ebe8ad74ac15
George Gastaldi <gegastaldi@gmail.com>
2014-02-06 19:50:49 -0200

    FORGE-1560: Added edit command

---
+++ shell/impl/src/main/java/org/jboss/forge/addon/shell/command/EditCommand.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.shell.command;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.resource.FileResource;
+import org.jboss.forge.addon.resource.Resource;
+import org.jboss.forge.addon.resource.ResourceFactory;
+import org.jboss.forge.addon.resource.util.ResourcePathResolver;
+import org.jboss.forge.addon.shell.ui.AbstractShellCommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.hints.InputType;
+import org.jboss.forge.addon.ui.input.UIInputMany;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Metadata;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class EditCommand extends AbstractShellCommand
+{
+   @Inject
+   ResourceFactory resourceFactory;
+
+   @Inject
+   @WithAttributes(label = "Arguments", type = InputType.FILE_PICKER)
+   private UIInputMany<String> arguments;
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      builder.add(arguments);
+   }
+
+   @Override
+   public UICommandMetadata getMetadata(UIContext context)
+   {
+      return Metadata.from(super.getMetadata(context), getClass()).name("edit")
+               .description("Edit files with the default system editor");
+   }
+
+   @Override
+   public Result execute(UIExecutionContext context) throws Exception
+   {
+      Resource<?> currentResource = (Resource<?>) context.getUIContext().getInitialSelection().get();
+      Iterable<String> value = arguments.getValue();
+      Iterator<String> it = value == null ? Collections.<String> emptyIterator() : value.iterator();
+      final Result result;
+      if (it.hasNext())
+      {
+         String newPath = it.next();
+         final List<Resource<?>> newResource = new ResourcePathResolver(resourceFactory, currentResource, newPath)
+                  .resolve();
+         if (newResource.isEmpty() || !newResource.get(0).exists())
+         {
+            result = Results.fail(newPath + ": No such file or directory");
+         }
+         else
+         {
+            for (Resource<?> resource : newResource)
+            {
+               editResource(resource);
+            }
+            result = Results.success();
+         }
+      }
+      else if (currentResource != null)
+      {
+         editResource(currentResource);
+         result = Results.success();
+      }
+      else
+      {
+         result = Results.fail("Resource not found");
+      }
+      return result;
+   }
+
+   private void editResource(Resource<?> resource) throws IOException
+   {
+      Desktop dt = Desktop.getDesktop();
+      FileResource<?> fileResource = resource.reify(FileResource.class);
+      if (fileResource != null)
+      {
+         dt.edit(fileResource.getUnderlyingResourceObject());
+      }
+   }
+
+}
