1c5db91efbe71341a603912cd15a9b1d0d2db906
Adam Wyłuda <adamwyl92@gmail.com>
2014-02-08 15:36:12 +0100

    ProjectFactoryImpl: Remove unused imports

--- projects/impl/src/main/java/org/jboss/forge/addon/projects/impl/ProjectFactoryImpl.java
+++ projects/impl/src/main/java/org/jboss/forge/addon/projects/impl/ProjectFactoryImpl.java
@@ -8,7 +8,6 @@ package org.jboss.forge.addon.projects.impl;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
