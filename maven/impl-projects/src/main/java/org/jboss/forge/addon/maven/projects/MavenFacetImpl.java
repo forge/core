c4b64a927637149282762bddb3a4a2d46edd8af0
George Gastaldi <gegastaldi@gmail.com>
2014-02-24 15:14:50 -0300

    FORGE-1598: Created MavenBuildManager

--- maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/MavenFacetImpl.java
+++ maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/MavenFacetImpl.java
@@ -7,13 +7,11 @@
 package org.jboss.forge.addon.maven.projects;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.Writer;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,47 +20,20 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.cli.MavenCli;
-import org.apache.maven.execution.DefaultMavenExecutionRequest;
-import org.apache.maven.execution.MavenExecutionRequest;
-import org.apache.maven.execution.MavenExecutionRequestPopulator;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
-import org.apache.maven.project.ProjectBuilder;
 import org.apache.maven.project.ProjectBuildingRequest;
 import org.apache.maven.project.ProjectBuildingResult;
-import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
-import org.apache.maven.settings.Mirror;
-import org.apache.maven.settings.Profile;
-import org.apache.maven.settings.Proxy;
-import org.apache.maven.settings.Repository;
-import org.apache.maven.settings.Settings;
-import org.eclipse.aether.DefaultRepositorySystemSession;
-import org.eclipse.aether.RepositorySystem;
-import org.eclipse.aether.repository.LocalRepository;
-import org.eclipse.aether.util.repository.DefaultMirrorSelector;
-import org.eclipse.aether.util.repository.DefaultProxySelector;
-import org.jboss.forge.addon.environment.Environment;
 import org.jboss.forge.addon.facets.AbstractFacet;
-import org.jboss.forge.addon.maven.environment.Network;
 import org.jboss.forge.addon.maven.projects.util.NativeSystemCall;
-import org.jboss.forge.addon.maven.projects.util.RepositoryUtils;
 import org.jboss.forge.addon.maven.resources.MavenModelResource;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.ProjectFacet;
 import org.jboss.forge.addon.resource.DirectoryResource;
 import org.jboss.forge.addon.resource.ResourceFactory;
-import org.jboss.forge.addon.resource.events.ResourceEvent;
-import org.jboss.forge.addon.resource.monitor.ResourceListener;
-import org.jboss.forge.addon.resource.monitor.ResourceMonitor;
-import org.jboss.forge.furnace.container.cdi.events.Local;
-import org.jboss.forge.furnace.event.PreShutdown;
-import org.jboss.forge.furnace.manager.maven.MavenContainer;
-import org.jboss.forge.furnace.spi.ListenerRegistration;
 import org.jboss.forge.furnace.util.OperatingSystemUtils;
 
 /**
@@ -72,131 +43,25 @@ public class MavenFacetImpl extends AbstractFacet<Project> implements ProjectFac
 {
    private static final Logger log = Logger.getLogger(MavenFacetImpl.class.getName());
 
-   private ProjectBuildingResult buildingResult;
-   private ProjectBuilder builder;
-   private ResourceMonitor monitor;
-   private ListenerRegistration<ResourceListener> listenerRegistration;
-
-   @Inject
-   private MavenContainer container;
-
-   @Inject
-   private Environment environment;
-
    @Inject
    private ResourceFactory factory;
 
    @Inject
-   private PlexusContainer plexus;
-
-   private ProjectBuilder getBuilder()
-   {
-      if (builder == null)
-         builder = plexus.lookup(ProjectBuilder.class);
-      return builder;
-   }
+   private MavenBuildManager buildManager;
 
    public ProjectBuildingRequest getRequest()
    {
-      return getBuildingRequest(Network.isOffline(environment));
+      return buildManager.getProjectBuildingRequest();
    }
 
    public ProjectBuildingRequest getOfflineRequest()
    {
-      return getBuildingRequest(true);
+      return buildManager.getProjectBuildingRequest(isInstalled());
    }
 
    public ProjectBuildingRequest getBuildingRequest(final boolean offline)
    {
-      ClassLoader cl = Thread.currentThread().getContextClassLoader();
-      try
-      {
-         Settings settings = container.getSettings();
-         // TODO this needs to be configurable via .forge
-         // TODO this reference to the M2_REPO should probably be centralized
-
-         MavenExecutionRequest executionRequest = new DefaultMavenExecutionRequest();
-         MavenExecutionRequestPopulator populator = plexus.lookup(MavenExecutionRequestPopulator.class);
-         populator.populateFromSettings(executionRequest, container.getSettings());
-         populator.populateDefaults(executionRequest);
-         RepositorySystem system = plexus.lookup(RepositorySystem.class);
-         ProjectBuildingRequest request = executionRequest.getProjectBuildingRequest();
-
-         ArtifactRepository localRepository = RepositoryUtils.toArtifactRepository("local",
-                  new File(settings.getLocalRepository()).toURI().toURL().toString(), null, true, true);
-         request.setLocalRepository(localRepository);
-
-         List<ArtifactRepository> settingsRepos = new ArrayList<>(request.getRemoteRepositories());
-         List<String> activeProfiles = settings.getActiveProfiles();
-
-         Map<String, Profile> profiles = settings.getProfilesAsMap();
-
-         for (String id : activeProfiles)
-         {
-            Profile profile = profiles.get(id);
-            if (profile != null)
-            {
-               List<Repository> repositories = profile.getRepositories();
-               for (Repository repository : repositories)
-               {
-                  settingsRepos.add(RepositoryUtils.convertFromMavenSettingsRepository(repository));
-               }
-            }
-         }
-         request.setRemoteRepositories(settingsRepos);
-         request.setSystemProperties(System.getProperties());
-
-         DefaultRepositorySystemSession repositorySession = MavenRepositorySystemUtils.newSession();
-         Proxy activeProxy = settings.getActiveProxy();
-         if (activeProxy != null)
-         {
-            DefaultProxySelector dps = new DefaultProxySelector();
-            dps.add(RepositoryUtils.convertFromMavenProxy(activeProxy), activeProxy.getNonProxyHosts());
-            repositorySession.setProxySelector(dps);
-         }
-         LocalRepository localRepo = new LocalRepository(settings.getLocalRepository());
-         repositorySession.setLocalRepositoryManager(system.newLocalRepositoryManager(repositorySession, localRepo));
-         repositorySession.setOffline(offline);
-         List<Mirror> mirrors = executionRequest.getMirrors();
-         if (mirrors != null)
-         {
-            DefaultMirrorSelector mirrorSelector = new DefaultMirrorSelector();
-            for (Mirror mirror : mirrors)
-            {
-               mirrorSelector.add(mirror.getId(), mirror.getUrl(), mirror.getLayout(), false, mirror.getMirrorOf(),
-                        mirror.getMirrorOfLayouts());
-            }
-            repositorySession.setMirrorSelector(mirrorSelector);
-         }
-
-         request.setRepositorySession(repositorySession);
-         request.setProcessPlugins(false);
-         request.setResolveDependencies(false);
-         return request;
-      }
-      catch (RuntimeException e)
-      {
-         throw e;
-      }
-      catch (Exception e)
-      {
-         throw new RuntimeException(
-                  "Could not create Maven project building request", e);
-      }
-      finally
-      {
-         /*
-          * We reset the classloader to prevent potential modules bugs if Classwords container changes classloaders on
-          * us
-          */
-         Thread.currentThread().setContextClassLoader(cl);
-      }
-   }
-
-   @Override
-   public void setFaceted(Project project)
-   {
-      super.setFaceted(project);
+      return buildManager.getProjectBuildingRequest(offline);
    }
 
    @Override
@@ -208,33 +73,8 @@ public class MavenFacetImpl extends AbstractFacet<Project> implements ProjectFac
          if (!pom.createNewFile())
             throw new IllegalStateException("Could not create POM file.");
          pom.setContents(createDefaultPOM());
-         monitor = pom.monitor();
-         listenerRegistration = monitor.addResourceListener(new ResourceListener()
-         {
-            @Override
-            public void processEvent(ResourceEvent event)
-            {
-               invalidateBuildingResults();
-            }
-         });
       }
       return isInstalled();
-   }
-
-   public void onShutdown(@Observes @Local PreShutdown event)
-   {
-      if (listenerRegistration != null)
-         listenerRegistration.removeListener();
-   }
-
-   @Override
-   public boolean uninstall()
-   {
-      if (monitor != null)
-      {
-         monitor.cancel();
-      }
-      return super.uninstall();
    }
 
    private String createDefaultPOM()
@@ -279,8 +119,7 @@ public class MavenFacetImpl extends AbstractFacet<Project> implements ProjectFac
    {
       MavenXpp3Writer writer = new MavenXpp3Writer();
       ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-      Writer fw = new OutputStreamWriter(outputStream);
-      try
+      try (Writer fw = new OutputStreamWriter(outputStream))
       {
          writer.write(fw, pom);
          getModelResource().setContents(outputStream.toString());
@@ -289,24 +128,6 @@ public class MavenFacetImpl extends AbstractFacet<Project> implements ProjectFac
       {
          throw new RuntimeException("Could not write POM file: " + getModelResource().getFullyQualifiedName(), e);
       }
-      finally
-      {
-         try
-         {
-            fw.close();
-            outputStream.close();
-         }
-         catch (IOException e)
-         {
-            throw new RuntimeException(e);
-         }
-      }
-
-      /*
-       * Invalidate build result immediately; otherwise, the current thread may not get correct results until the
-       * monitor thread catches the change.
-       */
-      invalidateBuildingResults();
    }
 
    /*
@@ -314,40 +135,7 @@ public class MavenFacetImpl extends AbstractFacet<Project> implements ProjectFac
     */
    public synchronized ProjectBuildingResult getProjectBuildingResult() throws Exception
    {
-      if (this.buildingResult == null)
-      {
-         ProjectBuildingRequest request = null;
-         request = getRequest();
-         MavenModelResource pomResource = getModelResource();
-         if (request != null)
-         {
-            try
-            {
-               request.setResolveDependencies(true);
-               // FORGE-1287
-               // buildingResult = getBuilder().build(new FileResourceModelSource(pomResource), request);
-               buildingResult = getBuilder().build(pomResource.getUnderlyingResourceObject(), request);
-            }
-            catch (RuntimeException full)
-            {
-               throw full;
-            }
-            catch (Exception full)
-            {
-               throw new RuntimeException(full);
-            }
-         }
-         else
-         {
-            throw new RuntimeException("Project building request was null");
-         }
-      }
-      return buildingResult;
-   }
-
-   private void invalidateBuildingResults()
-   {
-      this.buildingResult = null;
+      return buildManager.getProjectBuildingResult(getModelResource());
    }
 
    @Override
@@ -467,8 +255,7 @@ public class MavenFacetImpl extends AbstractFacet<Project> implements ProjectFac
    @Override
    public DirectoryResource getLocalRepositoryDirectory()
    {
-      return factory.create(new File(container.getSettings().getLocalRepository())).reify(
-               DirectoryResource.class);
+      return factory.create(buildManager.getLocalRepositoryDirectory()).reify(DirectoryResource.class);
    }
 
    @Override
