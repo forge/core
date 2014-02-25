bddac71be4fa7c7d57260e7a7fd97aa8326e3e78
George Gastaldi <gegastaldi@gmail.com>
2014-02-25 13:54:36 -0300

    FORGE-1493: Created cdi-new-qualifier command

---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/cdi/ui/NewQualifierCommand.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.cdi.ui;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Inject;
+import javax.inject.Qualifier;
+
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.parser.java.ui.AbstractJavaSourceCommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.hints.InputType;
+import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.result.Failed;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.util.Categories;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.parser.java.JavaAnnotation;
+import org.jboss.forge.parser.java.JavaSource;
+
+/**
+ * Creates a new CDI Qualifier annotation
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class NewQualifierCommand extends AbstractJavaSourceCommand
+{
+   @Inject
+   @WithAttributes(label = "Custom Scope Annotation", type = InputType.JAVA_CLASS_PICKER)
+   private UIInput<Boolean> inherited;
+
+   @Override
+   public Metadata getMetadata(UIContext context)
+   {
+      return Metadata.from(super.getMetadata(context), getClass())
+               .name("CDI: New Qualifier")
+               .description("Creates a new CDI Qualifier annotation")
+               .category(Categories.create(super.getMetadata(context).getCategory(), "CDI"));
+   }
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      super.initializeUI(builder);
+      builder.add(inherited);
+   }
+
+   @Override
+   public Result execute(UIExecutionContext context) throws Exception
+   {
+      // TODO: Super implementation should have an "overwrite" flag for existing files?
+      Result result = super.execute(context);
+      if (!(result instanceof Failed))
+      {
+         JavaSourceFacet javaSourceFacet = getSelectedProject(context).getFacet(JavaSourceFacet.class);
+         JavaResource javaResource = context.getUIContext().getSelection();
+         JavaSource<?> qualifier = javaResource.getJavaSource();
+         qualifier.addAnnotation(Qualifier.class);
+         if (inherited.getValue())
+         {
+            qualifier.addAnnotation(Inherited.class);
+         }
+         qualifier.addAnnotation(Retention.class).setEnumValue(RUNTIME);
+         qualifier.addAnnotation(Target.class).setEnumValue(METHOD, FIELD, PARAMETER, TYPE);
+         qualifier.addAnnotation(Documented.class);
+         javaSourceFacet.saveJavaSource(qualifier);
+      }
+      return result;
+   }
+
+   @Override
+   protected boolean isProjectRequired()
+   {
+      return true;
+   }
+
+   @Override
+   protected String getType()
+   {
+      return "CDI Qualifier";
+   }
+
+   @Override
+   protected Class<? extends JavaSource<?>> getSourceType()
+   {
+      return JavaAnnotation.class;
+   }
+
+}
---
+++ javaee/tests/src/test/java/org/jboss/forge/addon/javaee/cdi/ui/NewQualifierCommandTest.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.cdi.ui;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+import java.lang.annotation.Inherited;
+
+import javax.inject.Inject;
+import javax.inject.Qualifier;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.addon.javaee.ProjectHelper;
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.ui.controller.CommandController;
+import org.jboss.forge.addon.ui.result.Failed;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.test.UITestHarness;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.forge.parser.java.JavaAnnotation;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class NewQualifierCommandTest
+{
+   @Deployment
+   @Dependencies({
+            @AddonDependency(name = "org.jboss.forge.addon:ui"),
+            @AddonDependency(name = "org.jboss.forge.addon:ui-test-harness"),
+            @AddonDependency(name = "org.jboss.forge.addon:javaee"),
+            @AddonDependency(name = "org.jboss.forge.addon:maven")
+   })
+   public static ForgeArchive getDeployment()
+   {
+      return ShrinkWrap
+               .create(ForgeArchive.class)
+               .addClass(ProjectHelper.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:projects"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:javaee"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:maven"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:ui"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:ui-test-harness")
+               );
+   }
+
+   @Inject
+   private UITestHarness testHarness;
+
+   @Inject
+   private ProjectHelper projectHelper;
+
+   @Test
+   public void testCreateNewQualifier() throws Exception
+   {
+      Project project = projectHelper.createJavaLibraryProject();
+      CommandController controller = testHarness.createCommandController(NewQualifierCommand.class,
+               project.getRootDirectory());
+      controller.initialize();
+      controller.setValueFor("named", "MyQualifier");
+      controller.setValueFor("targetPackage", "org.jboss.forge.test");
+      Assert.assertTrue(controller.isValid());
+      Assert.assertTrue(controller.canExecute());
+      Result result = controller.execute();
+      Assert.assertThat(result, is(not(instanceOf(Failed.class))));
+
+      JavaSourceFacet facet = project.getFacet(JavaSourceFacet.class);
+      JavaResource javaResource = facet.getJavaResource("org.jboss.forge.test.MyQualifier");
+      Assert.assertNotNull(javaResource);
+      Assert.assertThat(javaResource.getJavaSource(), is(instanceOf(JavaAnnotation.class)));
+      JavaAnnotation ann = (JavaAnnotation) javaResource.getJavaSource();
+      Assert.assertTrue(ann.hasAnnotation(Qualifier.class));
+      Assert.assertFalse(ann.hasAnnotation(Inherited.class));
+   }
+
+   @Test
+   public void testCreateNewQualifierInherited() throws Exception
+   {
+      Project project = projectHelper.createJavaLibraryProject();
+      CommandController controller = testHarness.createCommandController(NewQualifierCommand.class,
+               project.getRootDirectory());
+      controller.initialize();
+      controller.setValueFor("named", "MyInheritedQualifier");
+      controller.setValueFor("targetPackage", "org.jboss.forge.test");
+      controller.setValueFor("inherited", Boolean.TRUE);
+      Assert.assertTrue(controller.isValid());
+      Assert.assertTrue(controller.canExecute());
+      Result result = controller.execute();
+      Assert.assertThat(result, is(not(instanceOf(Failed.class))));
+
+      JavaSourceFacet facet = project.getFacet(JavaSourceFacet.class);
+      JavaResource javaResource = facet.getJavaResource("org.jboss.forge.test.MyInheritedQualifier");
+      Assert.assertNotNull(javaResource);
+      Assert.assertThat(javaResource.getJavaSource(), is(instanceOf(JavaAnnotation.class)));
+      JavaAnnotation ann = (JavaAnnotation) javaResource.getJavaSource();
+      Assert.assertTrue(ann.hasAnnotation(Qualifier.class));
+      Assert.assertTrue(ann.hasAnnotation(Inherited.class));
+   }
+
+}
