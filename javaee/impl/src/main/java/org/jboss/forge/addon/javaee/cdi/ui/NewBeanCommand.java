9515a0fb2cf5363d03adf99c4e0faf469fb0f523
George Gastaldi <gegastaldi@gmail.com>
2014-02-25 13:45:56 -0300

    FORGE-1493: Created cdi-new-bean command

---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/cdi/ui/NewBeanCommand.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.cdi.ui;
+
+import java.util.concurrent.Callable;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.parser.java.ui.AbstractJavaSourceCommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.hints.InputType;
+import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.addon.ui.input.UISelectOne;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.result.Failed;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.util.Categories;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.parser.java.JavaClass;
+import org.jboss.forge.parser.java.JavaSource;
+
+/**
+ * Creates a new CDI Bean with a specific scope
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class NewBeanCommand extends AbstractJavaSourceCommand
+{
+   @Inject
+   @WithAttributes(label = "Scope", defaultValue = "DEPENDENT")
+   private UISelectOne<BeanScope> scoped;
+
+   @Inject
+   @WithAttributes(label = "Custom Scope Annotation", type = InputType.JAVA_CLASS_PICKER)
+   private UIInput<String> customScopeAnnotation;
+
+   @Override
+   public Metadata getMetadata(UIContext context)
+   {
+      return Metadata.from(super.getMetadata(context), getClass())
+               .name("CDI: New Bean")
+               .description("Creates a new CDI Managed bean")
+               .category(Categories.create(super.getMetadata(context).getCategory(), "CDI"));
+   }
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      super.initializeUI(builder);
+      Callable<Boolean> customScopeSelected = new Callable<Boolean>()
+      {
+         @Override
+         public Boolean call() throws Exception
+         {
+            return scoped.getValue() == BeanScope.CUSTOM;
+         }
+      };
+      customScopeAnnotation.setEnabled(customScopeSelected).setRequired(customScopeSelected);
+      builder.add(scoped).add(customScopeAnnotation);
+   }
+
+   @Override
+   public Result execute(UIExecutionContext context) throws Exception
+   {
+      // TODO: Super implementation should have an "overwrite" flag for existing files?
+      Result result = super.execute(context);
+      if (!(result instanceof Failed))
+      {
+         JavaSourceFacet javaSourceFacet = getSelectedProject(context).getFacet(JavaSourceFacet.class);
+         JavaResource javaResource = context.getUIContext().getSelection();
+         JavaSource<?> javaSource = javaResource.getJavaSource();
+         BeanScope scopedValue = scoped.getValue();
+         if (BeanScope.CUSTOM == scopedValue)
+         {
+            javaSource.addAnnotation(customScopeAnnotation.getValue());
+         }
+         else if (BeanScope.DEPENDENT != scopedValue)
+         {
+            javaSource.addAnnotation(scopedValue.getAnnotation());
+         }
+         javaSourceFacet.saveJavaSource(javaSource);
+      }
+      return result;
+   }
+
+   @Override
+   protected boolean isProjectRequired()
+   {
+      return true;
+   }
+
+   @Override
+   protected String getType()
+   {
+      return "CDI Bean";
+   }
+
+   @Override
+   protected Class<? extends JavaSource<?>> getSourceType()
+   {
+      return JavaClass.class;
+   }
+
+}
