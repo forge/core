cdc0f58bdda5b1332e90e21df8478e4c4f43a9cf
George Gastaldi <gegastaldi@gmail.com>
2014-02-26 12:34:00 -0300

    Added prompt commands to test

--- ui/example/src/main/java/org/jboss/forge/addon/ui/example/commands/PromptCommand.java
+++ ui/example/src/main/java/org/jboss/forge/addon/ui/example/commands/PromptCommand.java
@@ -7,45 +7,36 @@
 
 package org.jboss.forge.addon.ui.example.commands;
 
-import org.jboss.forge.addon.ui.UIProvider;
-import org.jboss.forge.addon.ui.command.AbstractUICommand;
-import org.jboss.forge.addon.ui.context.UIBuilder;
-import org.jboss.forge.addon.ui.context.UIContext;
-import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.annotation.Command;
 import org.jboss.forge.addon.ui.input.UIPrompt;
-import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.output.UIOutput;
-import org.jboss.forge.addon.ui.result.Result;
-import org.jboss.forge.addon.ui.util.Metadata;
 
 /**
  * 
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
-public class PromptCommand extends AbstractUICommand
+public class PromptCommand
 {
 
-   @Override
-   public void initializeUI(UIBuilder builder) throws Exception
+   @Command(value = "prompt-boolean", help = "Prompts for information")
+   public void promptBoolean(UIOutput output, UIPrompt prompt)
    {
-
-   }
-
-   @Override
-   public UICommandMetadata getMetadata(UIContext context)
-   {
-      return Metadata.forCommand(PromptCommand.class).name("prompt").description("Prompts for Information");
-   }
-
-   @Override
-   public Result execute(UIExecutionContext context) throws Exception
-   {
-      UIProvider provider = context.getUIContext().getProvider();
-      UIOutput output = provider.getOutput();
-      UIPrompt prompt = context.getPrompt();
       boolean answer = prompt.promptBoolean("Do you love Forge 2?");
       output.out().println("You answered: " + answer);
-      return null;
+   }
+
+   @Command(value = "prompt-secret", help = "Prompts for information")
+   public void promptSecret(UIOutput output, UIPrompt prompt)
+   {
+      String answer = prompt.promptSecret("Type your password: ");
+      output.out().println("You typed: " + answer);
+   }
+
+   @Command(value = "prompt", help = "Prompts for information")
+   public void prompt(UIOutput output, UIPrompt prompt)
+   {
+      String answer = prompt.prompt("Type something: ");
+      output.out().println("You typed: " + answer);
    }
 
 }
