58ebbffa44215e51f9825b5e80dfe41c7d660077
George Gastaldi <gegastaldi@gmail.com>
2014-04-04 12:47:52 -0300

    FORGE-1734: ShellHandle is an interface now

--- shell/api/src/main/java/org/jboss/forge/addon/shell/ShellHandle.java
+++ shell/api/src/main/java/org/jboss/forge/addon/shell/ShellHandle.java
@@ -10,13 +10,7 @@ package org.jboss.forge.addon.shell;
 import java.io.File;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import javax.inject.Inject;
-
-import org.jboss.aesh.console.settings.Settings;
-import org.jboss.aesh.console.settings.SettingsBuilder;
 import org.jboss.forge.addon.ui.command.CommandExecutionListener;
 
 /**
@@ -24,43 +18,12 @@ import org.jboss.forge.addon.ui.command.CommandExecutionListener;
  *
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
-public class ShellHandle
+public interface ShellHandle
 {
-   @Inject
-   private ShellFactory shellFactory;
+   public void initialize(File currentResource, InputStream stdIn, PrintStream stdOut, PrintStream stdErr);
 
-   private Shell shell;
+   public void destroy();
 
-   public void initialize(File currentResource, InputStream stdIn, PrintStream stdOut, PrintStream stdErr)
-   {
-      Settings settings = new SettingsBuilder()
-               .inputStream(stdIn)
-               .outputStream(stdOut)
-               .outputStreamError(stdErr)
-               .enableMan(true)
-               .create();
-      this.shell = shellFactory.createShell(currentResource, settings);
-   }
-
-   public void destroy()
-   {
-      if (this.shell != null)
-         try
-         {
-            this.shell.close();
-         }
-         catch (Exception e)
-         {
-            Logger.getLogger(getClass().getName()).log(Level.FINE, "Error while closing Shell", e);
-         }
-   }
-
-   public void addCommandExecutionListener(CommandExecutionListener listener)
-   {
-      if (shell != null)
-      {
-         shell.addCommandExecutionListener(listener);
-      }
-   }
+   public void addCommandExecutionListener(CommandExecutionListener listener);
 
 }
---
+++ shell/impl/src/main/java/org/jboss/forge/addon/shell/ShellHandleImpl.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.shell;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.aesh.console.settings.Settings;
+import org.jboss.aesh.console.settings.SettingsBuilder;
+import org.jboss.forge.addon.ui.command.CommandExecutionListener;
+
+/**
+ * Holds a shell instance (no need for proxies)
+ *
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class ShellHandleImpl implements ShellHandle
+{
+   @Inject
+   private ShellFactory shellFactory;
+
+   private Shell shell;
+
+   @Override
+   public void initialize(File currentResource, InputStream stdIn, PrintStream stdOut, PrintStream stdErr)
+   {
+      Settings settings = new SettingsBuilder()
+               .inputStream(stdIn)
+               .outputStream(stdOut)
+               .outputStreamError(stdErr)
+               .enableMan(true)
+               .create();
+      this.shell = shellFactory.createShell(currentResource, settings);
+   }
+
+   @Override
+   public void destroy()
+   {
+      if (this.shell != null)
+         try
+         {
+            this.shell.close();
+         }
+         catch (Exception e)
+         {
+            Logger.getLogger(getClass().getName()).log(Level.FINE, "Error while closing Shell", e);
+         }
+   }
+
+   @Override
+   public void addCommandExecutionListener(CommandExecutionListener listener)
+   {
+      if (shell != null)
+      {
+         shell.addCommandExecutionListener(listener);
+      }
+   }
+
+}
