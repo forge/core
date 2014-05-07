0c6b7b009e2b94e8e25a078a857119ce6e6f2055
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2014-05-08 03:18:33 +0530

    FORGE-1796 Make JTA 1.1 optional for EJB 3.1

--- javaee/api/src/main/java/org/jboss/forge/addon/javaee/ejb/EJBFacet_3_1.java
+++ javaee/api/src/main/java/org/jboss/forge/addon/javaee/ejb/EJBFacet_3_1.java
@@ -8,13 +8,14 @@
 package org.jboss.forge.addon.javaee.ejb;
 
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.facets.constraints.FacetConstraintType;
 import org.jboss.forge.addon.javaee.jta.JTAFacet_1_1;
 
 /**
  * 
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
-@FacetConstraint(JTAFacet_1_1.class)
+@FacetConstraint(value = JTAFacet_1_1.class, type = FacetConstraintType.OPTIONAL)
 public interface EJBFacet_3_1 extends EJBFacet
 {
 
