2c7f9f7f55497af7730a571602b1c33c0ec1f03d
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2014-05-16 19:42:47 +0530

    Extracted names of RestResourceGenerator as constants in javaee-api.

---
+++ javaee/api/src/main/java/org/jboss/forge/addon/javaee/rest/generation/RestGenerationConstants.java
@@ -0,0 +1,18 @@
+package org.jboss.forge.addon.javaee.rest.generation;
+
+/**
+ * Created by vreynolds on 5/16/14.
+ */
+public class RestGenerationConstants
+{
+
+   /**
+    * Name identifier for the JPA entity based REST resource generator.
+    */
+   public static final String JPA_ENTITY = "JPA_ENTITY";
+
+   /**
+    * Name identifier for the DTO resource based REST resource generator.
+    */
+   public static final String ROOT_AND_NESTED_DTO = "ROOT_AND_NESTED_DTO";
+}
