2386e6742ef00b064dc12dd9c322d2b0cdbe1349
George Gastaldi <gegastaldi@gmail.com>
2014-07-02 14:01:41 -0300

    Revert "FORGE-1898: Calling Packages.toValidPackageName when setTopLevelPackage is called"
    This reverts commit b84c299d95e561c5cc9e402960cc2b367d5daa68.

--- maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/facets/MavenMetadataFacet.java
+++ maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/facets/MavenMetadataFacet.java
@@ -23,7 +23,6 @@ import org.jboss.forge.addon.facets.AbstractFacet;
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 import org.jboss.forge.addon.maven.projects.MavenBuildSystem;
 import org.jboss.forge.addon.maven.projects.MavenFacet;
-import org.jboss.forge.addon.parser.java.utils.Packages;
 import org.jboss.forge.addon.projects.ProjectProvider;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.facets.MetadataFacet;
@@ -112,7 +111,7 @@ public class MavenMetadataFacet extends AbstractFacet<Project> implements Metada
    {
       MavenFacet mvn = getFaceted().getFacet(MavenFacet.class);
       Model pom = mvn.getModel();
-      pom.setGroupId(Packages.toValidPackageName(groupId));
+      pom.setGroupId(groupId);
       mvn.setModel(pom);
       return this;
    }
