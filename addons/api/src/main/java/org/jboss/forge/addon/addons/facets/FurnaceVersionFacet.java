5201959ce6df1baa6e652bd88ad50b57c5d9af7d
George Gastaldi <gegastaldi@gmail.com>
2014-08-18 21:15:33 -0300

    FORGE-1983: Fixed invalid range specification in Furnace version

--- addons/api/src/main/java/org/jboss/forge/addon/addons/facets/FurnaceVersionFacet.java
+++ addons/api/src/main/java/org/jboss/forge/addon/addons/facets/FurnaceVersionFacet.java
@@ -6,15 +6,18 @@
  */
 package org.jboss.forge.addon.addons.facets;
 
+import javax.inject.Inject;
+
 import org.jboss.forge.addon.facets.AbstractFacet;
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.ProjectFacet;
 import org.jboss.forge.addon.projects.facets.MetadataFacet;
+import org.jboss.forge.furnace.Furnace;
 
 /**
  * Ensures that a project depends on the default Furnace container API.
- * 
+ *
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  */
 @FacetConstraint(MetadataFacet.class)
@@ -24,10 +27,14 @@ public class FurnaceVersionFacet extends AbstractFacet<Project> implements Proje
 
    private static final String VERSION_PROPERTY_NAME = "version.furnace";
 
+   @Inject
+   private Furnace furnace;
+
    @Override
    public boolean install()
    {
-      getFaceted().getFacet(MetadataFacet.class).setDirectProperty(VERSION_PROPERTY_NAME, "[,)");
+      getFaceted().getFacet(MetadataFacet.class).setDirectProperty(VERSION_PROPERTY_NAME,
+               furnace.getVersion().toString());
       return isInstalled();
    }
 
