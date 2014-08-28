1e9465de002b8df72a2c0c23a5e147b9f1558083
George Gastaldi <gegastaldi@gmail.com>
2014-08-28 14:44:56 -0300

    FORGE-1989: Merging same dialect class in a single description

--- database-tools/api/src/main/java/org/jboss/forge/addon/database/tools/jpa/HibernateDialect.java
+++ database-tools/api/src/main/java/org/jboss/forge/addon/database/tools/jpa/HibernateDialect.java
@@ -1,14 +1,13 @@
 package org.jboss.forge.addon.database.tools.jpa;
 
 public enum HibernateDialect {
-	
+
 	MYSQL5("MySQL5", "org.hibernate.dialect.MySQL5Dialect"),
 	MYSQL5_WITH_INNODB("MySQL5 with InnoDB", "org.hibernate.dialect.MySQL5InnoDBDialect"),
 	MYSQL_WITH_MYISAM("MySQL with MyISAM", "org.hibernate.dialect.MySQLMyISAMDialect"),
 	ORACLE_ANY_VERSION("Oracle (any version)", "org.hibernate.dialect.OracleDialect"),
 	ORACLE_9I("Oracle 9i", "org.hibernate.dialect.Oracle9iDialect"),
-	ORACLE_10G("Oracle 10g", "org.hibernate.dialect.Oracle10gDialect"),
-	ORACLE_11G("Oracle 11g", "org.hibernate.dialect.Oracle10gDialect"),
+	ORACLE_10G_AND_HIGHER("Oracle 10g and higher", "org.hibernate.dialect.Oracle10gDialect"),
 	DB2("DB2", "org.hibernate.dialect.DB2Dialect"),
 	DB2_AS400("DB2 AS/400", "org.hibernate.dialect.DB2400Dialect"),
 	DB2_OS390("DB2 OS390", "org.hibernate.dialect.DB2390Dialect"),
@@ -29,23 +28,23 @@ public enum HibernateDialect {
 	FIREBIRD("Firebird", "org.hibernate.dialect.FirebirdDialect"),
 	SYBASE("Sybase", "org.hibernate.dialect.SybaseASE15Dialect"),
 	SYBASE_ANYWHERE("Sybase Anywhere", "org.hibernate.dialect.SybaseAnywhereDialect");
-	
+
 	private String className;
 	private String databaseName;
-	
+
 	private HibernateDialect(String databaseName, String className) {
 		this.className = className;
 		this.databaseName = databaseName;
 	}
-	
+
 	public String getClassName() {
 		return className;
 	}
-	
+
 	public String getDatabaseName() {
 		return databaseName;
 	}
-	
+
 	public static HibernateDialect fromClassName(String className) {
 	   for (HibernateDialect dialect : HibernateDialect.values()) {
 	      if (dialect.getClassName().equals(className)) {
@@ -54,5 +53,5 @@ public enum HibernateDialect {
 	   }
 	   return null;
 	}
-	
+
 }
