1e354c026951abe9c489a271e9c6e34f42724b3b
George Gastaldi <gegastaldi@gmail.com>
2014-09-12 20:06:53 -0300

    Invalidating project cache after JavaEE setup is executed, since it needs to recalculate the installed facets again.

--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/ui/JavaEESetupCommand.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/ui/JavaEESetupCommand.java
@@ -11,6 +11,7 @@ import javax.inject.Inject;
 
 import org.jboss.forge.addon.facets.FacetFactory;
 import org.jboss.forge.addon.javaee.facets.JavaEESpecFacet;
+import org.jboss.forge.addon.projects.ProjectFactory;
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
@@ -21,13 +22,16 @@ import org.jboss.forge.addon.ui.result.Results;
 import org.jboss.forge.addon.ui.util.Metadata;
 
 /**
- * 
+ *
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
 public class JavaEESetupCommand extends AbstractJavaEECommand
 {
    @Inject
    private FacetFactory facetFactory;
+
+   @Inject
+   private ProjectFactory projectFactory;
 
    @Inject
    @WithAttributes(required = true, label = "Java EE Version", defaultValue = "6")
@@ -53,6 +57,8 @@ public class JavaEESetupCommand extends AbstractJavaEECommand
       JavaEESpecFacet chosen = javaEEVersion.getValue();
       if (facetFactory.install(getSelectedProject(context.getUIContext()), chosen))
       {
+         // This facet may activate other facets, so better invalidate the cache
+         projectFactory.invalidateCaches();
          return Results.success("JavaEE " + chosen.getSpecVersion() + " has been installed.");
       }
       return Results.fail("Could not install JavaEE " + chosen.getSpecVersion());
