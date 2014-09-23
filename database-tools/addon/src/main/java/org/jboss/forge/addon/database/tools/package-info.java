64fd515d89ad85bdfe11d18f2caff952322999dd
George Gastaldi <gegastaldi@gmail.com>
2014-09-23 19:41:12 -0300

    Added package-info to avoid test failures

---
+++ database-tools/addon/src/main/java/org/jboss/forge/addon/database/tools/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+package org.jboss.forge.addon.database.tools;
