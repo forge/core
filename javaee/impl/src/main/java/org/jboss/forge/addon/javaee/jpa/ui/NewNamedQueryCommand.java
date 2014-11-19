e017f37bf3a091ee800b6152a8b6a2c0ab526368
George Gastaldi <gegastaldi@gmail.com>
2014-11-19 19:10:13 -0200

    FORGE-2138: Created jpa-new-named-query command with tests

---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/ui/NewNamedQueryCommand.java
@@ -0,0 +1,175 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.jpa.ui;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.jboss.forge.addon.facets.constraints.FacetConstraint;
+import org.jboss.forge.addon.javaee.jpa.JPAFacet;
+import org.jboss.forge.addon.javaee.ui.AbstractJavaEECommand;
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.parser.java.resources.JavaResourceVisitor;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.resource.Resource;
+import org.jboss.forge.addon.resource.ResourceException;
+import org.jboss.forge.addon.resource.visit.VisitContext;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.context.UISelection;
+import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.addon.ui.input.UISelectOne;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Categories;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.roaster.model.source.AnnotationSource;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.JavaSource;
+
+/**
+ * Creates a {@link NamedQuery} annotation in a JPA {@link Entity}
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+@FacetConstraint(JPAFacet.class)
+public class NewNamedQueryCommand extends AbstractJavaEECommand
+{
+   @Inject
+   @WithAttributes(label = "Query Name", description = "The name attribute of the @NamedQuery annotation", required = true)
+   private UIInput<String> named;
+
+   @Inject
+   @WithAttributes(label = "Query", description = "The query attribute of the @NamedQuery annotation", required = true)
+   private UIInput<String> query;
+
+   @Inject
+   @WithAttributes(label = "Target Entity", required = true)
+   private UISelectOne<JavaResource> targetEntity;
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      UIContext context = builder.getUIContext();
+      UISelection<Resource<?>> selection = context.getInitialSelection();
+      Project project = getSelectedProject(builder);
+      List<JavaResource> entities = getProjectEntities(project);
+      targetEntity.setValueChoices(entities);
+      int idx = -1;
+      if (!selection.isEmpty())
+      {
+         idx = entities.indexOf(selection.get());
+      }
+      if (idx != -1)
+      {
+         targetEntity.setDefaultValue(entities.get(idx));
+      }
+      builder.add(named).add(query).add(targetEntity);
+   }
+
+   @Override
+   public UICommandMetadata getMetadata(UIContext context)
+   {
+      return Metadata.from(super.getMetadata(context), getClass()).name("JPA: New Named Query")
+               .description("Creates a @NamedQuery in a JPA Entity")
+               .category(Categories.create(super.getMetadata(context).getCategory().getName(), "JPA"));
+   }
+
+   @Override
+   public Result execute(UIExecutionContext context) throws Exception
+   {
+      String queryName = named.getValue();
+      String queryValue = query.getValue();
+      JavaResource entity = targetEntity.getValue();
+      JavaClassSource javaClass = entity.getJavaType();
+      AnnotationSource<JavaClassSource> namedQueriesAnn;
+      if (javaClass.hasAnnotation(NamedQueries.class))
+      {
+         namedQueriesAnn = javaClass.getAnnotation(NamedQueries.class);
+      }
+      else
+      {
+         namedQueriesAnn = javaClass.addAnnotation(NamedQueries.class);
+      }
+      // if (javaClass.hasAnnotation(NamedQuery.class))
+      // {
+      // // Move this annotation to a NamedQueries annotation
+      // AnnotationSource<JavaClassSource> annotation = javaClass.getAnnotation(NamedQuery.class);
+      // namedQueriesAnn.addAnnotationValue(annotation);
+      // javaClass.removeAnnotation(annotation);
+      // }
+      if (!javaClass.hasImport(NamedQuery.class))
+         javaClass.addImport(NamedQuery.class);
+
+      AnnotationSource<JavaClassSource> namedQuery = null;
+      // Find if an existing NamedQuery exists
+      if (namedQueriesAnn.getAnnotationArrayValue() != null)
+         for (AnnotationSource<JavaClassSource> namedQueryItem : namedQueriesAnn.getAnnotationArrayValue())
+         {
+            if (queryName.equals(namedQueryItem.getStringValue("name")))
+            {
+               namedQuery = namedQueryItem;
+               break;
+            }
+         }
+      if (namedQuery == null)
+      {
+         namedQuery = namedQueriesAnn.addAnnotationValue().setName(NamedQuery.class.getSimpleName());
+         namedQuery.setStringValue("name", queryName);
+      }
+      namedQuery.setStringValue("query", queryValue);
+      entity.setContents(javaClass);
+      return Results.success("Named query " + queryName + " was created.");
+   }
+
+   @Override
+   protected boolean isProjectRequired()
+   {
+      return true;
+   }
+
+   private List<JavaResource> getProjectEntities(Project project)
+   {
+      final List<JavaResource> entities = new ArrayList<>();
+      if (project != null)
+      {
+         project.getFacet(JavaSourceFacet.class).visitJavaSources(new JavaResourceVisitor()
+         {
+            @Override
+            public void visit(VisitContext context, JavaResource resource)
+            {
+               try
+               {
+                  JavaSource<?> javaSource = resource.getJavaType();
+                  if (javaSource.hasAnnotation(Entity.class) || javaSource.hasAnnotation(MappedSuperclass.class))
+                  {
+                     entities.add(resource);
+                  }
+               }
+               catch (ResourceException | FileNotFoundException e)
+               {
+                  // ignore
+               }
+            }
+         });
+      }
+      return entities;
+   }
+
+}
---
+++ javaee/tests/src/test/java/org/jboss/forge/addon/javaee/jpa/ui/NewNamedQueryCommandTest.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.jpa.ui;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+
+import javax.inject.Inject;
+import javax.persistence.NamedQueries;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.addon.javaee.ProjectHelper;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.ui.controller.CommandController;
+import org.jboss.forge.addon.ui.result.Failed;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.test.UITestHarness;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.Dependencies;
+import org.jboss.forge.arquillian.archive.ForgeArchive;
+import org.jboss.forge.furnace.repositories.AddonDependencyEntry;
+import org.jboss.forge.roaster.model.source.AnnotationSource;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests for the {@link NewNamedQueryCommand} class
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class NewNamedQueryCommandTest
+{
+   @Deployment
+   @Dependencies({
+            @AddonDependency(name = "org.jboss.forge.addon:ui"),
+            @AddonDependency(name = "org.jboss.forge.addon:ui-test-harness"),
+            @AddonDependency(name = "org.jboss.forge.addon:javaee"),
+            @AddonDependency(name = "org.jboss.forge.addon:maven")
+   })
+   public static ForgeArchive getDeployment()
+   {
+      return ShrinkWrap
+               .create(ForgeArchive.class)
+               .addClass(ProjectHelper.class)
+               .addBeansXML()
+               .addAsAddonDependencies(
+                        AddonDependencyEntry.create("org.jboss.forge.furnace.container:cdi"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:projects"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:javaee"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:maven"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:ui"),
+                        AddonDependencyEntry.create("org.jboss.forge.addon:ui-test-harness")
+               );
+   }
+
+   @Inject
+   private UITestHarness testHarness;
+
+   @Inject
+   private ProjectHelper projectHelper;
+
+   @Test
+   public void testCreateNamedQuery() throws Exception
+   {
+      Project project = projectHelper.createWebProject();
+      projectHelper.installJPA_2_0(project);
+      JavaResource jpaEntity = projectHelper.createJPAEntity(project, "Customer");
+      try (CommandController controller = testHarness.createCommandController(NewNamedQueryCommand.class,
+               project.getRoot()))
+      {
+         controller.initialize();
+         controller.setValueFor("named", "Customer.findAll");
+         controller.setValueFor("query", "select OBJ from Customer OBJ");
+         controller.setValueFor("targetEntity", jpaEntity);
+         Assert.assertTrue(controller.isValid());
+         Assert.assertTrue(controller.canExecute());
+         Result result = controller.execute();
+         Assert.assertThat(result, is(not(instanceOf(Failed.class))));
+      }
+      Assert.assertTrue(jpaEntity.exists());
+      JavaClassSource javaClass = jpaEntity.getJavaType();
+      Assert.assertTrue(javaClass.hasAnnotation(NamedQueries.class));
+      AnnotationSource<JavaClassSource> namedQueries = javaClass.getAnnotation(NamedQueries.class);
+      AnnotationSource<JavaClassSource>[] namedQueryArray = namedQueries.getAnnotationArrayValue();
+      Assert.assertNotNull(namedQueryArray);
+      Assert.assertEquals(1, namedQueryArray.length);
+      AnnotationSource<JavaClassSource> namedQuery = namedQueryArray[0];
+      Assert.assertEquals("Customer.findAll", namedQuery.getStringValue("name"));
+      Assert.assertEquals("select OBJ from Customer OBJ", namedQuery.getStringValue("query"));
+   }
+}
