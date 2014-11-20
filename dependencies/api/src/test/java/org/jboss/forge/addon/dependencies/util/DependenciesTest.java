879d45528c322f280cc257b50511c0d054da2e6e
George Gastaldi <gegastaldi@gmail.com>
2014-11-20 14:24:57 -0200

    FORGE-2154: Dependencies.areEquivalent now checks for packaging type

---
+++ dependencies/api/src/test/java/org/jboss/forge/addon/dependencies/util/DependenciesTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.dependencies.util;
+
+import org.jboss.forge.addon.dependencies.Dependency;
+import org.jboss.forge.addon.dependencies.builder.DependencyBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * {@link Dependencies} test methods
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class DependenciesTest
+{
+
+   /**
+    * Test method for
+    * {@link org.jboss.forge.addon.dependencies.util.Dependencies#areEquivalent(org.jboss.forge.addon.dependencies.Dependency, org.jboss.forge.addon.dependencies.Dependency)}
+    * .
+    */
+   @Test
+   public void testAreEquivalentDependencyDependency()
+   {
+      Dependency depOne = DependencyBuilder.create().setGroupId("org.jboss.errai").setArtifactId("errai-cdi-client")
+               .setPackaging("jar");
+      Dependency depTwo = DependencyBuilder.create().setGroupId("org.jboss.errai").setArtifactId("errai-cdi-client")
+               .setPackaging("test-jar");
+      Assert.assertFalse(Dependencies.areEquivalent(depOne, depTwo));
+   }
+
+   /**
+    * Test method for
+    * {@link org.jboss.forge.addon.dependencies.util.Dependencies#areEquivalent(org.jboss.forge.addon.dependencies.Coordinate, org.jboss.forge.addon.dependencies.Coordinate)}
+    * .
+    */
+   @Test
+   public void testAreEquivalentCoordinateCoordinate()
+   {
+      Dependency depOne = DependencyBuilder.create().setGroupId("org.jboss.errai").setArtifactId("errai-cdi-client")
+               .setPackaging("jar");
+      Dependency depTwo = DependencyBuilder.create().setGroupId("org.jboss.errai").setArtifactId("errai-cdi-client")
+               .setPackaging("test-jar");
+      Assert.assertFalse(Dependencies.areEquivalent(depOne, depTwo));
+   }
+
+}
