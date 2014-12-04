8bdd8d489962ce6be05d060c3c89ac03cb2a8d64
George Gastaldi <gegastaldi@gmail.com>
2014-12-04 02:05:53 -0200

    Update DefaultUIDesktop.java

--- ui/api/src/main/java/org/jboss/forge/addon/ui/DefaultUIDesktop.java
+++ ui/api/src/main/java/org/jboss/forge/addon/ui/DefaultUIDesktop.java
@@ -21,51 +21,45 @@ import org.jboss.forge.addon.ui.UIDesktop;
  */
 public class DefaultUIDesktop implements UIDesktop
 {
-   private final Desktop desktop;
-
-   public DefaultUIDesktop()
-   {
-      this.desktop = Desktop.getDesktop();
-   }
-
-   public DefaultUIDesktop(Desktop desktop)
-   {
-      this.desktop = desktop;
-   }
-
    @Override
    public void open(File file) throws IOException
    {
-      desktop.open(file);
+      getDesktop().open(file);
    }
 
    @Override
    public void edit(File file) throws IOException
    {
-      desktop.edit(file);
+      getDesktop().edit(file);
    }
 
    @Override
    public void print(File file) throws IOException
    {
-      desktop.print(file);
+      getDesktop().print(file);
    }
 
    @Override
    public void browse(URI uri) throws IOException
    {
-      desktop.browse(uri);
+      getDesktop().browse(uri);
    }
 
    @Override
    public void mail() throws IOException
    {
-      desktop.mail();
+      getDesktop().mail();
    }
 
    @Override
    public void mail(URI mailtoURI) throws IOException
    {
-      desktop.mail(mailtoURI);
+      getDesktop().mail(mailtoURI);
    }
+
+   protected Desktop getDesktop()
+   {
+      return Desktop.getDesktop();
+   }
+
 }
