322b8d73df62305b1db64f1b8772ed24e6211303
George Gastaldi <gegastaldi@gmail.com>
2014-12-10 14:35:02 -0200

    Avoid overriding of a preset settings.xml

--- addon-manager/tests/src/test/java/org/jboss/forge/addon/manager/AddonManagerTest.java
+++ addon-manager/tests/src/test/java/org/jboss/forge/addon/manager/AddonManagerTest.java
@@ -42,11 +42,16 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class AddonManagerTest
 {
+   private static String previousUserSettings;
+   private static String previousLocalRepository;
+
    @BeforeClass
    public static void setRemoteRepository() throws IOException
    {
-      System.setProperty(MavenContainer.ALT_USER_SETTINGS_XML_LOCATION, getAbsolutePath("profiles/settings.xml"));
-      System.setProperty(MavenContainer.ALT_LOCAL_REPOSITORY_LOCATION, "target/the-other-repository");
+      previousUserSettings = System.setProperty(MavenContainer.ALT_USER_SETTINGS_XML_LOCATION,
+               getAbsolutePath("profiles/settings.xml"));
+      previousLocalRepository = System.setProperty(MavenContainer.ALT_LOCAL_REPOSITORY_LOCATION,
+               "target/the-other-repository");
    }
 
    private static String getAbsolutePath(String path) throws FileNotFoundException
@@ -60,8 +65,22 @@ public class AddonManagerTest
    @AfterClass
    public static void clearRemoteRepository()
    {
-      System.clearProperty(MavenContainer.ALT_USER_SETTINGS_XML_LOCATION);
-      System.clearProperty(MavenContainer.ALT_LOCAL_REPOSITORY_LOCATION);
+      if (previousUserSettings == null)
+      {
+         System.clearProperty(MavenContainer.ALT_USER_SETTINGS_XML_LOCATION);
+      }
+      else
+      {
+         System.setProperty(MavenContainer.ALT_USER_SETTINGS_XML_LOCATION, previousUserSettings);
+      }
+      if (previousLocalRepository == null)
+      {
+         System.clearProperty(MavenContainer.ALT_LOCAL_REPOSITORY_LOCATION);
+      }
+      else
+      {
+         System.setProperty(MavenContainer.ALT_LOCAL_REPOSITORY_LOCATION, previousUserSettings);
+      }
    }
 
    @Deployment
