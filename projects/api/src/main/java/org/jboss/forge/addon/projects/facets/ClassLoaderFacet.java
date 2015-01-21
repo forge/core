d04a9735e6562a9fb2168145d9bd42996d9b2c67
George Gastaldi <gegastaldi@gmail.com>
2015-01-21 16:51:08 -0200

    Moar Javadoc Improvements

--- projects/api/src/main/java/org/jboss/forge/addon/projects/facets/ClassLoaderFacet.java
+++ projects/api/src/main/java/org/jboss/forge/addon/projects/facets/ClassLoaderFacet.java
@@ -7,9 +7,9 @@
 
 package org.jboss.forge.addon.projects.facets;
 
-import java.net.URL;
 import java.net.URLClassLoader;
 
+import org.jboss.forge.addon.dependencies.Dependency;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.ProjectFacet;
 
@@ -21,11 +21,18 @@ import org.jboss.forge.addon.projects.ProjectFacet;
 public interface ClassLoaderFacet extends ProjectFacet
 {
    /**
-    * Returns a {@link URLClassLoader} that encompasses all {@link Dependency} instances on which this project depends and the built artifact from the project sources. This is the equivalent of class-loading the entire project classpath.
-    * 
-    * WARNING: You *MUST* call {@link URLClassLoader#close()} when finished with this object. Failure to close this object upon completion will result in fatal memory leaks over time. If the scope of work is appropriate, consider using a try-with-resources block to encapsulate the operations and automatically clean up any ClassLoader resources.
+    * Returns a {@link URLClassLoader} that encompasses all {@link Dependency} instances on which this project depends.
+    * It also includes the compiled/fully built {@link PackagingFacet#getFinalArtifact()} from the project sources
+    * itself. This is the equivalent of class-loading the entire project classpath.
     * <p/>
-    * IMPORTANT: You must also clean up and release any {@link Class} references that were produced by this {@link ClassLoader}. It is not enough to close this. Held {@link Class} references will keep the {@link ClassLoader} from being garbage collected.
+    * WARNING: You *MUST* call {@link URLClassLoader#close()} when finished with this object. Failure to close this
+    * object upon completion will result in fatal memory leaks over time. If the scope of work is appropriate, consider
+    * using a try-with-resources block to encapsulate the operations and automatically clean up any ClassLoader
+    * resources.
+    * <p/>
+    * IMPORTANT: You must also clean up and release any {@link Class} references that were produced by this
+    * {@link ClassLoader}. It is not enough to close this. Held {@link Class} references will keep the
+    * {@link ClassLoader} from being garbage collected.
     */
    URLClassLoader getClassLoader();
 
