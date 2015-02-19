74465c8b59170346de6b5b03013e7b25472e1a14
George Gastaldi <gegastaldi@gmail.com>
2015-02-19 03:16:19 -0200

    FORGE-2240: from-archetype-catalog does not show if no catalogs are registered

--- maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/archetype/MavenArchetypeCatalogProjectType.java
+++ maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/archetype/MavenArchetypeCatalogProjectType.java
@@ -9,6 +9,9 @@ package org.jboss.forge.addon.maven.projects.archetype;
 
 import java.util.Arrays;
 
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.maven.archetype.ArchetypeCatalogFactoryRegistry;
 import org.jboss.forge.addon.maven.projects.MavenFacet;
 import org.jboss.forge.addon.maven.projects.archetype.ui.ArchetypeCatalogSelectionWizardStep;
 import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
@@ -16,6 +19,7 @@ import org.jboss.forge.addon.projects.AbstractProjectType;
 import org.jboss.forge.addon.projects.ProjectFacet;
 import org.jboss.forge.addon.projects.ProjectType;
 import org.jboss.forge.addon.projects.facets.MetadataFacet;
+import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.wizard.UIWizardStep;
 
 /**
@@ -26,35 +30,45 @@ import org.jboss.forge.addon.ui.wizard.UIWizardStep;
 public class MavenArchetypeCatalogProjectType extends AbstractProjectType
 {
 
-   @Override
-   public String getType()
-   {
-      return "From Archetype Catalog";
-   }
+    @Inject
+    private ArchetypeCatalogFactoryRegistry catalogRegistry;
+    
+    @Override
+    public String getType()
+    {
+        return "From Archetype Catalog";
+    }
 
-   @Override
-   public Class<? extends UIWizardStep> getSetupFlow()
-   {
-      return ArchetypeCatalogSelectionWizardStep.class;
-   }
+    @Override
+    public Class<? extends UIWizardStep> getSetupFlow()
+    {
+        return ArchetypeCatalogSelectionWizardStep.class;
+    }
 
-   @Override
-   public Iterable<Class<? extends ProjectFacet>> getRequiredFacets()
-   {
-      return Arrays
-               .<Class<? extends ProjectFacet>> asList(MetadataFacet.class, JavaSourceFacet.class, MavenFacet.class);
-   }
+    @Override
+    public Iterable<Class<? extends ProjectFacet>> getRequiredFacets()
+    {
+        return Arrays
+            .<Class<? extends ProjectFacet>> asList(MetadataFacet.class, JavaSourceFacet.class,
+                                                    MavenFacet.class);
+    }
 
-   @Override
-   public int priority()
-   {
-      return 10001;
-   }
+    @Override
+    public int priority()
+    {
+        return 10001;
+    }
 
-   @Override
-   public String toString()
-   {
-      return "from-archetype-catalog";
-   }
+    @Override
+    public String toString()
+    {
+        return "from-archetype-catalog";
+    }
+
+    @Override
+    public boolean isEnabled(UIContext context)
+    {
+        return catalogRegistry.hasArchetypeCatalogFactories();
+    }
 
 }
