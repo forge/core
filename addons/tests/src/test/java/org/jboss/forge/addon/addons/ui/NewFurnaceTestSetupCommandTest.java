e2a14c891ecf749a16fe6544ad80d816351a0047
George Gastaldi <gegastaldi@gmail.com>
2015-02-26 16:05:22 -0300

    Renamed test to match the class name

--- addons/tests/src/test/java/org/jboss/forge/addon/addons/ui/NewFurnaceTestSetupCommandTest.java
+++ addons/tests/src/test/java/org/jboss/forge/addon/addons/ui/NewFurnaceTestSetupCommandTest.java
@@ -34,7 +34,7 @@ import org.junit.runner.RunWith;
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
 @RunWith(Arquillian.class)
-public class FurnaceTestSetupCommandTest
+public class NewFurnaceTestSetupCommandTest
 {
 
    @Deployment
