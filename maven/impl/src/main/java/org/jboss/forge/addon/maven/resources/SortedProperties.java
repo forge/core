e72f52e54ebca5cbbeb0bdfdcf2426efc58f72a0
George Gastaldi <gegastaldi@gmail.com>
2015-03-10 16:38:01 -0300

    FORGE-2273: Sorting Maven model properties

---
+++ maven/impl/src/main/java/org/jboss/forge/addon/maven/resources/SortedProperties.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.maven.resources;
+
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Introduces an ordered {@link Properties#keySet()}
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+class SortedProperties extends Properties
+{
+   private static final long serialVersionUID = 1L;
+
+   @Override
+   public Set<Object> keySet()
+   {
+      return new TreeSet<Object>(super.keySet());
+   }
+}
---
+++ maven/tests/src/test/java/org/jboss/forge/addon/maven/projects/facets/MavenFacetTest.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.maven.projects.facets;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.maven.model.Model;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.addon.maven.projects.MavenFacet;
+import org.jboss.forge.addon.maven.resources.MavenModelResource;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFactory;
+import org.jboss.forge.arquillian.AddonDependencies;
+import org.jboss.forge.arquillian.AddonDependency;
+import org.jboss.forge.arquillian.archive.AddonArchive;
+import org.jboss.forge.parser.xml.Node;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class MavenFacetTest
+{
+
+   @Deployment
+   @AddonDependencies({
+            @AddonDependency(name = "org.jboss.forge.furnace.container:cdi"),
+            @AddonDependency(name = "org.jboss.forge.addon:resources"),
+            @AddonDependency(name = "org.jboss.forge.addon:projects"),
+            @AddonDependency(name = "org.jboss.forge.addon:maven")
+   })
+   public static AddonArchive getDeployment()
+   {
+      AddonArchive archive = ShrinkWrap
+               .create(AddonArchive.class)
+               .addBeansXML();
+      return archive;
+   }
+
+   @Inject
+   private ProjectFactory projectFactory;
+
+   @Test
+   public void testSortedProperties() throws Exception
+   {
+      Project project = projectFactory.createTempProject();
+      Assert.assertTrue(project.hasFacet(MavenFacet.class));
+      MavenFacet facet = project.getFacet(MavenFacet.class);
+      Model model = facet.getModel();
+      model.addProperty("A", "1");
+      model.addProperty("B", "2");
+      model.addProperty("C", "3");
+      model.addProperty("D", "4");
+      model.addProperty("E", "5");
+      facet.setModel(model);
+
+      MavenModelResource modelResource = facet.getModelResource();
+      Node contents = modelResource.getXmlSource();
+      Node propertiesNode = contents.get("properties").get(0);
+      List<Node> propEntries = propertiesNode.getChildren();
+      Assert.assertEquals(5, propEntries.size());
+      Assert.assertEquals("A", propEntries.get(0).getName());
+      Assert.assertEquals("B", propEntries.get(1).getName());
+      Assert.assertEquals("C", propEntries.get(2).getName());
+      Assert.assertEquals("D", propEntries.get(3).getName());
+      Assert.assertEquals("E", propEntries.get(4).getName());
+   }
+}
