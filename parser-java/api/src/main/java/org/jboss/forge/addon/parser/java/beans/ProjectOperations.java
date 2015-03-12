bca1d94661e993b8d60dfb4d543303f1d3e7fa78
George Gastaldi <gegastaldi@gmail.com>
2015-03-12 16:55:11 -0300

    FORGE-2228: Support adding methods in interfaces

--- parser-java/api/src/main/java/org/jboss/forge/addon/parser/java/beans/ProjectOperations.java
+++ parser-java/api/src/main/java/org/jboss/forge/addon/parser/java/beans/ProjectOperations.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.jboss.forge.addon.parser.java.beans;
 
 import java.io.FileNotFoundException;
@@ -9,13 +15,35 @@ import org.jboss.forge.addon.parser.java.resources.JavaResource;
 import org.jboss.forge.addon.parser.java.resources.JavaResourceVisitor;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.resource.visit.VisitContext;
+import org.jboss.forge.roaster.model.source.JavaAnnotationSource;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.JavaInterfaceSource;
 import org.jboss.forge.roaster.model.source.JavaSource;
 
 /**
+ * Operations related to java elements in a {@link Project}
+ * 
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
 public class ProjectOperations
 {
+   /**
+    * Returns all the {@link JavaInterfaceSource} objects from the given {@link Project}
+    */
+   public List<JavaResource> getProjectInterfaces(Project project)
+   {
+      final List<JavaResource> classes = new ArrayList<>();
+      if (project != null)
+      {
+         project.getFacet(JavaSourceFacet.class).visitJavaSources(new JavaInterfaceSourceVisitor(classes));
+      }
+      return classes;
+   }
+
+   /**
+    * Returns all the {@link JavaClassSource} objects from the given {@link Project}
+    */
    public List<JavaResource> getProjectClasses(Project project)
    {
       final List<JavaResource> classes = new ArrayList<>();
@@ -26,6 +54,9 @@ public class ProjectOperations
       return classes;
    }
 
+   /**
+    * Returns all the {@link JavaAnnotationSource} objects from the given {@link Project}
+    */
    public List<JavaResource> getProjectAnnotations(Project project)
    {
       final List<JavaResource> classes = new ArrayList<>();
@@ -52,6 +83,33 @@ public class ProjectOperations
          {
             JavaSource<?> javaType = resource.getJavaType();
             if (javaType.isClass())
+            {
+               classes.add(resource);
+            }
+         }
+         catch (FileNotFoundException e)
+         {
+            // ignore
+         }
+      }
+   }
+
+   private static class JavaInterfaceSourceVisitor extends JavaResourceVisitor
+   {
+      private final List<JavaResource> classes;
+
+      private JavaInterfaceSourceVisitor(List<JavaResource> classes)
+      {
+         this.classes = classes;
+      }
+
+      @Override
+      public void visit(VisitContext context, JavaResource resource)
+      {
+         try
+         {
+            JavaSource<?> javaType = resource.getJavaType();
+            if (javaType.isInterface())
             {
                classes.add(resource);
             }
