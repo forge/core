24b093bb15ae0e938b8c8dac5228fcf5a7ed00a3
George Gastaldi <gegastaldi@gmail.com>
2015-05-08 03:02:21 -0300

    Created AbstractProjectFacet as a base class for future project facets

---
+++ projects/api/src/main/java/org/jboss/forge/addon/projects/facets/AbstractProjectFacet.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.projects.facets;
+
+import org.jboss.forge.addon.facets.AbstractFacet;
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.projects.ProjectFacet;
+
+/**
+ * Abstract class for {@link ProjectFacet} implementations
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public abstract class AbstractProjectFacet extends AbstractFacet<Project> implements ProjectFacet
+{
+
+}
