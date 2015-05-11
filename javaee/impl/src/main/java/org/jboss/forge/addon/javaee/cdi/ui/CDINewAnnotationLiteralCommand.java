4001eb6a1213655d7ca0a04b190f96b228bdfe27
George Gastaldi <gegastaldi@gmail.com>
2015-05-11 14:39:18 -0300

    FORGE-2327: Added CDI: New Annotation Literal command

---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/cdi/ui/CDINewAnnotationLiteralCommand.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.javaee.cdi.ui;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.projects.Project;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.hints.InputType;
+import org.jboss.forge.addon.ui.input.UIInput;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.MethodSource;
+import org.jboss.forge.roaster.model.util.Types;
+
+/**
+ * Creates a new CDI Annotation Literal class
+ *
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class CDINewAnnotationLiteralCommand extends AbstractCDICommand<JavaClassSource>
+{
+
+   @Inject
+   @WithAttributes(label = "Qualifier", type = InputType.JAVA_CLASS_PICKER, required = true)
+   private UIInput<String> qualifier;
+
+   @Override
+   public Metadata getMetadata(UIContext context)
+   {
+      return Metadata.from(super.getMetadata(context), getClass())
+               .name("CDI: New Annotation Literal")
+               .description("Creates an Annotation Literal Type");
+   }
+
+   @Override
+   protected String getType()
+   {
+      return "CDI Annotation Literal Type";
+   }
+
+   @Override
+   protected Class<JavaClassSource> getSourceType()
+   {
+      return JavaClassSource.class;
+   }
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      super.initializeUI(builder);
+      builder.add(qualifier);
+   }
+
+   @Override
+   public JavaClassSource decorateSource(UIExecutionContext context, Project project, JavaClassSource annotationLiteral)
+            throws Exception
+   {
+      String qualifierValue = qualifier.getValue();
+      annotationLiteral.addImport(qualifierValue);
+      annotationLiteral.addImport(javax.enterprise.util.AnnotationLiteral.class);
+      annotationLiteral.setSuperType("AnnotationLiteral<" + Types.toSimpleName(qualifierValue) + ">").addInterface(
+               qualifierValue);
+      // Field
+      annotationLiteral.addField().setPrivate().setFinal(true).setType("Class<?>").setName("value");
+
+      // Constructor
+      MethodSource<JavaClassSource> constructor = annotationLiteral.addMethod().setConstructor(true).setPublic();
+      constructor.addParameter("Class<?>", "value");
+      constructor.setBody("this.value = value;");
+
+      // value() method
+      annotationLiteral.addMethod().setPublic().setReturnType("Class<?>").setName("value")
+               .setBody("return value;").addAnnotation(Override.class);
+      return annotationLiteral;
+   }
+}
--- javaee/tests/src/test/java/org/jboss/forge/addon/javaee/cdi/ui/CDINewAnnotationLiteralCommandTest.java
+++ javaee/tests/src/test/java/org/jboss/forge/addon/javaee/cdi/ui/CDINewAnnotationLiteralCommandTest.java
@@ -15,11 +15,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.lang.annotation.Inherited;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
-import javax.inject.Qualifier;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -36,7 +34,7 @@ import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.test.UITestHarness;
 import org.jboss.forge.arquillian.AddonDependencies;
 import org.jboss.forge.arquillian.archive.AddonArchive;
-import org.jboss.forge.roaster.model.JavaAnnotation;
+import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.junit.Assert;
 import org.junit.Before;
@@ -48,7 +46,7 @@ import org.junit.runner.RunWith;
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
 @RunWith(Arquillian.class)
-public class CDINewQualifierCommandTest
+public class CDINewAnnotationLiteralCommandTest
 {
    @Deployment
    @AddonDependencies
@@ -78,14 +76,14 @@ public class CDINewQualifierCommandTest
    @Test
    public void checkCommandMetadata() throws Exception
    {
-      try (CommandController controller = uiTestHarness.createCommandController(CDINewQualifierCommand.class,
+      try (CommandController controller = uiTestHarness.createCommandController(CDINewAnnotationLiteralCommand.class,
                project.getRoot()))
       {
          controller.initialize();
          // Checks the command metadata
-         assertTrue(controller.getCommand() instanceof CDINewQualifierCommand);
+         assertTrue(controller.getCommand() instanceof CDINewAnnotationLiteralCommand);
          UICommandMetadata metadata = controller.getMetadata();
-         assertEquals("CDI: New Qualifier", metadata.getName());
+         assertEquals("CDI: New Annotation Literal", metadata.getName());
          assertEquals("Java EE", metadata.getCategory().getName());
          assertEquals("CDI", metadata.getCategory().getSubCategory().getName());
          assertEquals(4, controller.getInputs().size());
@@ -93,7 +91,7 @@ public class CDINewQualifierCommandTest
          assertTrue(controller.hasInput("named"));
          assertTrue(controller.hasInput("targetPackage"));
          assertTrue(controller.hasInput("overwrite"));
-         assertTrue(controller.hasInput("inherited"));
+         assertTrue(controller.hasInput("qualifier"));
          assertTrue(controller.getValueFor("targetPackage").toString().endsWith(DEFAULT_CDI_PACKAGE));
       }
    }
@@ -102,21 +100,24 @@ public class CDINewQualifierCommandTest
    public void checkCommandShell() throws Exception
    {
       shellTest.getShell().setCurrentResource(project.getRoot());
-      Result result = shellTest.execute("cdi-new-qualifier --named Dummy", 10, TimeUnit.SECONDS);
+      Result result = shellTest
+               .execute("cdi-new-annotation-literal --named Dummy --qualifier javax.enterprise.inject.New", 10,
+                        TimeUnit.SECONDS);
 
       Assert.assertThat(result, not(instanceOf(Failed.class)));
       Assert.assertTrue(project.hasFacet(CDIFacet.class));
    }
 
    @Test
-   public void testCreateNewQualifier() throws Exception
+   public void testCreateNewAnnotationLiteral() throws Exception
    {
-      try (CommandController controller = uiTestHarness.createCommandController(CDINewQualifierCommand.class,
+      try (CommandController controller = uiTestHarness.createCommandController(CDINewAnnotationLiteralCommand.class,
                project.getRoot()))
       {
          controller.initialize();
-         controller.setValueFor("named", "MyQualifier");
+         controller.setValueFor("named", "MyAnnotationLiteral");
          controller.setValueFor("targetPackage", "org.jboss.forge.test");
+         controller.setValueFor("qualifier", "javax.enterprise.inject.New");
          Assert.assertTrue(controller.isValid());
          Assert.assertTrue(controller.canExecute());
          Result result = controller.execute();
@@ -124,36 +125,8 @@ public class CDINewQualifierCommandTest
       }
 
       JavaSourceFacet facet = project.getFacet(JavaSourceFacet.class);
-      JavaResource javaResource = facet.getJavaResource("org.jboss.forge.test.MyQualifier");
+      JavaResource javaResource = facet.getJavaResource("org.jboss.forge.test.MyAnnotationLiteral");
       Assert.assertNotNull(javaResource);
-      Assert.assertThat(javaResource.getJavaType(), is(instanceOf(JavaAnnotation.class)));
-      JavaAnnotation<?> ann = javaResource.getJavaType();
-      Assert.assertTrue(ann.hasAnnotation(Qualifier.class));
-      Assert.assertFalse(ann.hasAnnotation(Inherited.class));
-   }
-
-   @Test
-   public void testCreateNewQualifierInherited() throws Exception
-   {
-      try (CommandController controller = uiTestHarness.createCommandController(CDINewQualifierCommand.class,
-               project.getRoot()))
-      {
-         controller.initialize();
-         controller.setValueFor("named", "MyInheritedQualifier");
-         controller.setValueFor("targetPackage", "org.jboss.forge.test");
-         controller.setValueFor("inherited", Boolean.TRUE);
-         Assert.assertTrue(controller.isValid());
-         Assert.assertTrue(controller.canExecute());
-         Result result = controller.execute();
-         Assert.assertThat(result, is(not(instanceOf(Failed.class))));
-      }
-
-      JavaSourceFacet facet = project.getFacet(JavaSourceFacet.class);
-      JavaResource javaResource = facet.getJavaResource("org.jboss.forge.test.MyInheritedQualifier");
-      Assert.assertNotNull(javaResource);
-      Assert.assertThat(javaResource.getJavaType(), is(instanceOf(JavaAnnotation.class)));
-      JavaAnnotation<?> ann = javaResource.getJavaType();
-      Assert.assertTrue(ann.hasAnnotation(Qualifier.class));
-      Assert.assertTrue(ann.hasAnnotation(Inherited.class));
+      Assert.assertThat(javaResource.getJavaType(), is(instanceOf(JavaClassSource.class)));
    }
 }
