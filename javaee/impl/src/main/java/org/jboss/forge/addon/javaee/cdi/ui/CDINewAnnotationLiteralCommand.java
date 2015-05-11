db177dd2ad5ec6e734ce47ec7b95ca5724091877
George Gastaldi <gegastaldi@gmail.com>
2015-05-11 16:56:53 -0300

    FORGE-2327: Added UICompleter to qualifiers

--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/cdi/ui/CDINewAnnotationLiteralCommand.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/cdi/ui/CDINewAnnotationLiteralCommand.java
@@ -7,21 +7,30 @@
 
 package org.jboss.forge.addon.javaee.cdi.ui;
 
+import java.io.FileNotFoundException;
 import java.lang.reflect.Method;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
+import org.jboss.forge.addon.javaee.cdi.CDIOperations;
+import org.jboss.forge.addon.parser.java.resources.JavaResource;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.facets.ClassLoaderFacet;
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.hints.InputType;
+import org.jboss.forge.addon.ui.input.InputComponent;
+import org.jboss.forge.addon.ui.input.UICompleter;
 import org.jboss.forge.addon.ui.input.UIInput;
 import org.jboss.forge.addon.ui.metadata.WithAttributes;
 import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.furnace.util.Strings;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
+import org.jboss.forge.roaster.model.source.JavaSource;
 import org.jboss.forge.roaster.model.source.MethodSource;
 
 /**
@@ -34,6 +43,9 @@ public class CDINewAnnotationLiteralCommand extends AbstractCDICommand<JavaClass
    @Inject
    @WithAttributes(label = "Qualifier", type = InputType.JAVA_CLASS_PICKER, required = true)
    private UIInput<String> qualifier;
+
+   @Inject
+   private CDIOperations cdiOperations;
 
    @Override
    public Metadata getMetadata(UIContext context)
@@ -59,7 +71,48 @@ public class CDINewAnnotationLiteralCommand extends AbstractCDICommand<JavaClass
    public void initializeUI(UIBuilder builder) throws Exception
    {
       super.initializeUI(builder);
+      setupQualifiers();
       builder.add(qualifier);
+   }
+
+   private void setupQualifiers()
+   {
+      qualifier.setCompleter(new UICompleter<String>()
+      {
+         @Override
+         public Iterable<String> getCompletionProposals(final UIContext context, final InputComponent<?, String> input,
+                  final String value)
+         {
+            final Project project = getSelectedProject(context);
+            final List<String> options = new ArrayList<>();
+            for (String type : CDIOperations.DEFAULT_QUALIFIERS)
+            {
+               if (Strings.isNullOrEmpty(value) || type.startsWith(value))
+               {
+                  options.add(type);
+               }
+            }
+            if (project != null)
+            {
+               for (JavaResource resource : cdiOperations.getProjectQualifiers(project))
+               {
+                  try
+                  {
+                     JavaSource<?> javaSource = resource.getJavaType();
+                     String qualifiedName = javaSource.getQualifiedName();
+                     if (Strings.isNullOrEmpty(value) || qualifiedName.startsWith(value))
+                     {
+                        options.add(qualifiedName);
+                     }
+                  }
+                  catch (FileNotFoundException ignored)
+                  {
+                  }
+               }
+            }
+            return options;
+         }
+      });
    }
 
    @Override
