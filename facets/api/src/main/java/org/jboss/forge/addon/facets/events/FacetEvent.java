8926c4a92eec6ee39f5b6b3a06a635f37de054de
George Gastaldi <gegastaldi@gmail.com>
2015-06-17 08:25:30 -0300

    FORGE-2319: Support for Facet events

---
+++ facets/api/src/main/java/org/jboss/forge/addon/facets/events/FacetEvent.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.facets.events;
+
+import org.jboss.forge.addon.facets.Facet;
+
+/**
+ * Super interface for {@link Facet} events (installation, removal, etc)
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public interface FacetEvent
+{
+   /**
+    * @return the facet
+    */
+   Facet<?> getFacet();
+}
