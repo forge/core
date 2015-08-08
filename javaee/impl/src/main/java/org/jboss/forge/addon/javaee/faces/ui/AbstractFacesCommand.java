e925f28fc24ab9f7838a8a8998d729e00cb82d65
Antonio Goncalves <antonio.goncalves@gmail.com>
2015-08-08 20:04:36 +0200

    FORGE-2419: Refactoring the JSF Commands Adding a first test class

--- javaee/impl/src/main/java/org/jboss/forge/addon/javaee/faces/ui/AbstractFacesCommand.java
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/faces/ui/AbstractFacesCommand.java
@@ -6,39 +6,59 @@
  */
 package org.jboss.forge.addon.javaee.faces.ui;
 
+import static org.jboss.forge.addon.javaee.JavaEEPackageConstants.DEFAULT_FACES_PACKAGE;
+
+import org.jboss.forge.addon.javaee.cdi.CDIFacet;
+import org.jboss.forge.addon.javaee.cdi.ui.CDISetupCommand;
 import org.jboss.forge.addon.javaee.faces.FacesFacet;
-import org.jboss.forge.addon.javaee.ui.AbstractJavaEECommand;
+import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
+import org.jboss.forge.addon.parser.java.ui.AbstractJavaSourceCommand;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.ui.command.PrerequisiteCommandsProvider;
 import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.result.NavigationResult;
 import org.jboss.forge.addon.ui.result.navigation.NavigationResultBuilder;
+import org.jboss.forge.addon.ui.util.Categories;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.roaster.model.source.JavaSource;
 
 /**
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
  * 
  */
-public abstract class AbstractFacesCommand extends AbstractJavaEECommand implements PrerequisiteCommandsProvider
-{
+public abstract class AbstractFacesCommand<T extends JavaSource<?>> extends AbstractJavaSourceCommand<T>
+        implements PrerequisiteCommandsProvider {
 
-   public AbstractFacesCommand()
-   {
-      super();
+   @Override
+   public Metadata getMetadata(UIContext context) {
+      return Metadata.from(super.getMetadata(context), getClass())
+              .category(Categories.create(Categories.create("Java EE"), "JSF"));
    }
 
    @Override
-   public NavigationResult getPrerequisiteCommands(UIContext context)
-   {
+   protected boolean isProjectRequired() {
+      return true;
+   }
+
+   @Override
+   protected String calculateDefaultPackage(UIContext context) {
+      return getSelectedProject(context).getFacet(JavaSourceFacet.class).getBasePackage() + "."
+              + DEFAULT_FACES_PACKAGE;
+   }
+
+   @Override
+   public NavigationResult getPrerequisiteCommands(UIContext context) {
       NavigationResultBuilder builder = NavigationResultBuilder.create();
       Project project = getSelectedProject(context);
-      if (project != null)
-      {
-         if (!project.hasFacet(FacesFacet.class))
-         {
+      if (project != null) {
+         if (!project.hasFacet(CDIFacet.class)) {
+            builder.add(CDISetupCommand.class);
+         }
+         if (!project.hasFacet(FacesFacet.class)) {
             builder.add(FacesSetupWizardImpl.class);
          }
       }
       return builder.build();
-   }
 
+   }
 }
