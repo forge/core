58ec6417ed7fd2c4fe88e109a25074dff5fdcf05
George Gastaldi <gegastaldi@gmail.com>
2015-08-20 14:35:33 -0300

    FORGE-2446: PackageRootConverter is now explicitly set

--- database-tools/impl/src/main/java/org/jboss/forge/addon/database/tools/generate/GenerateEntitiesCommand.java
+++ database-tools/impl/src/main/java/org/jboss/forge/addon/database/tools/generate/GenerateEntitiesCommand.java
@@ -15,6 +15,7 @@ import org.jboss.forge.addon.database.tools.connections.ConnectionProfileManager
 import org.jboss.forge.addon.database.tools.util.HibernateToolsHelper;
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 import org.jboss.forge.addon.javaee.jpa.JPAFacet;
+import org.jboss.forge.addon.parser.java.converters.PackageRootConverter;
 import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
 import org.jboss.forge.addon.parser.java.resources.JavaResource;
 import org.jboss.forge.addon.parser.java.resources.JavaResourceVisitor;
@@ -66,6 +67,7 @@ public class GenerateEntitiesCommand extends AbstractProjectCommand implements
       targetPackage = factory.createInput("targetPackage", String.class).setLabel("Target package")
                .setDescription("The name of the target package in which to generate the entities").setRequired(true);
       targetPackage.getFacet(HintsFacet.class).setInputType(InputType.JAVA_PACKAGE_PICKER);
+      targetPackage.setValueConverter(new PackageRootConverter(getProjectFactory(), builder));
 
       connectionProfile = factory.createSelectOne("connectionProfile", String.class).setLabel("Connection Profile")
                .setDescription("Select the database connection profile you want to use");
