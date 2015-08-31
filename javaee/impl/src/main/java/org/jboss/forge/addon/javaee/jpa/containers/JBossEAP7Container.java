99dc3f8cf984cb0de3c2c06453b2a59226cfc696
George Gastaldi <gegastaldi@gmail.com>
2015-08-31 12:08:37 -0300

    FORGE-2462: Added JBoss EAP 7.x as a JPA container

---
+++ javaee/impl/src/main/java/org/jboss/forge/addon/javaee/jpa/containers/JBossEAP7Container.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.javaee.jpa.containers;
+
+import org.jboss.forge.addon.javaee.jpa.DatabaseType;
+
+/**
+ * Support for JBoss EAP 7.x container
+ * 
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class JBossEAP7Container extends JavaEEDefaultContainer
+{
+   private static final String EXAMPLE_DS = "java:jboss/datasources/ExampleDS";
+
+   @Override
+   public DatabaseType getDefaultDatabaseType()
+   {
+      return DatabaseType.H2;
+   }
+
+   @Override
+   public String getDefaultDataSource()
+   {
+      return EXAMPLE_DS;
+   }
+
+   @Override
+   public String getName(boolean isGUI)
+   {
+      return isGUI ? "JBoss Enterprise Application Platform 7.x" : "JBOSS_EAP7";
+   }
+}
