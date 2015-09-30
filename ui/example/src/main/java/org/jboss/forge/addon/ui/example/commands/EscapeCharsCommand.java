cd70ec20321fe42619b7c1f5d1fe088dba34019a
George Gastaldi <gegastaldi@gmail.com>
2015-09-30 18:17:34 -0300

    FORGE-552: Added EscapeCharsCommand for testing purposes

---
+++ ui/example/src/main/java/org/jboss/forge/addon/ui/example/commands/EscapeCharsCommand.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.ui.example.commands;
+
+import java.util.Arrays;
+
+import javax.inject.Inject;
+
+import org.jboss.forge.addon.ui.command.AbstractUICommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.input.UISelectOne;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
+import org.jboss.forge.addon.ui.metadata.WithAttributes;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Metadata;
+
+/**
+ *
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class EscapeCharsCommand extends AbstractUICommand
+{
+   @Inject
+   @WithAttributes(label = "Input", required = true)
+   private UISelectOne<String> input;
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+      input.setValueChoices(Arrays.asList("A&B", "B & C", "C & D"));
+      builder.add(input);
+   }
+
+   @Override
+   public Result execute(UIExecutionContext context) throws Exception
+   {
+      return Results.success(input.getValue());
+   }
+
+   @Override
+   public UICommandMetadata getMetadata(UIContext context)
+   {
+      return Metadata.forCommand(getClass()).name("escape-chars");
+   }
+}
