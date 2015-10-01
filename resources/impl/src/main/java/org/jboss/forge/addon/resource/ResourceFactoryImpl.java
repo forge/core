684ba4db06b01bc2e47f790655608d87e004f3cb
George Gastaldi <gegastaldi@gmail.com>
2015-10-01 17:36:39 -0300

    FORGE-2496: No need of synchronized blocks when using Imported

--- resources/impl/src/main/java/org/jboss/forge/addon/resource/ResourceFactoryImpl.java
+++ resources/impl/src/main/java/org/jboss/forge/addon/resource/ResourceFactoryImpl.java
@@ -6,8 +6,6 @@
  */
 package org.jboss.forge.addon.resource;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.TreeMap;
 
 import org.jboss.forge.addon.resource.monitor.FileMonitor;
@@ -16,8 +14,8 @@ import org.jboss.forge.addon.resource.transaction.ResourceTransactionListener;
 import org.jboss.forge.addon.resource.transaction.file.FileResourceTransactionImpl;
 import org.jboss.forge.addon.resource.transaction.file.FileResourceTransactionManager;
 import org.jboss.forge.addon.resource.util.RelatedClassComparator;
-import org.jboss.forge.furnace.addons.AddonRegistry;
 import org.jboss.forge.furnace.container.simple.lifecycle.SimpleContainer;
+import org.jboss.forge.furnace.services.Imported;
 import org.jboss.forge.furnace.spi.ListenerRegistration;
 import org.jboss.forge.furnace.util.Assert;
 
@@ -28,58 +26,37 @@ import org.jboss.forge.furnace.util.Assert;
  * @author Mike Brock <cbrock@redhat.com>
  * @author <a href="ggastald@redhat.com">George Gastaldi</a>
  */
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class ResourceFactoryImpl implements ResourceFactory
 {
-   @SuppressWarnings("rawtypes")
-   private final Set<ResourceGenerator> generators = new HashSet<>();
-   private long version = -1;
+   private Imported<ResourceGenerator> generators;
 
    @Override
-   @SuppressWarnings({ "unchecked", "rawtypes" })
    public <E, T extends Resource<E>> T create(final Class<T> type, final E underlyingResource)
    {
       T result = null;
-      synchronized (this)
-      {
-         TreeMap<Class<?>, ResourceGenerator> generated = new TreeMap<>(
-                  new RelatedClassComparator());
+      TreeMap<Class<?>, ResourceGenerator> generated = new TreeMap<>(
+               new RelatedClassComparator());
 
-         for (ResourceGenerator generator : getGenerators())
+      for (ResourceGenerator generator : getGenerators())
+      {
+         if (generator.handles(type, underlyingResource))
          {
-            if (generator.handles(type, underlyingResource))
+            Class resourceType = generator.getResourceType(this, type, underlyingResource);
+            if (type.isAssignableFrom(resourceType))
             {
-               Class resourceType = generator.getResourceType(this, type, underlyingResource);
-               if (type.isAssignableFrom(resourceType))
-               {
-                  generated.put(resourceType, generator);
-               }
+               generated.put(resourceType, generator);
             }
          }
-         if (generated.size() > 0)
-         {
-            result = (T) generated.lastEntry().getValue().getResource(this, type, underlyingResource);
-         }
+      }
+      if (generated.size() > 0)
+      {
+         result = (T) generated.lastEntry().getValue().getResource(this, type, underlyingResource);
       }
       return result;
    }
 
-   @SuppressWarnings("rawtypes")
-   private Iterable<ResourceGenerator> getGenerators()
-   {
-      if (getAddonRegistry().getVersion() != version)
-      {
-         version = getAddonRegistry().getVersion();
-         generators.clear();
-         for (ResourceGenerator generator : getAddonRegistry().getServices(ResourceGenerator.class))
-         {
-            generators.add(generator);
-         }
-      }
-      return generators;
-   }
-
    @Override
-   @SuppressWarnings("unchecked")
    public <E> Resource<E> create(E underlyingResource)
    {
       return create(Resource.class, underlyingResource);
@@ -131,9 +108,12 @@ public class ResourceFactoryImpl implements ResourceFactory
       return getTransactionManager().addTransactionListener(listener);
    }
 
-   private AddonRegistry getAddonRegistry()
+   private Imported<ResourceGenerator> getGenerators()
    {
-      return SimpleContainer.getFurnace(getClass().getClassLoader()).getAddonRegistry();
+      if (generators == null)
+         generators = SimpleContainer.getServices(getClass().getClassLoader(),
+                  ResourceGenerator.class);
+      return generators;
    }
 
    private FileMonitor getFileMonitor()
