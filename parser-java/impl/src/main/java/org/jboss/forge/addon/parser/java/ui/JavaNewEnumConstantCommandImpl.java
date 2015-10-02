cad555674559a17d1119fe3cfa80e1b7580ada3b
George Gastaldi <gegastaldi@gmail.com>
2015-10-02 14:29:50 -0300

    JavaNewEnumConstantCommandImpl uses UIInput instead of UISelectOne

--- parser-java/impl/src/main/java/org/jboss/forge/addon/parser/java/ui/JavaNewEnumConstantCommandImpl.java
+++ parser-java/impl/src/main/java/org/jboss/forge/addon/parser/java/ui/JavaNewEnumConstantCommandImpl.java
@@ -22,8 +22,8 @@ import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.context.UISelection;
 import org.jboss.forge.addon.ui.context.UIValidationContext;
+import org.jboss.forge.addon.ui.input.UIInput;
 import org.jboss.forge.addon.ui.input.UIInputMany;
-import org.jboss.forge.addon.ui.input.UISelectOne;
 import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.metadata.WithAttributes;
 import org.jboss.forge.addon.ui.result.Result;
@@ -43,7 +43,7 @@ public class JavaNewEnumConstantCommandImpl extends AbstractProjectCommand imple
 
    @Inject
    @WithAttributes(label = "Enum Class", required = true)
-   private UISelectOne<JavaResource> enumClass;
+   private UIInput<JavaResource> enumClass;
 
    @Inject
    @WithAttributes(label = "Enum Constants", required = true)
