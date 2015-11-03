edcf67b9cf24c31050ec6255c27e87d153ef7978
George Gastaldi <gegastaldi@gmail.com>
2015-11-03 21:03:23 -0200

    FORGE-2524: Added test

--- ui/tests/src/test/java/org/jboss/forge/addon/ui/input/InputComponentsTest.java
+++ ui/tests/src/test/java/org/jboss/forge/addon/ui/input/InputComponentsTest.java
@@ -7,13 +7,20 @@
 
 package org.jboss.forge.addon.ui.input;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+
+import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
 
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.forge.addon.convert.ConverterFactory;
 import org.jboss.forge.addon.ui.util.InputComponents;
+import org.jboss.forge.furnace.util.Lists;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +33,9 @@ import org.junit.runner.RunWith;
 public class InputComponentsTest
 {
    @Inject
+   private UIInputMany<File> files;
+
+   @Inject
    private UISelectOne<String> values;
 
    @Inject
@@ -37,5 +47,14 @@ public class InputComponentsTest
       values.setValueChoices(Arrays.asList("A", "B", "C"));
       InputComponents.setValueFor(converterFactory, values, "D");
       Assert.assertNull(values.getValue());
+   }
+
+   @Test
+   public void testUIInputManyFileSingleList()
+   {
+      InputComponents.setValueFor(converterFactory, files, OperatingSystemUtils.getTempDirectory());
+      List<File> list = Lists.toList(files.getValue());
+      Assert.assertThat(list.size(), equalTo(1));
+      Assert.assertThat(list, hasItem(OperatingSystemUtils.getTempDirectory()));
    }
 }
