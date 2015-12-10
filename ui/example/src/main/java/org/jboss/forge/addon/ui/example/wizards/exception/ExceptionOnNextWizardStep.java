8b5236bfae51ddf469fa92b3dccd09c9c9073397
George Gastaldi <gegastaldi@gmail.com>
2015-12-10 11:33:01 -0200

    Added examples throwing exceptions

---
+++ ui/example/src/main/java/org/jboss/forge/addon/ui/example/wizards/exception/ExceptionOnNextWizardStep.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.ui.example.wizards.exception;
+
+import org.jboss.forge.addon.ui.command.AbstractUICommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.context.UINavigationContext;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
+import org.jboss.forge.addon.ui.result.NavigationResult;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.addon.ui.wizard.UIWizardStep;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class ExceptionOnNextWizardStep extends AbstractUICommand implements UIWizardStep
+{
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+   }
+
+   @Override
+   public UICommandMetadata getMetadata(UIContext context)
+   {
+      return Metadata.forCommand(ExceptionOnNextWizardStep.class).name("exception-on-next")
+               .description("Tests Exceptions");
+   }
+
+   @Override
+   public Result execute(UIExecutionContext context) throws Exception
+   {
+      return Results.success();
+   }
+
+   @Override
+   public NavigationResult next(UINavigationContext context) throws Exception
+   {
+      throw new Exception("Exception on Next!");
+   }
+
+}
---
+++ ui/example/src/main/java/org/jboss/forge/addon/ui/example/wizards/exception/FirstExceptionOnNextWizard.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.jboss.forge.addon.ui.example.wizards.exception;
+
+import org.jboss.forge.addon.ui.command.AbstractUICommand;
+import org.jboss.forge.addon.ui.context.UIBuilder;
+import org.jboss.forge.addon.ui.context.UIContext;
+import org.jboss.forge.addon.ui.context.UIExecutionContext;
+import org.jboss.forge.addon.ui.context.UINavigationContext;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
+import org.jboss.forge.addon.ui.result.NavigationResult;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.result.navigation.NavigationResultBuilder;
+import org.jboss.forge.addon.ui.util.Metadata;
+import org.jboss.forge.addon.ui.wizard.UIWizard;
+
+/**
+ * 
+ * @author <a href="ggastald@redhat.com">George Gastaldi</a>
+ */
+public class FirstExceptionOnNextWizard extends AbstractUICommand implements UIWizard
+{
+
+   @Override
+   public void initializeUI(UIBuilder builder) throws Exception
+   {
+   }
+
+   @Override
+   public UICommandMetadata getMetadata(UIContext context)
+   {
+      return Metadata.forCommand(FirstExceptionOnNextWizard.class).name("exception-first-on-next")
+               .description("Tests Exceptions");
+   }
+
+   @Override
+   public Result execute(UIExecutionContext context) throws Exception
+   {
+      return Results.success();
+   }
+
+   @Override
+   public NavigationResult next(UINavigationContext context) throws Exception
+   {
+      return NavigationResultBuilder.create().add(ExceptionOnNextWizardStep.class).build();
+   }
+
+}
