8a48ca87b3c953da03ebce69ff491dc650e7f0b5
George Gastaldi <gegastaldi@gmail.com>
2016-01-05 21:49:36 -0200

    Added @Repeatable to @FacetConstraint

--- facets/api/src/main/java/org/jboss/forge/addon/facets/constraints/FacetConstraint.java
+++ facets/api/src/main/java/org/jboss/forge/addon/facets/constraints/FacetConstraint.java
@@ -9,6 +9,7 @@ package org.jboss.forge.addon.facets.constraints;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -24,6 +25,7 @@ import org.jboss.forge.addon.facets.Faceted;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Repeatable(FacetConstraints.class)
 public @interface FacetConstraint
 {
    /**
--- facets/tests/src/main/java/test/org/jboss/forge/addon/facets/constraints/FacetN.java
+++ facets/tests/src/main/java/test/org/jboss/forge/addon/facets/constraints/FacetN.java
@@ -2,14 +2,11 @@ package test.org.jboss.forge.addon.facets.constraints;
 
 import org.jboss.forge.addon.facets.constraints.FacetConstraint;
 import org.jboss.forge.addon.facets.constraints.FacetConstraintType;
-import org.jboss.forge.addon.facets.constraints.FacetConstraints;
 
 import test.org.jboss.forge.addon.facets.factory.MockFacet;
 
-@FacetConstraints({
-         @FacetConstraint(value = FacetP.class, type = FacetConstraintType.REQUIRED),
-         @FacetConstraint(value = FacetO.class, type = FacetConstraintType.OPTIONAL)
-})
+@FacetConstraint(value = FacetP.class, type = FacetConstraintType.REQUIRED)
+@FacetConstraint(value = FacetO.class, type = FacetConstraintType.OPTIONAL)
 public class FacetN extends MockFacet
 {
    @Override
