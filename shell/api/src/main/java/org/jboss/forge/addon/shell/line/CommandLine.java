42d1903138b666693d7fde80c88022f92fc0a244
George Gastaldi <gegastaldi@gmail.com>
2016-01-12 16:47:31 -0200

    FORGE-2574: Improved javadoc

--- shell/api/src/main/java/org/jboss/forge/addon/shell/line/CommandLine.java
+++ shell/api/src/main/java/org/jboss/forge/addon/shell/line/CommandLine.java
@@ -17,17 +17,25 @@ import java.util.List;
 public interface CommandLine
 {
    /**
+    * An argument is the unnamed option part of a command.
+    * 
+    * Eg: <code>git push</code>. <i>push</i> is the argument
+    * 
     * @return the arguments for a given command line
     */
    CommandOption getArgument();
 
    /**
+    * An option is a named parameter.
+    * 
+    * Eg: <code>foo --name george</code>
+    * 
     * @return the parsed options from the command line
     */
    List<CommandOption> getOptions();
 
    /**
-    * @return <code>true</code> if any parameter is set
+    * @return <code>true</code> if any option (or argument) is set
     */
    default boolean hasParameters()
    {
