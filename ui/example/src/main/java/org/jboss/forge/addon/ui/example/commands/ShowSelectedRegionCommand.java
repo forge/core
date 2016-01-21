1e6666429888d47684b3f76df4b5f775ace27bbd
George Gastaldi <gegastaldi@gmail.com>
2016-01-21 08:51:29 -0200

    FORGE-2585: Fixed command name

--- ui/example/src/main/java/org/jboss/forge/addon/ui/example/commands/ShowSelectedRegionCommand.java
+++ ui/example/src/main/java/org/jboss/forge/addon/ui/example/commands/ShowSelectedRegionCommand.java
@@ -15,9 +15,12 @@ import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.context.UIRegion;
 import org.jboss.forge.addon.ui.context.UISelection;
+import org.jboss.forge.addon.ui.metadata.UICommandMetadata;
 import org.jboss.forge.addon.ui.output.UIOutput;
 import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.util.Categories;
+import org.jboss.forge.addon.ui.util.Metadata;
 
 /**
  * Displays the selected {@link UIRegion} for the selected resources
@@ -26,6 +29,13 @@ import org.jboss.forge.addon.ui.result.Results;
  */
 public class ShowSelectedRegionCommand implements UICommand
 {
+
+   @Override
+   public UICommandMetadata getMetadata(UIContext context)
+   {
+      return Metadata.forCommand(getClass()).name("Show Selected Region")
+               .category(Categories.create("UI", "Example"));
+   }
 
    @Override
    public Result execute(UIExecutionContext context) throws Exception
