e6c8dd80d4e687470671889f4d594f1de73606d8
James Strachan <james.strachan@gmail.com>
2016-01-26 13:51:12 +0000

    FORGE-2590 fix license headers, whoops!

--- maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/archetype/ui/ConstantArchetypeSelectionWizardStep.java
+++ maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/archetype/ui/ConstantArchetypeSelectionWizardStep.java
@@ -1,18 +1,8 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
  */
 package org.jboss.forge.addon.maven.projects.archetype.ui;
 
