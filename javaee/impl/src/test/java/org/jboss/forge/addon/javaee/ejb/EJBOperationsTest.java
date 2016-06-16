089f82892180336b5906e9b7229ab01f7dbfb7fc
George Gastaldi <gegastaldi@gmail.com>
2016-06-16 17:19:15 -0300

    Improved EJBOperationsTest

--- javaee/impl/src/test/java/org/jboss/forge/addon/javaee/ejb/EJBOperationsTest.java
+++ javaee/impl/src/test/java/org/jboss/forge/addon/javaee/ejb/EJBOperationsTest.java
@@ -7,12 +7,14 @@
 
 package org.jboss.forge.addon.javaee.ejb;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 
 import javax.ejb.MessageDriven;
 
 import org.jboss.forge.roaster.Roaster;
+import org.jboss.forge.roaster.model.source.AnnotationSource;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.junit.Assert;
 import org.junit.Test;
@@ -38,6 +40,14 @@ public class EJBOperationsTest
       ejbOp.setupMessageDrivenBean(mdb, JMSDestinationType.QUEUE, "queue/foo");
       Assert.assertThat(mdb.hasAnnotation(MessageDriven.class), is(true));
       Assert.assertThat(mdb.getInterfaces(), hasItem("javax.jms.MessageListener"));
+      AnnotationSource<JavaClassSource> annotation = mdb.getAnnotation(MessageDriven.class);
+      AnnotationSource<JavaClassSource> configs[] = annotation.getAnnotationArrayValue("activationConfig");
+
+      Assert.assertThat(configs[0].getStringValue("propertyName"), equalTo("destinationType"));
+      Assert.assertThat(configs[0].getStringValue("propertyValue"), equalTo("javax.jms.Queue"));
+
+      Assert.assertThat(configs[1].getStringValue("propertyName"), equalTo("destination"));
+      Assert.assertThat(configs[1].getStringValue("propertyValue"), equalTo("queue/foo"));
    }
 
 }
