58a458f963eac5a76e5d027bf96c89eb951c1612
George Gastaldi <gegastaldi@gmail.com>
2016-12-01 01:54:35 -0200

    FORGE-2705: Removed bogus import

--- maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/archetype/ui/AbstractArchetypeSelectionWizardStep.java
+++ maven/impl-projects/src/main/java/org/jboss/forge/addon/maven/projects/archetype/ui/AbstractArchetypeSelectionWizardStep.java
@@ -11,7 +11,6 @@ import org.jboss.forge.addon.dependencies.DependencyRepository;
 import org.jboss.forge.addon.dependencies.DependencyResolver;
 import org.jboss.forge.addon.dependencies.builder.DependencyQueryBuilder;
 import org.jboss.forge.addon.maven.archetype.ArchetypeHelper;
-import org.jboss.forge.addon.maven.projects.archetype.ArchetypeHelper;
 import org.jboss.forge.addon.parser.java.facets.JavaSourceFacet;
 import org.jboss.forge.addon.projects.Project;
 import org.jboss.forge.addon.projects.facets.MetadataFacet;
