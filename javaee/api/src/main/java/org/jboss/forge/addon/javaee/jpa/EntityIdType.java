f7d7eafa7bdc697c34075cd83ddbcad9aeb742b8
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2018-03-23 12:23:56 +0100

    FORGE-1525: Update year

--- javaee/api/src/main/java/org/jboss/forge/addon/javaee/jpa/EntityIdType.java
+++ javaee/api/src/main/java/org/jboss/forge/addon/javaee/jpa/EntityIdType.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2018 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Eclipse Public License version 1.0, available at
  * http://www.eclipse.org/legal/epl-v10.html
