5f23cdbe72ce89a493ae9d587a3914e3a5823df3
George Gastaldi <gegastaldi@gmail.com>
2018-05-02 19:06:33 +0000

    Removed unused code

--- text/src/test/java/org/jboss/forge/addon/text/highlight/scanner/AbstractScannerTestCase.java
+++ text/src/test/java/org/jboss/forge/addon/text/highlight/scanner/AbstractScannerTestCase.java
@@ -25,8 +25,9 @@ public abstract class AbstractScannerTestCase
 
    private static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 
-   public static final String ASSERT_ENCODER = "TEST";
-   {
+   static final String ASSERT_ENCODER = "TEST";
+
+   static {
       Syntax.builtIns();
       Encoder.Factory.registrer(ASSERT_ENCODER, AssertEncoder.class);
    }
@@ -83,48 +84,5 @@ public abstract class AbstractScannerTestCase
          }
       }
       Assert.assertEquals(expectedContent, result);
-   }
-
-   static byte[] asByteArray(final InputStream in) throws IllegalArgumentException
-   {
-      // Precondition check
-      if (in == null)
-      {
-         throw new IllegalArgumentException("stream must be specified");
-      }
-
-      // Get content as an array of bytes
-      final ByteArrayOutputStream out = new ByteArrayOutputStream(8192);
-      final int len = 4096;
-      final byte[] buffer = new byte[len];
-      int read = 0;
-      try
-      {
-         while (((read = in.read(buffer)) != -1))
-         {
-            out.write(buffer, 0, read);
-         }
-      }
-      catch (final IOException ioe)
-      {
-         throw new RuntimeException("Error in obtainting bytes from " + in, ioe);
-      }
-      finally
-      {
-         try
-         {
-            in.close();
-         }
-         catch (final IOException ignore)
-         {
-         }
-         // We don't need to close the outstream, it's a byte array out
-      }
-
-      // Represent as byte array
-      final byte[] content = out.toByteArray();
-
-      // Return
-      return content;
    }
 }
