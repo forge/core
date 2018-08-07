084ef8de658157abfc2304f090b84239eeefbe8f
George Gastaldi <gegastaldi@gmail.com>
2018-08-07 16:36:23 -0300

    Introduce ScriptOperations

---
+++ script/src/main/java/org/jboss/forge/addon/script/ScriptOperations.java
@@ -0,0 +1,60 @@
+package org.jboss.forge.addon.script;
+
+import java.io.File;
+import java.io.PrintStream;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import org.jboss.forge.addon.resource.DirectoryResource;
+import org.jboss.forge.addon.resource.ResourceFactory;
+import org.jboss.forge.addon.script.impl.ForgeScriptEngineFactory;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.furnace.Furnace;
+import org.jboss.forge.furnace.addons.AddonRegistry;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+public class ScriptOperations
+{
+   private ResourceFactory resourceFactory;
+   private ForgeScriptEngineFactory engineFactory;
+
+   public ScriptOperations()
+   {
+   }
+
+   public Result executeScript(File currentDir, String script, PrintStream stdout, PrintStream stderr) throws ScriptException
+   {
+      ScriptEngine scriptEngine = getEngineFactory().getScriptEngine();
+      DirectoryResource resource = getResourceFactory().create(DirectoryResource.class, currentDir);
+      ScriptContext context = ScriptContextBuilder.create()
+               .currentResource(resource)
+               .stdout(stdout)
+               .stderr(stderr)
+               .build();
+      return (Result) scriptEngine.eval(script, context);
+   }
+
+   private ForgeScriptEngineFactory getEngineFactory()
+   {
+      if (engineFactory == null)
+      {
+         AddonRegistry addonRegistry = Furnace.instance(getClass().getClassLoader()).getAddonRegistry();
+         this.engineFactory = addonRegistry.getServices(ForgeScriptEngineFactory.class).get();
+      }
+      return engineFactory;
+   }
+
+   private ResourceFactory getResourceFactory()
+   {
+      if (resourceFactory == null)
+      {
+         AddonRegistry addonRegistry = Furnace.instance(getClass().getClassLoader()).getAddonRegistry();
+         this.resourceFactory = addonRegistry.getServices(ResourceFactory.class).get();
+      }
+      return resourceFactory;
+   }
+}
---
+++ script/src/test/java/org/jboss/forge/addon/script/ScriptOperationsTest.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.forge.addon.script;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.forge.addon.ui.result.Failed;
+import org.jboss.forge.addon.ui.result.Result;
+import org.jboss.forge.furnace.container.simple.lifecycle.SimpleContainer;
+import org.jboss.forge.furnace.util.OperatingSystemUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+/**
+ * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
+ */
+@RunWith(Arquillian.class)
+public class ScriptOperationsTest
+{
+   protected ScriptOperations scriptOperations;
+
+   @Before
+   public void setUp()
+   {
+      scriptOperations = SimpleContainer.getServices(getClass().getClassLoader(), ScriptOperations.class).get();
+   }
+
+   @Test
+   public void testLookupScriptOperationsThroughContainer()
+   {
+      Assert.assertThat(scriptOperations, notNullValue());
+   }
+
+   @Test
+   public void testLookupScriptEngineFactoryThroughScriptEngineManager() throws Exception
+   {
+      Result result = scriptOperations.executeScript(OperatingSystemUtils.getUserHomeDir(), "pwd", null, null);
+      Assert.assertThat(result, not(instanceOf(Failed.class)));
+   }
+}
