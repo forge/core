851c9e64866b0a6fd31afe3b2091a4613092008a
George Gastaldi <gegastaldi@gmail.com>
2018-08-08 00:19:17 -0300

    FORGE-2785: Wait until wizard command is finished before proceeding

--- script/src/main/java/org/jboss/forge/addon/script/impl/ScriptCommandListener.java
+++ script/src/main/java/org/jboss/forge/addon/script/impl/ScriptCommandListener.java
@@ -13,10 +13,35 @@ import org.jboss.forge.addon.ui.context.UIContext;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.result.Results;
+import org.jboss.forge.addon.ui.wizard.UIWizard;
+import org.jboss.forge.addon.ui.wizard.WizardExecutionListener;
 
-class ScriptCommandListener extends AbstractCommandExecutionListener implements CommandNotFoundListener
+class ScriptCommandListener extends AbstractCommandExecutionListener implements CommandNotFoundListener,
+         WizardExecutionListener
 {
    Result result;
+   boolean wizard;
+
+   @Override public void preWizardExecuted(UIWizard wizard, UIExecutionContext context)
+   {
+      this.wizard = true;
+   }
+
+   @Override public void postWizardExecuted(UIWizard wizard, UIExecutionContext context, Result result)
+   {
+      synchronized (this)
+      {
+         this.result = result;
+      }
+   }
+
+   @Override public void postWizardFailure(UIWizard wizard, UIExecutionContext context, Throwable failure)
+   {
+      synchronized (this)
+      {
+         this.result = Results.fail("Error encountered during command execution.", failure);
+      }
+   }
 
    @Override
    public void preCommandExecuted(UICommand command, UIExecutionContext context)
@@ -26,18 +51,24 @@ class ScriptCommandListener extends AbstractCommandExecutionListener implements 
    @Override
    public void postCommandExecuted(UICommand command, UIExecutionContext context, Result result)
    {
-      synchronized (this)
+      if (!wizard)
       {
-         this.result = result;
+         synchronized (this)
+         {
+            this.result = result;
+         }
       }
    }
 
    @Override
    public void postCommandFailure(UICommand command, UIExecutionContext context, Throwable failure)
    {
-      synchronized (this)
+      if (!wizard)
       {
-         this.result = Results.fail("Error encountered during command execution.", failure);
+         synchronized (this)
+         {
+            this.result = Results.fail("Error encountered during command execution.", failure);
+         }
       }
    }
 
