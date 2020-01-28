232b3aada99886c8111041058dfeff6a66318546
Gianluigi Disconzi <gianluigi.disconzi@gmail.com>
2020-01-28 17:31:08 +0100

    FORGE-2793: Add custom proxy exceptions support with property nonProxyHosts (#633)
    Co-authored-by: George Gastaldi <gegastaldi@gmail.com>

--- configuration/impl/src/main/java/org/jboss/forge/addon/configuration/proxy/ProxySettings.java
+++ configuration/impl/src/main/java/org/jboss/forge/addon/configuration/proxy/ProxySettings.java
@@ -1,11 +1,12 @@
 /**
  * Copyright 2016 Red Hat, Inc. and/or its affiliates.
  *
- * Licensed under the Eclipse Public License version 1.0, available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Licensed under the Eclipse Public License version 1.0, available at http://www.eclipse.org/legal/epl-v10.html
  */
 package org.jboss.forge.addon.configuration.proxy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.jboss.forge.addon.configuration.Configuration;
 
 public class ProxySettings
@@ -15,29 +16,38 @@ public class ProxySettings
    private static final String PROXY_CONFIG_PORT_KEY = "port";
    private static final String PROXY_CONFIG_USERNAME_KEY = "username";
    private static final String PROXY_CONFIG_PASSWORD_KEY = "password";
+   private static final String PROXY_CONFIG_NON_PROXY_HOSTS = "nonProxyHosts";
 
    private final String proxyHost;
    private final int proxyPort;
    private final String proxyUserName;
    private final String proxyPassword;
+   private final List<String> nonProxyHosts;
 
-   private ProxySettings(String proxyHost, int proxyPort, String proxyUserName, String proxyPassword)
+   private ProxySettings(String proxyHost, int proxyPort, String proxyUserName, String proxyPassword,
+            List<String> nonProxyHosts)
    {
       this.proxyHost = proxyHost;
       this.proxyPort = proxyPort;
       this.proxyUserName = proxyUserName;
       this.proxyPassword = proxyPassword;
+      this.nonProxyHosts = nonProxyHosts;
    }
 
    public static ProxySettings fromHostAndPort(String proxyHost, int proxyPort)
    {
-      return new ProxySettings(proxyHost, proxyPort, null, null);
+      return new ProxySettings(proxyHost, proxyPort, null, null, null);
+   }
+
+   public static ProxySettings fromHostPortAndNonProxyHosts(String proxyHost, int proxyPort, List<String> nonProxyHosts)
+   {
+      return new ProxySettings(proxyHost, proxyPort, null, null, nonProxyHosts);
    }
 
    public static ProxySettings fromHostPortAndCredentials(String proxyHost, int proxyPort,
             String proxyUserName, String proxyPassword)
    {
-      return new ProxySettings(proxyHost, proxyPort, proxyUserName, proxyPassword);
+      return new ProxySettings(proxyHost, proxyPort, proxyUserName, proxyPassword, null);
    }
 
    public static ProxySettings fromForgeConfiguration(Configuration configuration)
@@ -46,9 +56,20 @@ public class ProxySettings
       Configuration proxyConfig = configuration.subset("proxy");
       if (proxyConfig != null && !proxyConfig.isEmpty())
       {
-         return new ProxySettings(proxyConfig.getString(PROXY_CONFIG_HOST_KEY),
-                  proxyConfig.getInt(PROXY_CONFIG_PORT_KEY), proxyConfig.getString(PROXY_CONFIG_USERNAME_KEY),
-                  proxyConfig.getString(PROXY_CONFIG_PASSWORD_KEY));
+         final String nonProxyHosts = proxyConfig.getString(PROXY_CONFIG_NON_PROXY_HOSTS);
+         List<String> nonProxyHostsList = null;
+         if (nonProxyHosts != null)
+         {
+            nonProxyHostsList = Arrays.asList(nonProxyHosts.split(","));
+         }
+
+         return new ProxySettings(
+                  proxyConfig.getString(PROXY_CONFIG_HOST_KEY),
+                  proxyConfig.getInt(PROXY_CONFIG_PORT_KEY),
+                  proxyConfig.getString(PROXY_CONFIG_USERNAME_KEY),
+                  proxyConfig.getString(PROXY_CONFIG_PASSWORD_KEY),
+                  nonProxyHostsList
+         );
       }
       else
       {
@@ -74,6 +95,11 @@ public class ProxySettings
    public String getProxyPassword()
    {
       return proxyPassword;
+   }
+
+   public List<String> getNonProxyHosts()
+   {
+      return nonProxyHosts;
    }
 
    public boolean isAuthenticationSupported()
